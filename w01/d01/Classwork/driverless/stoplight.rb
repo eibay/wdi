e0a24626e7d843df2dc02f3bb1881383f28f3bab
wuuk <alexfong@hotmail.com>
2014-08-04 17:28:22 -0400

    my classword

---
+++ w01/d01/Classwork/driverless/stoplight.rb
@@ -0,0 +1,12 @@
+#puts("Hellow class/world")
+
+light = gets.chomp
+
+if light == "red"
+	puts("Stop")
+elsif light == "yellow"
+	puts("Slow Down")
+elsif light == "green"
+	puts("Go")
+
+end
---
+++ w01/d01/Classwork/driverless/valid_location.rb
@@ -0,0 +1,11 @@
+
+
+
+location = "Boston"
+
+if (location = "New York") || San Francisco || Dallas
+   puts "Yes we go there"
+
+else
+	puts "um .... try again"
+end
