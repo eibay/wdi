f745c2eaffd980c17d3692897ba86214c5cf94ce
neener <janinemrosen@gmail.com>
2014-08-04 17:28:18 -0400

    my classword

---
+++ w01/d01/Classwork/driverless/location.rb
@@ -0,0 +1,8 @@
+puts("Where do you want to go?")
+location = "Boston"
+
+if (location == "New York") || (location == "San Francisco") || (location == "Dallas") || (location == "Albequerque") || (location =="Toronto")
+	puts "we go there"
+else 
+	puts "we dont go there sorry!"
+end
