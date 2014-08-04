ced6a5e07d9d619c8ff136469e136cd1e63f4c05
theerickramer <theerickramer@gmail.com>
2014-08-04 17:26:42 -0400

    my classwork

---
+++ w01/d01/Classwork/driverless/runner.rb
@@ -0,0 +1,8 @@
+gas = 10
+
+while gas > 0
+	puts gas
+	gas -= 1
+end
+
+puts "car shut down" if gas == 0
---
+++ w01/d01/Classwork/driverless/stoplight.rb
@@ -0,0 +1,13 @@
+light = gets
+
+if light == "red"
+	puts "STOP"
+elsif light == "green"
+	puts "GO"
+elsif light == "yellow"
+	puts "SLOWDOWN"
+else 
+	puts "That is not a light"
+end
+
+		
