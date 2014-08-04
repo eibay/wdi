8ac08024021eb2179bbc01367a151783beeb5cbb
crawfordforbes <crawford.forbes@gmail.com>
2014-08-04 17:29:20 -0400

    my classwork

---
+++ w01/d01/Classwork/driverless/runner.rb
@@ -0,0 +1,9 @@
+gas = gets.chomp.to_i
+
+while gas > 0
+gas -= 1
+puts("#{gas}")
+end
+puts("and again")
+	
+
---
+++ w01/d01/Classwork/driverless/stoplight.rb
@@ -0,0 +1,17 @@
+puts("what color is the light?")
+light = gets.chomp
+
+if light == "red"
+	puts("stop")
+	elsif 
+		light == "yellow"
+		puts("slow down")
+	elsif light == "green"
+		puts("go")
+	else
+		puts("woah nelly")
+	end
+
+
+		
+		
