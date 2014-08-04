73cdb7504189187668bd4889764f112369821735
HeidiW <foywilliams@gmail.com>
2014-08-04 17:58:11 -0400

    my classwork

---
+++ w01/d01/Classwork/driverless/stoplight.rb
@@ -0,0 +1,12 @@
+light = gets.chomp
+
+if light == "red"
+ puts ("stop")
+
+elsif light == "yellow"
+ puts ("slow down")
+
+elsif light == "green"
+ puts ("go")
+
+end
---
+++ w01/d01/Classwork/driverless/valid_location.rb
@@ -0,0 +1,10 @@
+#does this car go to these cities?
+
+location = "Boston"
+
+if (location == "New York")
+ puts "Yes we go there!"
+elsif location == "San Francisco"
+	puts 
+	puts "Um... try again"
+end
