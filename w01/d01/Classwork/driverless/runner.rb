6cced57cdaadb2d1d85acbf6a7e5e7b099a8498f
TimoorKurdi <timoor.kurdi@gmail.com>
2014-08-04 17:23:40 -0400

    My Classwork

---
+++ w01/d01/Classwork/driverless/runner.rb
@@ -0,0 +1,10 @@
+gas = gets.chomp.to_i
+
+gas = 20
+
+while gas > 0
+	gas -= 1
+	puts "running... "
+end
+
+puts "Car Shut down"
---
+++ w01/d01/Classwork/driverless/valid_location.rb
@@ -0,0 +1,41 @@
+location = "Ohio"
+
+if location == "New York" 
+	puts "Let's Go!"
+elsif location == "San Francisco"
+	puts "Let's Go!"
+elsif location == "Dallas"
+	puts "Let's Go!"
+elsif location == "Alburquerque"
+	puts "Let's Go!"
+elsif location == "Toronto"
+	puts "Let's Go!"
+else
+	puts "Sorry we don't quite know how to get there yet!"
+end
+
+
+# Alternate
+
+location = "New York" 
+
+case location
+
+when "New York" 
+	puts "Let's Go!"
+
+when "San fran"
+	puts "Let's Go!"
+
+when "Dallas"
+	puts "Let's Go!"
+
+when "Alburquerque"
+	puts "Let's Go!"
+
+when "Toronto"
+	puts "Let's Go!"
+
+else
+	"Let's Go!"
+end
