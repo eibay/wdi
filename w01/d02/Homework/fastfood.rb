243d476f0626e1ccd04f08c941c4b391149c262e
lisajwells <lisawells999@gmail.com>
2014-08-06 15:28:24 -0400

    Halfway through Wednesday

--- w01/d02/Homework/fastfood.rb
+++ w01/d02/Homework/fastfood.rb
@@ -1,21 +1,35 @@
 menu = ["Soda", "Fries", "Milkshake", "Chicken Tenders", "Hot Dog", "Hamburger", "Chicken Sandwich", "Double Cheeseburger", "Pizza"]
-order = []
-total_cooktime = 0
-order_complete = "no"
+# order = []
+# total_cooktime = 0
+# order_complete = "no"
 
-puts "What is your first item?"
-order_item = gets.chomp
-order << order_item
-total_cooktime = menu.index(order_item) + 1.0
-puts "Is that all?"
-order_complete = gets.chomp
-while order_complete == "no"
-	puts "What else do you want?"
-	order_item = gets.chomp
-	order << order_item
-	total_cooktime = total_cooktime + menu.index(order_item) + 1.5
-	puts "Is that all?"
-	order_complete = gets.chomp
+# puts "What is your first item?"
+# order_item = gets.chomp
+# order << order_item
+# total_cooktime = menu.index(order_item) + 1.0
+# puts "Is that all?"
+# order_complete = gets.chomp
+# while order_complete == "no"
+# 	puts "What else do you want?"
+# 	order_item = gets.chomp
+# 	order << order_item
+# 	total_cooktime = total_cooktime + menu.index(order_item) + 1.5
+# 	puts "Is that all?"
+# 	order_complete = gets.chomp
+# end
+# puts "That will be #{total_cooktime} minutes."
+# puts order
+
+#=============
+
+order = ["Soda", "Pizza", "Hamburger"]
+order_index = 0 # declare outside loop so it doesn't just change every time
+total_completion_time = 0
+
+while order_index < order.length
+	item_completion_time = menu.index(order[order_index]) + 1
+	total_completion_time += item_completion_time
+	order_index += 1
 end
-puts "That will be #{total_cooktime} minutes."
-puts order
+
+puts total_completion_time
---
+++ w01/d03/Classwork/hashes.rb
@@ -0,0 +1,65 @@
+users = {
+      jeff: {
+        github: "jkonowitch",
+        favorite_numbers: [12, 42, 75]
+      },
+      neel: {
+        github: "darthneel",
+        favorite_numbers: [11, 99, 24]
+      },
+      sean: {
+        github: "sean-west",
+        favorite_numbers: [17, 13, 21]
+      }
+  }
+
+index = 0
+numbers = user[:jeff][:favorite_numbers]
+print numbers
+
+
+
+
+# zoo = {
+# 	bird: ["owl", "toucan", "parrot"],
+# 	mammal: ["cow", "dog", "mole"],
+# 	fish: ["tetra", "catfish", "halibut"]
+# }
+#zoo[:fish].push("monkfish")
+
+
+
+
+
+
+# fav_artists = {Radiohead: ["Ok, Computer", "In Rainbows"]}
+# fav_artists[:Beatles] = []
+# fav_artists[:Beatles].push("The White Album")
+# fav_artists[:Beatles][1]
+
+# harry.potter@hogwarts.edu
+# hermione.granger@hogwarts.edu
+# ronald.weasley@hogwarts.edu
+# arthur_weasley@ministryofmagix.gov.uk
+# garrick@olivanderswands.co.uk
+
+# emails = {}
+
+# emails["Harry"] = "harry.potter@hogwarts.edu"
+# emails["Hermione"] = "hermione.granger@hogwarts.edu"
+
+# emails
+# emails.key("")
+
+# ===============
+
+# zoo = {
+# 	"Fred" => "Zebra",
+# 	"Honey" => "Hippo",
+# 	"Cheetah" => "Chimp",
+# 	"Esmeralda" => "Alpaca",
+# 	"Mo" => "Owl"
+# }
+# zoo["Geegee"] = "Snake"
+
+# zoo
