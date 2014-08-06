26e24eaafc7249956b5d88eeb67339c316825dec
HeidiW <foywilliams@gmail.com>
2014-08-06 13:34:26 -0400

    Morning work

---
+++ w01/d03/Classwork/hashes.rb
@@ -0,0 +1,19 @@
+harry_potter@hogwarts.edu
+hermione_granger@hogwarts.edu
+ronald_weasley@hogwarts.edu
+arthur_weasley@ministryofmagic.gov.uk
+garrick@oliverswands.edu
+
+#Lets use a hash of zoo animals.
+#Let the key be the name of an animal and let the value be the species of animal
+
+zoo = {}
+
+"Larry" => "Lion"
+"Gerry" => "Giraffe"
+"Sammy" => "Seal"
+"Peter" => "Penguin"
+"Andrew" => "Otter"
+
+menu = {"Soda" => 1, "Chicken Tenders" => 6, "Fries" => 2, "Cheeseburger" => 7, "Deluxe Triple Hamburger and Bacon Combo" => 18, "Pizza" => 9, "Kale Salad" => 6}
+  order = ["Soda", "Kale Salad", "Fries"]
