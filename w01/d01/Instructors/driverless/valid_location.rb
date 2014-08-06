43716731d5498a0cb24923de889acbc23f2ee181
leecrawford <leecrawford808@gmail.com>
2014-08-06 00:12:20 -0400

    my homework such as it is

--- w01/d01/Instructors/driverless/valid_location.rb
+++ w01/d01/Instructors/driverless/valid_location.rb
@@ -1,11 +1,50 @@
-location = "Boston"
+# location = "San Fisco"
 
-if (location == "New York")
-  puts "Yes we go there!"
-elsif location == "San Francisco"
-  puts "Yes we go there!"
-elsif location == "Dallas"
-    puts "Yes we go there!"
-else
-  puts "Um...try again"
+# if (location == "New York")
+#   puts "Yes we go there!"
+# elsif location == "San Francisco"
+#   puts "Yes we go there!"
+# elsif location == "Dallas"
+#     puts "Yes we go there!"
+# else
+#   puts "Um...try again"
+# end
+
+# case location
+# 	when "New York"
+# 		puts "yes we go there"
+# 	when "San Francisco"
+# 		puts "yes there too"
+# 	when "Dallas"
+# 		puts "sure"
+# 	else
+# 		puts "try again"
+# end
+# # this is the same algorithm as above, but with case, it's more streamlined)
+# # could also do a method for the puts statement affirming if same text
+
+
+# # or you could do it with an array, as below, which is less cumbersome
+
+# Using index to access particular items in arrays
+# foods = ["cake", "cookies", "shark"] 
+# puts foods [2] #will return shark=end
+
+
+
+location = "San Francisco"
+valid_locations = ["New York", "San Francisco", "Dallas"] 
+index = 0
+found = false   #this operates like a switch; to be turned off when valid_location == location
+while index <= 2
+	if valid_locations[index] == location
+	found = true  #here's where the switch is turned off
 end
+	index = index + 1 # this is the iteration through the positions on the index of the array
+end
+if found
+	puts "Yes we go there"
+else
+	puts "No try again"
+end
+
---
+++ w01/d02/Homework/fastfood2.rb
@@ -0,0 +1,73 @@
+# This is a mess. At various points earlier on, I seemed more on the right path, but I definitely lost it and seem only to be getting more lost.
+# I expect to get clear when we review homework, but otherwise, I'll go over it with the TA tomorrow.
+
+
+#PRELIMINARIES
+
+item_i = 0	#counter set to 0
+
+desire = "Yes"
+
+menu_option_array = ["Soda", "Fries", "Milkshake", "Chicken Tenders", "Hot Dog", "Hamburger", "Chicken Sandwich", "Double Cheeseburger", "Pizza"]
+
+order_array = []					#creates array to hold selected_items
+
+
+#PROCEDURES
+
+while (item_i <=7) 
+  puts "Do you want to order food? Yes or No"
+  desire = gets.chomp
+  if desire == "Yes"
+  	puts "What will you have?"
+  	food = gets.chomp
+	puts food
+
+order_array << food   
+puts order_array
+
+	puts "Anything else?"
+	more = gets.chomp
+  item_i = item_i + 1
+end
+end
+
+# order_array.push(4).push(5)
+
+
+# order_array << "world"   
+
+#--ITERATION
+
+# while (item_i <=7) 					# for elements 0-7
+# 	puts "would you like to order something?"
+# 	response = gets.chomp
+# 		if response == "yes"
+# 			what_u_want
+# 			item_i = item_i +1	
+# 			order_array.push(food)  	#pushes (adds) selected_item to order_array
+# 		end
+# end
+	puts order_array[]
+
+# #--METHODS
+
+# def want_to_order
+# 	puts "would you like to order something?"
+# 	response = gets.chomp
+# end
+
+# def what_u_want
+# 	puts "what would you like?"
+# 	food = gets.chomp
+# end
+
+# def anything_more
+# 	puts "would you like anything else?"
+# 	more_yes_no = gets.chomp
+# end
+
+# def what_more
+# 	puts "what else would you like?"
+# 	food = gets.chomp
+# end
