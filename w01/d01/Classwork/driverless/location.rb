b11df5a1ca8145c7c721d49289323542a9488112
neener <janinemrosen@gmail.com>
2014-08-06 10:03:27 -0400

    fastfood

--- w01/d01/Classwork/driverless/location.rb
+++ w01/d01/Classwork/driverless/location.rb
@@ -1,8 +1,64 @@
-puts("Where do you want to go?")
-location = "Boston"
+location = "San Francisco"
 
-if (location == "New York") || (location == "San Francisco") || (location == "Dallas") || (location == "Albequerque") || (location =="Toronto")
-	puts "we go there"
-else 
-	puts "we dont go there sorry!"
+valid_locations = ["New York", "San	Francisco", "Dallas"]
+
+# new_york = "New York"
+# san_fran = "San Francisco"
+# dallas = "Dallas"
+# valid_locations = [new_york, san_fran, dallas]
+
+
+#while 
+	
+#end
+
+index = 0
+found = false
+
+while index <= 2
+
+#	if location == valid_locations[index]
+#		puts "YES WE GO THERE!"
+#	else
+#		puts "umm. try again"
+#	end
+	
+if valid_locations[index] == location
+	found = true
 end
+
+
+	index = index + 1
+end
+
+if found
+	puts "yes we go there!"
+else
+	puts "no we don't go there!"
+end
+
+
+
+
+
+#if (location == "New York") || (location == "San Francisco") || (location == "Dallas") || (location == "Albequerque") || (location =="Toronto")
+#	puts "we go there"
+# elsif location == "San Francisco"
+# 	puts "Yes we go there!"
+# elsif location == "Dallas"
+# 	puts "Yes we go there!"
+#else 
+#	puts "we dont go there sorry!"
+#end
+
+# case location
+# when "New York"
+# 	puts "yes!"
+# when "San Francisco"
+# 	puts "yep"
+# when "Dallas"
+# 	puts "uh huh"
+# else 
+# 	puts "try again"
+# end
+
---
+++ w01/d02/Homework/fastfood.rb
@@ -0,0 +1,23 @@
+
+menu = ["Soda", "Fries", "Milkshake", "Chicken Tenders", "Hot Dog", "Hamburger", "Chicken Sandwich", "Double Cheeseburger", "Pizza"]
+
+order = ["Soda", "Pizza", "Hamburger"]
+
+total_completion_time = 0
+
+#this index is used to loop thorugh the order array
+order_index= 0
+
+while order_index < order.length
+	item = order[order_index]
+	item_completion_time = menu.index(item) + 1
+	total_completion_time += item_completion_time
+	order_index += 1
+	#everytime the loop finishes we want to trigger the order index
+
+	#i wanna look in menu and find the item i am searching for. take the index value and let me go to my order and get the index value.
+	# give me the index value of pizza, then pizza, then hamburger
+puts total_completion_time
+
+end
+
