951c79c351d18c9cde69f793f68e620772a10aea
clay-reed-a <clay.reed.a@gmail.com>
2014-08-07 11:45:54 -0400

    continue to refractor example.rb

--- w01/d04/Classwork/example.rb
+++ w01/d04/Classwork/example.rb
@@ -1,3 +1,4 @@
+# setup the system
 $subway = {
 :N => ["Times_sq", "34th", "28th", "23rd", "Union_sq", "8th"],
 :"#{6}" => ["Grand_central", "33rd", "28th", "23rd", "Union_sq", "Astor_pl"],
@@ -5,30 +6,23 @@ $subway = {
 :Q => ["Times_sq", "34th", "Union_sq", "Canal_st"]
 }
 
+# describe the system to the user 
+puts "\nThe only lines in service today are:", "The N Line: #{$subway[:N]}", "The 6 Line: #{$subway[:"#{6}"]}", "The L Line: #{$subway[:L]}", "The Q Line: #{$subway[:Q]}"
 
-
-puts "\nThe only lines in service today are:"
-puts "The N Line: #{$subway[:N]}"
-puts "The 6 Line: #{$subway[:"#{6}"]}"
-puts "The L Line: #{$subway[:L]}"
-puts "The Q Line: #{$subway[:Q]}"
-
-
+# get the user's input 
 puts "\nAre you starting on the N, 6, L, or Q line?"
 begline = gets.chomp.capitalize
-
 puts "\nWhere on the #{begline} are you now?"
 begstop = gets.chomp.capitalize
-
-
 puts "\nAre you ending on the N, 6, L, or Q line?"
 endline = gets.chomp.capitalize
-
 puts "\nWhere on the #{endline} are you going to?"
 endstop = gets.chomp.capitalize
 
+# store the transfer station 
 trns = "Union_sq" 
 
+# helper functions 
 def get_stop l, s 
 	$subway[l.to_sym].index s 
 end 
@@ -37,10 +31,12 @@ def diff a, b
 	(a - b).abs 
 end 
 
+# main logic  
 def get_stops line1, stop1, line2, stop2
 	diff get_stop(line1, stop1), get_stop(line2, stop2)
 end 
 
+# control structure 
 if begline == endline 
 	totalstops = get_stops begline, begstop, endline, endstop 
 else 
@@ -49,5 +45,6 @@ else
 	totalstops = first_leg + second_leg 
 end 
 
+# emit output 
 puts "You will ride a total of #{totalstops} stops"
 puts "\nWe are being held indefinitely because of train traffic ahead.."
