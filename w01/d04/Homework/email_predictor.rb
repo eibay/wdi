c9dbee0594c028543879c6f142f4676944f4636d
clay-reed-a <clay.reed.a@gmail.com>
2014-08-07 15:39:29 -0400

    emits a string stating the pattern

--- w01/d04/Homework/email_predictor.rb
+++ w01/d04/Homework/email_predictor.rb
@@ -1,5 +1,5 @@
 
-in = [ 
+inp = [ 
   ["Jeffrey Konowitch", "jeff.konowitch@generalassemb.ly"],
   ["Neel Patel", "neel.patel@generalassemb.ly"],
   ["Sean West", "sean.west@generalassemb.ly"],
@@ -11,23 +11,23 @@ in = [
 ]
 
 i = 0
+histogram = {} 
+while i < inp.length-1 
 
-while i < in.length-1 
 	# extract the name & addr 
-	name = in[i][0] 
-	addr = in[i][1] 
+	name = inp[i][0] 
+	addr = inp[i][1] 
+	
+	# output addr for testing 
+	# puts addr 
+
 	# break them down into component parts 
 	name_arr = name.split(" ").each(&:downcase!) 
-	name_no = name.length 
+	name_no = name_arr.length 
 
 	addr_arr = addr.split "@" 
 	username = addr_arr[0]
-	domain = addr_arr[1] 
-	
-	# str represents a regex, assumes a strange letter
-	# in the middle of an addr is 
-	# a middle initial 
-	regex = "/" + first + "_." + last + "/" 
+	domain = addr_arr[1] 	
 
 	case name_no  
 	when 1
@@ -39,19 +39,23 @@ while i < in.length-1
 		end 
 	when 2 
 		first, last = name_arr
+		regex = "/" + first + "_." + last + "/" 
+		# str representing a regex, assumes a strange letter
+		# in the middle of an addr is 
+		# a middle initial 
 		if "#{first}.#{last}" == username
 			puts "It's \"first.last\""
 		elsif "#{first}" == username 
 			puts "It's \"first\"" 
 		elsif first + last == username 
 			puts "It's \"firstlast\""
-		elsif (regex.eval =~ username) == 0 
+		elsif ((eval regex) =~ username) == 0 
 			puts "It's \"first_initiallast\"" 
 		else 
 			puts "I can't find a pattern"
 		end 
 	when 3
-		first, middle, last == name_arr 
+		first, middle, last = name_arr 
 		if "#{first}.#{last}" == username
 			puts "It's \"first.last\""
 		elsif "#{first}" == username 
