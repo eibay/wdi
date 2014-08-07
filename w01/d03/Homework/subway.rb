50f82919abf5ddc3de12c75920282bca473759c0
trishfromtisch <parlaws@gmail.com>
2014-08-07 01:34:05 -0400

    made progress

--- w01/d03/Homework/subway.rb
+++ w01/d03/Homework/subway.rb
@@ -4,27 +4,33 @@
 
 subway_system = 
 {starting_line: {
-	starting_n_stops: ["Times Square", "34th", "W 28th", "W 23rd", "Union Square", "8th"], 
-	starting_l_stops: ["8th", "6th", "Union Square", "3rd", "1st"],
-	starting_six_stops: ["Grand Central", "33rd", "E 28th", "E 23rd", "Union Square", "Astor Place"]
+	n: ["Times Square", "34th", "W 28th", "W 23rd", "Union Square", "8th St"], 
+	l: ["8th Ave", "6th", "Union Square", "3rd", "1st"],
+	six: ["Grand Central", "33rd", "E 28th", "E 23rd", "Union Square", "Astor Place"]
 },
 ending_line: {
-	ending_n_stops: ["Times Square", "34th", "W 28th", "W 23rd", "Union Square", "8th"], 
-	ending_l_stops: ["8th", "6th", "Union Square", "3rd", "1st"],
-	ending_six_stops: ["Grand Central", "33rd", "E 28th", "E 23rd", "Union Square", "Astor Place"]
+	n: ["Times Square", "34th", "W 28th", "W 23rd", "Union Square", "8th St"], 
+	l: ["8th Ave", "6th", "Union Square", "3rd", "1st"],
+	six: ["Grand Central", "33rd", "E 28th", "E 23rd", "Union Square", "Astor Place"]
 	}
 }
 
-origin = "W 28th"
-pertinent_starting_line = subway_system[:starting_line].key(origin)
+puts "Which line are you taking?"
+origin_line = gets.chomp.to_sym
+puts "Which station are you starting at?"
+origin = gets.chomp.to_s
 
-number_of_stops_to_union_sq = ((subway_system[:starting_line][pertinent_starting_line].index("Union Square")) - subway_system[:starting_line][pertinent_starting_line].index(origin)).abs
+number_of_stops_to_union_sq = ((subway_system[:starting_line][origin_line].index("Union Square")) - subway_system[:starting_line][origin_line].index(origin)).abs
 
-destination = "8th"
-pertinent_ending_line = subway_system[:starting_line].key(destination)
-number_of_stops_from_union_sq = ((subway_system[:ending_line][pertinent_ending_line].index(destination)) - subway_system[:ending_line][pertinent_ending_line].index("Union Square")).abs
+puts = "On which line is your destination?"
+destination_line = gets.chomp.to_sym
+puts ="What's your final stop?"
+destination = gets.chomp.to_s
 
-total_number_of_stops = number_of_stops_to_union_sq + number_of_stops_from_union_sq
+
+number_of_stops_from_union_sq = ((subway_system[:ending_line][destination_line].index(destination)) - subway_system[:ending_line][destination_line].index("Union Square")).abs
+
+total_number_of_stops = (number_of_stops_to_union_sq + number_of_stops_from_union_sq)
 
 print total_number_of_stops
 
