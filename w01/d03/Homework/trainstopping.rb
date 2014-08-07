da5c4134004a32b314c7ca2029e570263026473c
adrianmlin <adrian.mx.lin@gmail.com>
2014-08-07 09:12:42 -0400

    bonus to train homework

--- w01/d03/Homework/trainstopping.rb
+++ w01/d03/Homework/trainstopping.rb
@@ -12,7 +12,8 @@ totalstops = 0
 subway = {
 	:N => ["Times_sq", "34th", "28th", "23rd", "Union_sq", "8th"],
 	:"#{6}" => ["Grand_central", "33rd", "28th", "23rd", "Union_sq", "Astor_pl"],
-	:L => ["8th", "6th", "Union_sq", "3rd", "1st"]
+	:L => ["8th", "6th", "Union_sq", "3rd", "1st"],
+	:Q => ["Times_sq", "34th", "Union_sq", "Canal_st"]
 }
 
 
@@ -21,23 +22,24 @@ puts "The only lines in service today are:"
 puts "The N Line: #{subway[:N]}"
 puts "The 6 Line: #{subway[:"#{6}"]}"
 puts "The L Line: #{subway[:L]}"
+puts "The Q Line: #{subway[:Q]}"
 
 
-puts "\nAre you starting on the N, 6, or L line?"
+puts "\nAre you starting on the N, 6, L, or Q line?"
 begline = gets.chomp.capitalize
 
 puts "\nWhere on the #{begline} are you now?"
 begstop = gets.chomp.capitalize
 
 
-puts "\nAre you ending on the N, 6, or L line?"
+puts "\nAre you ending on the N, 6, L, or Q line?"
 endline = gets.chomp.capitalize
 
 puts "\nWhere on the #{endline} are you going to?"
 endstop = gets.chomp.capitalize
 
 
-### if single line ##
+
 
 if begline == endline 
 
@@ -45,9 +47,7 @@ if begline == endline
 
 else
 	first_leg = (subway[begline.to_sym].index("Union_sq") - subway[begline.to_sym].index(begstop)).abs
-	puts first_leg
 	second_leg = (subway[endline.to_sym].index("Union_sq") - subway[endline.to_sym].index(endstop)).abs
-	puts second_leg
 	totalstops = first_leg + second_leg
 
 	
