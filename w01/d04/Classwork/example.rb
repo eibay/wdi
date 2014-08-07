2bcbaae548a5f1812bde0f6e4d99859a9727195e
clay-reed-a <clay.reed.a@gmail.com>
2014-08-07 14:39:02 -0400

    comment out pry

--- w01/d04/Classwork/example.rb
+++ w01/d04/Classwork/example.rb
@@ -1,3 +1,8 @@
+#!/usr/bin/ruby 
+
+# uncomment to use pry for debugging 
+# require 'pry'
+
 # initialize the system
 $subway = {
 :N => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
@@ -21,38 +26,43 @@ puts dscry
 
 # get the user's input 
 puts "\nAre you starting on the N, 6, L, or Q line?"
-begline = gets.chomp.capitalize
+begline = gets.chomp
 puts "\nWhere on the #{begline} are you now?"
-begstop = gets.chomp.capitalize
+begstop = gets.chomp
 puts "\nAre you ending on the N, 6, L, or Q line?"
-endline = gets.chomp.capitalize
+endline = gets.chomp
 puts "\nWhere on the #{endline} are you going to?"
-endstop = gets.chomp.capitalize
+endstop = gets.chomp
 
 # store the transfer station 
-trns = "Union_sq" 
+trns = "Union Square" 
 
 # helper functions 
 def get_stop l, s 
+	# binding.pry 
 	$subway[l.to_sym].index s 
 end 
 
 def diff a, b 
-	(a - b).abs 
+	# binding.pry 
+	(a - b).abs 	
 end 
 
 # main logic  
 def get_stops line1, stop1, line2, stop2
-	diff get_stop(line1, stop1), get_stop(line2, stop2)
+	# binding.pry 
+	diff get_stop(line1, stop1), get_stop(line2, stop2) 
 end 
+
+
 
 # control structure 
 if begline == endline 
-	totalstops = get_stops begline, begstop, endline, endstop 
+	totalstops = get_stops(begline, begstop, endline, endstop) 
 else 
-	first_leg = get_stops begline, trns, begline, begstop 
-	second_leg = get_stops endline, trns, endline, endstop 
-	totalstops = first_leg + second_leg 
+	first_leg = get_stops begline, trns, begline, begstop  
+	second_leg = get_stops endline, trns, endline, endstop  
+	totalstops = first_leg + second_leg  
 end 
 
 # emit output 
