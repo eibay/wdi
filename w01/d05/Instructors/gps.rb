a2417ac849db4cef27dd72b9ff77bfd2379f0260
jkonowitch <jeff.konowitch@generalassemb.ly>
2014-08-08 11:08:19 -0400

    with tcp

--- w01/d05/Instructors/gps.rb
+++ w01/d05/Instructors/gps.rb
@@ -1,10 +1,18 @@
-puts "Where do you want to go?"
-requested_location = gets.chomp
+require 'socket'
+server = TCPServer.new 2000
+client = server.accept
+
+puts "Client connected!"
+
+client.puts "Where do you want to go?"
+requested_location = client.gets.chomp
 
 locations = ["New York", "San Francisco", "Dallas"]
 
 if locations.include?(requested_location)
-  puts "YES we go here"
+  client.puts "YES we go here"
 else
-  puts "NOPE"
+  client.puts "NOPE"
 end
+
+client.close
