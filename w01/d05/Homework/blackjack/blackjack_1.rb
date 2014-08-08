ab135fa916cbd755dfc38b587d4340ae8f561c25
adrianmlin <adrian.mx.lin@gmail.com>
2014-08-08 18:59:25 -0400

    2nd commit, saved files into separate folders outside working file

--- w01/d05/Homework/blackjack/blackjack_1.rb
+++ w01/d05/Homework/blackjack/blackjack_1.rb
@@ -3,9 +3,32 @@ ___
 
 ## Blackjack
 
-###Part 1
-- Create an array with all the cards in a deck (2-10, J, Q, K A)
-- **Don't** explicitly just write out an array with 52 cards
-  - You can write out a smaller array and then generate the rest
-- Output the array of cards
-------
+# ###Part 1
+# - Create an array with all the cards in a deck (2-10, J, Q, K A)
+# - **Don't** explicitly just write out an array with 52 cards
+#   - You can write out a smaller array and then generate the rest
+# - Output the array of cards
+# ------
+
+values = [*1..13,]
+suits = ["♤", "♡", "♢", "♧"]
+
+deck = [] 
+
+suits.each do |suit|
+  values.each do |value|
+    case value
+    when 1
+      value = "A"
+    when 11
+      value = "J"
+    when 12
+      value = "Q"
+    when 13
+      value = "K"
+    end
+    card = "#{value} of #{suit}"
+    deck << card
+  end
+end
+print "\nYour deck is  #{deck}"
--- w01/d05/Homework/blackjack/blackjack_2.rb
+++ w01/d05/Homework/blackjack/blackjack_2.rb
@@ -6,3 +6,45 @@
   - All face cards have the value of 10
   - Ace is 1
 
+#part1
+values = [*1..13,]
+suits = ["♤", "♡", "♢", "♧"]
+
+deck = [] 
+
+suits.each do |suit|
+  values.each do |value|
+    case value
+    when 1
+      value = "A"
+    when 11
+      value = "J"
+    when 12
+      value = "Q"
+    when 13
+      value = "K"
+    end
+    card = "#{value} of #{suit}"
+    deck << card
+  end
+end
+print "\nYour deck is  #{deck}"
+
+
+
+##### Q2 non-method attempt
+########## getting random cards
+random_card_array = []
+qty_random_cards = 2
+puts "\n\n"
+while qty_random_cards > 0 do
+  random_card = deck[rand(deck.length)]
+  # puts "You picked #{random_card}."
+  deck.delete(random_card)
+
+  qty_random_cards -= 1 
+  random_card_array << random_card
+end
+
+print "\nyour random cards are #{random_card_array}"
+
