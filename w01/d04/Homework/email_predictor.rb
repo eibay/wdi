c9d6509a49977879949e855969c7464ff0cf5371
TimoorKurdi <timoor.kurdi@gmail.com>
2014-08-08 13:44:14 -0400

    GPS

--- w01/d04/Homework/email_predictor.rb
+++ w01/d04/Homework/email_predictor.rb
@@ -1,21 +1,4 @@
-#Array style
-
- # puts "The name/email provided follows pattern A" # Your code should detect that the above name/email combo follows pattern a.
-
-
-
-# # puts "The name/email provided follows pattern B"
-
-
-
-# # puts "The name/email provided follows pattern C"
-
-
-
-# puts "The name/email provided follows pattern D"
-
-#e_mail_styles = [[name_dot_email = ["Sean West", "sean.west@generalassemb.ly"],[first_email = ["Sean", "sean@generalassemb.ly"],[first_last_email = ["Seanwest", "seanwest@generalassemb.ly"],[first_initiallast_email = ["Swest", "swest@generalassemb.ly"]]]
-
+	#require "pry"
 
 	name_email = ["Sean West", "sean.west@generalassemb.ly"] #Scenario A
 	name_split = name_email[0].split(" ") #Sean, West
@@ -24,21 +7,18 @@
 	last_name = name_split[1].downcase # West
 	domain = name_email[1].split("@")[1] #generalassemb.ly
 
-
-	first_email = ["Sean", "sean@generalassemb.ly"] #Scenario B
-	first_last_email = ["Seanwest", "seanwest@generalassemb.ly"] #Scenario C
-	first_initiallast_email = ["Swest", "swest@generalassemb.ly"] #Scenario D
-
-	if first_name + "." + last_name == name_email #if sean.west
+#binding.pry
+if first_name + "." + last_name == name_email #if sean.west
 	puts "The name/email provided follows pattern a"
 
-	elsif first_name == first_email #if sean
+	elsif first_name == domain[0] #if sean
 	puts "The name/email provided follows pattern b"
 
-	elsif first_name + last_name == first_last_email #if sean and west altogether
+	elsif first_name + last_name == domain[0] #if sean and west altogether
+
 	puts "The name/email provided follows pattern c"
 
-	elsif first_name.split(" ") + last_name == first_initiallast_email #if sean split at whatever character you want + west
+	#elsif first_name.split(" ") + last_name == domain[0] #if sean split at whatever character you want + west
 	puts "The name/email provided follows pattern d"
 
 	else
--- w01/d05/Instructors/gps.rb
+++ w01/d05/Instructors/gps.rb
@@ -1,10 +1,19 @@
-puts "Where do you want to go?"
-requested_location = gets.chomp
+require 'socket'
+server = TCPServer.new 2000
+client = server.accept
+
+puts "Client connected!" #Still connected to your own terminal that shows you info that the client
+#doesnt see because client is not being called before it.
+
+client.puts "Where do you want to go?"
+requested_location = client.gets.chomp
 
 locations = ["New York", "San Francisco", "Dallas"]
 
 if locations.include?(requested_location)
-  puts "YES we go here"
+  client.puts "YES we go here"
 else
-  puts "NOPE"
+  client.puts "NOPE"
 end
+
+client.close
