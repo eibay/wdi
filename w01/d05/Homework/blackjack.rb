806f7e9c953476ca555788a855335e3a8aa69397
clay-reed-a <clay.reed.a@gmail.com>
2014-08-10 00:43:07 -0400

    part 1 done

---
+++ w01/d05/Homework/blackjack.rb
@@ -0,0 +1,41 @@
+# set suits Unicode symbol to variable names 
+spades, clubs, diamonds, hearts = "\u2660", "\u2663", "\u2666", "\u2665"
+suits = [spades, clubs, diamonds, hearts]
+
+class String 
+
+	# ANSI color codes for strs to look like card suits 
+	def red_suit
+		"\033[1;31;47m" + self + "\033[m"
+	end 
+
+	def black_suit 
+		"\033[0;30;47m" + self + "\033[0m"
+	end 
+end 
+
+deck = [] 
+
+i = 0 
+suits.each do |suit|
+
+	(2..10).each do |no|
+		mk = "#{no} of #{suit}" 
+		if i < 2
+			deck << mk.black_suit
+		else 
+			deck << mk.red_suit
+		end 
+	end 
+	['J', 'Q', 'K', 'A'].each do |face|
+		mk = "#{face} of #{suit}"
+		if i < 2
+			deck << mk.black_suit
+		else 
+			deck << mk.red_suit
+		end 
+	end 
+	i += 1 
+end 
+
+puts deck.join " "
