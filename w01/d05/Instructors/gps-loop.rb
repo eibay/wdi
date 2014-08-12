df1d4ff03b15cffe5152131ea977e12deb43c1ff
conorhastings <hastings.conorm@gmail.com>
2014-08-11 20:37:11 -0400

    homework for week 2, day 1

--- w01/d05/Instructors/gps-loop.rb
+++ w01/d05/Instructors/gps-loop.rb
@@ -1,21 +1,13 @@
 require 'socket'
+require 'colorize'
 server = TCPServer.new 2000
 
 while true
   client = server.accept
 
   puts "Client connected!"
-
-  client.puts "Where do you want to go?"
-  requested_location = client.gets.chomp
-
-  locations = ["New York", "San Francisco", "Dallas"]
-
-  if locations.include?(requested_location)
-    client.puts "YES we go here"
-  else
-    client.puts "NOPE"
-  end
+client.puts "<font color='red'><blink><h1>My website is so annoying! PLEASE LEAVE<h1></font></blink> <h2><a href='10.6.113.186:2000'>EXIT HERE<a></h2>"
 
   client.close
+
 end
