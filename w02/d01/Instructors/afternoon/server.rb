8a4bc2f1992689bb8fd588c7309aaf84ec1bcda2
theerickramer <theerickramer@gmail.com>
2014-08-11 23:15:00 -0400

    Homework w02 d01

--- w02/d01/Instructors/afternoon/server.rb
+++ w02/d01/Instructors/afternoon/server.rb
@@ -25,7 +25,13 @@ while true
 	elsif path == "/about"
 		client.puts File.read("./views/about.html")
 		puts "Sent 'about.html' to #{client_ip}"
-		else
+	elsif path == "/gryffindor"
+		client.puts File.read("./views/gryffindor.html")
+		puts "Sent 'gryffindor.html' to #{client_ip}"
+	elsif path == "/slytherin"
+		client.puts File.read("./views/slytherin.html")
+		puts "Sent 'slytherin.html' to #{client_ip}"
+	else
 		client.puts "ERROR! Try again."
 		puts "Send ERROR to #{client_ip}"			
 	end
