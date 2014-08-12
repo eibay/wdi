858a2739d5f8ea97e2ab3bca77d9a93c336f7bfc
clay-reed-a <clay.reed.a@gmail.com>
2014-08-12 14:30:22 -0400

    have a list of people you know

--- w02/d02/Classwork/server.rb
+++ w02/d02/Classwork/server.rb
@@ -3,20 +3,26 @@ require 'socket'
 s = TCPServer.new 2000 
 
 loop do 
+	users = %w(monkey jeff severus)
 	client = s.accept 
 	puts "Client connected!"
 
 	request = client.gets.chomp 
 	path = request.split(' ')[1]
+	
+	a = path.split '/'
+	name = a[2] # WARNING: will be nil for /hello 
+
 	puts "\tThey want #{path}!"
 
 	if path == "/hello"
 		t = Time.new.strftime "%l:%M%p"
  		client.puts "<h1>Hello! The time is #{t}!</h1>"
  		puts "sent them the hello page!"
- 	elsif (path =~ /\/hello\/\w+/) == 0 
- 		name = path.split('/')[2].capitalize 
+ 	elsif a[1] == "hello" && users.include?(name) 
  		client.puts "<h1>Hello, #{name}!</h1>" 
+ 	elsif a[1] == "hello" && !users.include?(name)
+ 		client.puts "<h1>You aren't included in the users array, #{name}. Feel free to sign up.</h1>" 
 	elsif (path =~ /\/\w+/) == 0 
 		# when requests, say, favicon.ico 
 		# problem 
