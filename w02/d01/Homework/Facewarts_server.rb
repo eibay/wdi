3462f5a7c23e6b32358a2f970a8f62d2ac485c5a
gemsandjava <andreaortegawilliams@gmail.com>
2014-08-13 07:17:24 -0400

    Movie DB homework

--- w02/d01/Homework/Facewarts_server.rb
+++ w02/d01/Homework/Facewarts_server.rb
@@ -30,13 +30,8 @@ client_ip = client.remote_address.ip_address
 
 puts "Client has connected from #{client_ip}"
 
-#not sure if it would go here but i'm putting it here:
-
-# house 
-
-# student_name.html
-
-# page is actually going to be /house/student_name
+#Question = is there a way to dynamically populate the title tab using ruby?
+#i googled it and saw a way using php....
 
 # gryffindor_house = {harry_potter, ron_weasley}
 # slytherin_house = {draco_malfoy, vincent_crabbe}
@@ -55,31 +50,40 @@ path = request.split(" ")[1] #pulls the specific path they selected (which is af
 puts "Client #{client_ip} is attempting to reach #{path}"
 
 if path == '/' #means if the path == root
-	client.puts File.read('/views/index.html') #this tells it what file to read
+	client.puts File.read('./views/index.html') #this tells it what file to read
 	puts "Sent 'index.html' to #{client_ip}"#we sent this to the client  
 elsif path == '/stylesheets/style.css' #you always have a / on a URL, it's just to separate/show subfolder
 #means if the path is to the stylesheet - for browser / means folder where your website is index.html
 	# client.puts File.read('../stylesheets/style.css') #old path from classwork
-	client.puts File.read('/stylesheets/style.css') #new path - is this right b/c i dont have to go up a directory, right?
+	client.puts File.read('./stylesheets/style.css') #new path - is this right b/c i dont have to go up a directory, right?
 elsif path == "/about.html"
-	client.puts File.read('/views/about.html')
-	puts "Sent 'about.html' to #{client_ip}"#we sent this to the client
+	client.puts File.read('./views/about.html')
+	puts "Sent 'about.html' to #{client_ip}"
 elsif path == "/gryffindor.html"
-	client.puts File.read('/views/gryffindor.html')
-	puts "Sent 'gryffindor.html' to #{client_ip}"#we sent this to the client
+	client.puts File.read('./views/gryffindor.html')
+	puts "Sent 'gryffindor. html' to #{client_ip}"
 elsif path == "/slytherin.html"
-	client.puts File.read('/views/slytherin.html')
-	puts "Sent 'slytherin.html' to #{client_ip}"#we sent this to the client
-elsif path == "house/student_name.html"
-	client.puts File.read('/views/student_name.html')
-	puts "Sent '/student_name.html' to #{client_ip}"#we sent this to the client
+	client.puts File.read('./views/slytherin.html')
+	puts "Sent 'slytherin.html' to #{client_ip}"
+elsif path == "gryffindor/harry_potter.html"
+	client.puts File.read('./views/harry_potter.html')
+	puts "Sent '/harry_potter.html' to #{client_ip}"
+elsif path == "gryffindor/ron_weasley.html"
+	client.puts File.read('./views/ron_weasley.html')
+	puts "Sent '/ron_weasley.html' to #{client_ip}"
+elsif path == "slytherin/draco_malfoy.html"
+	client.puts File.read('./views/draco_malfoy.html')
+	puts "Sent '/draco_malfoy.html' to #{client_ip}"
+elsif path == "slytherin/vincent_crabbe.html"
+	client.puts File.read('./views/vincent_crabbe.html')
+	puts "Sent '/vincent_crabbe.html' to #{client_ip}"
 else client.puts "Error. Try again."
 	puts "sent error to client_ip"
-
 end
 
-#needs to be a file path without a period in the if statement, because thats how path will appear in the browser
-#but we need a .
+#needs to be a file path withOUT a period in the if statement, 
+#because thats how path will appear in the browser
+#but we need a . client.puts File.read if we are going up a directory
 
 puts "Client #{client_ip} has disconnected." #all these statements help us track and log 
 #user interaction with the server
---
+++ w02/d02/Homework/Movies_Website.rb
@@ -0,0 +1,119 @@
+### MovieDB!
+
+require 'socket'
+require 'uri'
+# require 'pry'
+
+server = TCPServer.new 2000
+
+while true
+   ##connect
+   client = server.accept
+   puts 'the server is now running'
+
+movie_db = [
+  {
+      name:"The Shawshank Redemption",
+      rating:"9.2",
+      rank:"1",
+      link:"http://www.imdb.com/title/tt0111161/",
+      year:"1994"
+   },
+   {
+      name:"The Godfather",
+      rating:"9.2",
+      rank:"2",
+      link:"http://www.imdb.com/title/tt0068646/",
+      year:"1972"
+   },
+   {
+      name:"The Godfather: Part II",
+      rating:"9.0",
+      rank:"3",
+      link:"http://www.imdb.com/title/tt0071562/",
+      year:"1974"
+   },
+   {
+      name:"Pulp Fiction",
+      rating:"8.9",
+      rank:"4",
+      link:"http://www.imdb.com/title/tt0110912/",
+      year:"1994"
+   },
+   {
+      name:"The Good, the Bad and the Ugly",
+      rating:"8.9",
+      rank:"5",
+      link:"http://www.imdb.com/title/tt0060196/",
+      year:"1966"
+   },
+   {
+      name:"The Dark Knight",
+      rating:"8.9",
+      rank:"6",
+      link:"http://www.imdb.com/title/tt0468569/",
+      year:"2008"
+   },
+]
+
+# #### A movies index page
+# `/movies`
+
+# The page should show a list of all the movies in the db and list them as follows with the name of the title and a link:
+# ```html
+# <ol>
+#   <li><a href="/movies/The%20Shawshank%20Redemption">The Shawshank Redemption</a></li>
+#   <!-- and so on..-->
+# </ol>
+# #we do it like this so when we want 
+# #to see if the name == the name in the hash, it will match
+
+
+# #### A page for each movie
+# Given:
+# `/movies/The%20Dark%20Night`
+
+# The page should show the title, rating, the year it was made, and a link to the imdb.
+
+##listen 
+   request = client.gets.chomp #need this to have a reference to know what they're looking for
+   puts "Client said: #{request}" 
+   #this line above is basically just a server log of this point in the connection
+   path = request.split(' ')[1] #this is to get the array of subpieces that make up the path
+   #[1] is the main path of the page browser wants to load
+
+
+## talk
+  if path == "/movies"
+      counter1 = 0
+      client.puts "<html><h1>TOP MOVIES</h1></html>"
+      client.puts "<ol>"
+      while counter1 < movie_db.length
+      client.puts "<li><a href='/movies/#{movie_db[counter1][:name]}'>#{movie_db[counter1][:name]}</a></li>"
+      counter1 +=1
+      end
+      client.puts "</ol>"
+
+   elsif path.split('/')[1]=="movies" && path.split('/')[2]
+         #path.split('/') vs path.split(' ') give us data to look at in different ways
+         #path.split(' ') gives us a '/'page and we need that to compare to the browser path
+         #path.split('/') allows us to isolate the actual name of the subpage, so we can see
+         #if that subpage name == to something else
+
+      name = URI.decode(path.split('/')[2])
+      counter2 = 0
+         while counter2 < movie_db.length
+            if movie_db[counter2].values.include?(name)
+            client.puts "<p>#{movie_db[counter2][:name]} has a rating of #{movie_db[counter2][:rating]} 
+            and was made in #{movie_db[counter2][:year]} 
+            and the link is #{movie_db[counter2][:link]}</p>"
+            end
+         counter2 += 1
+         end
+   else
+      client.puts "ERROR"
+   end 
+
+client.close
+end  
+
