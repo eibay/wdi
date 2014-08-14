1697b555773281c18b2263f4b630f4edde9a46a2
DanielFarber <farber.daniel.m@gmail.com>
2014-08-13 20:04:17 -0400

    Did my homework (without using .each)

--- w01/d05/Homework/blackjack/bonus_attempt.rb
+++ w01/d05/Homework/blackjack/bonus_attempt.rb
@@ -6,6 +6,7 @@ royals = ["J", "Q", "K", "A"]
 suits = ["H", "C", "S", "D"]
 unshuffled_deck_i = 0
 suit_i = 0
+
 4.times do
 	2.upto(10) { |x| unshuffled_deck.push(x.to_s) }
 	royals.each { |x| unshuffled_deck.push(x) }
@@ -14,6 +15,14 @@ suit_i = 0
 		unshuffled_deck_i += 1
 	end
 	suit_i += 1
+end
+
+def players_playing(array)
+	total = 0
+	array.each do |hash|
+		total += 1 if hash[:playing]
+	end
+	total
 end
 
 card_values = {}
@@ -31,11 +40,23 @@ server = TCPServer.new 1500
 
 $players = []
 
-$players << ["dealer", ["", 0, true]]
+$players[0] = {
+	hand: [],
+	total: 0,
+	playing: true,
+	busted: false,
+	player: "dealer"
+}
 
 client_generator = 1
 while client_generator < number_of_players
-	$players[client_generator] = [server.accept, ["", 0, true]]
+	$players[client_generator] = {
+		hand: [],
+		total: 0,
+		playing: true,
+		busted: false,
+		player: server.accept
+	}
 	client_generator += 1
 end
 
@@ -43,7 +64,7 @@ def tell_everyone(str)
 	puts str
 	index = 1
 	while index < $players.length
-		$players[index][0].puts str
+		$players[index][:player].puts str
 		index += 1
 	end
 end
@@ -54,9 +75,84 @@ tell_everyone "We're gonna play BlackJack.  It's your turn when I say it is.  Un
 score = []
 number_of_players.times{score << 0}
 
-binding.pry
+loop do
+
+	deck = unshuffled_deck.shuffle
+
+	$players.each do |player|
+		2.times do
+			player[:hand].push(deck.pop)
+			player[:total] += card_values[player[:hand][-1][0].to_sym]
+		end
+	end
+
+	while $players[0][:playing] && !$players[0][:busted]
+		puts "Here's your hand: #{$players[0][:hand]}."
+		puts "Your total is #{$players[0][:total]}."
+		puts "Do you hit?"
+		hit = gets.chomp
+		if hit[0].downcase == "y"
+			$players[0][:hand].push(deck.pop)
+			$players[0][:total] += card_values[$players[0][:hand][-1][0].to_sym]
+			puts "The #{$players[0][:hand][-1]} is dealt."
+			if $players[0][:total] > 21
+				puts "You busted at #{$players[0][:total]}!"
+				$players[0][:busted] = true
+			end
+		else
+			puts "Ok, you stand with #{$players[0][:total]}."
+			$players[0][:playing] = false
+		end
+	end
+
+	client_play_ind = 1
+	while client_play_ind < $players.length
+		binding.pry
+		while $players[client_play_ind][:playing] && !$players[client_play_ind][:busted]
+			$players[client_play_ind][:player].puts "Here's your hand: #{$players[client_play_ind][:hand]}."
+			$players[client_play_ind][:player].puts "Your total is #{$players[client_play_ind][:total]}."
+			$players[client_play_ind][:player].puts "Do you hit?"
+			hit = $players[client_play_ind][:player].gets.chomp
+			if hit[0].downcase == "y"
+				$players[client_play_ind][:hand].push(deck.pop)
+				$players[client_play_ind][:total] += card_values[$players[client_play_ind][:hand][-1][0].to_sym]
+				$players[client_play_ind][:player].puts "The #{$players[client_play_ind][:hand][-1]} is dealt."
+				if $players[client_play_ind][:total] > 21
+					$players[client_play_ind][:player].puts "You busted at #{$players[client_play_ind][:total]}!"
+					$players[client_play_ind][:busted] = true
+				end
+			else
+				$players[client_play_ind][:player].puts "Ok, you stand with #{$players[client_play_ind][:total]}."
+				$players[client_play_ind][:playing] = false
+			end
+			client_play_ind += 1
+		end
+	end
 
 
+	binding.pry
+	$players[0] = {
+		hand: [],
+		total: 0,
+		playing: true,
+		busted: false,
+		player: "dealer"
+	}
+
+	client_generator = 1
+	while client_generator < number_of_players
+		$players[client_generator][:hand] = []
+		$players[client_generator][:total] = 0
+		$players[client_generator][:playing] = true
+		$players[client_generator][:busted] = false
+		client_generator += 1
+end
+
+	
+
+
+
+end
 
 
 
