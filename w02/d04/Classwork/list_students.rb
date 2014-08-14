370e67b78b15ab5178ff5e8e0379568ab7409226
clay-reed-a <clay.reed.a@gmail.com>
2014-08-14 15:59:54 -0400

    format date, ANSI color codes blah blah blah blah

--- w02/d04/Classwork/list_students.rb
+++ w02/d04/Classwork/list_students.rb
@@ -1,5 +1,19 @@
+require 'time'
 eval File.read("./students.rb")  
 
+class String 
+	def blue
+		"\033[1;34m" + self + "\033[0m"
+	end 
+
+	def purple 
+		"\033[1;35m" + self + "\033[0m"
+	end 
+end 
+
 $students.each do |student|
-	puts "#{student["login"]} was created at #{student["created_at"]}"
+	t_obj = Time.parse student["created_at"]
+	t = t_obj.strftime "%A, %B %_d, %Y"
+	n = student["login"] 
+	puts "#{n.blue} was created on #{t.purple}."
 end 
