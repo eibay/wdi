81f5cd354c1d1cbbe1e69a2f85ceb139e8439559
clay-reed-a <clay.reed.a@gmail.com>
2014-08-15 15:15:35 -0400

    back on track

--- w02/d05/Classwork/parsing.rb
+++ w02/d05/Classwork/parsing.rb
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-url = "/the_forest?animal=monkeys&food=bananas"
-
-def parse_url(url)
-
-  path = url.split("?")[0]
-  query_string = url.split("?")[1]
-
-=======
 require 'pry'
 
 url = "/the_forest?animal=monkeys&food=bananas"
@@ -17,7 +8,6 @@ def parse_url(url)
 
   params = {}
   params[:path] = path
->>>>>>> 8a8c1869cba4449d527bf1f17f53f92404567841
 
   pairs = query_string.split("&")
 
@@ -33,20 +23,13 @@ def parse_url(url)
     query_params[key_value[0].to_sym] = key_value[1]
   end
 
-<<<<<<< HEAD
   params = {
     path: path,
     query_params: query_params
   }
-=======
-  params[:query_params] = query_params
->>>>>>> 8a8c1869cba4449d527bf1f17f53f92404567841
 
   return params
 end
-
-<<<<<<< HEAD
-puts parse_url(url)
 
 # Take ^, and turn into V
 
@@ -57,6 +40,5 @@ puts parse_url(url)
 #     food: "bananas"
 #   }
 # }
-=======
+
 puts parse_url(url)
->>>>>>> 8a8c1869cba4449d527bf1f17f53f92404567841
