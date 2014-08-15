1b31b79bae800ae33e5fea07f9b725238c3dba64
JBiggs91 <joe.biggica@gmail.com>
2014-08-14 21:23:05 -0400

    w02d04 hw

---
+++ w02/d04/Classwork/99bottles.rb
@@ -0,0 +1,22 @@
+# bottles = [*0..99]
+
+# i = 98
+
+# while i > 0
+
+# 	puts "#{bottles[i]} bottles of beer on the wall"
+# 	puts "#{bottles[i]} bottles of beer"
+# 	puts "Now there are #{bottles[i-1]}"
+
+# 	i-=1
+# end
+
+
+names = ["Jeff", "Sean", "Neel"]
+
+names.each do |x|
+	puts x 
+	
+end
+
+
--- w02/d04/Homework/starter/server.rb
+++ w02/d04/Homework/starter/server.rb
@@ -1,4 +1,6 @@
 require 'socket'
+require 'json'
+require 'pry'
 
 server = TCPServer.new 2000
 
@@ -6,13 +8,43 @@ server = TCPServer.new 2000
 loop do
 
   client = server.accept
+  puts "At #{Time.now.strftime("%I:%M:%S%P")} - Client connected"
 
   request = client.gets.chomp
   path = request.split(" ")[1]
+  puts "At #{Time.now.strftime("%I:%M:%S%P")} - Client requested #{path}"
 
   if path == "/"
     html = File.read('./views/index.html')
     client.puts(html)
+#Connecting with the API
+  elsif path.split("/")[1] == "words"
+    omdb_api = TCPSocket.new "www.omdbapi.com", 80
+
+    title = path.split("/")[2]
+
+    omdb_api.puts "GET /?s=#{title}"
+    puts "At #{Time.now.strftime("%I:%M:%S%P")} - Connected to OMDb"
+
+    response = JSON.parse(omdb_api.gets)
+
+    omdb_api.close
+    puts "At #{Time.now.strftime("%I:%M:%S%P")} - Disconnected from OMDb"
+#Disconnecting from the OMDb server
+
+    movie_list = response["Search"]
+
+    movie_list.each do |x|
+      
+      client.puts "<h1>#{x["Title"]}</h1>"
+      client.puts "<html><ul>"
+      client.puts "<li><h2>Released in #{x["Year"]}</h2></li>"
+      client.puts "<li><h2><a href='#{x["imdbID"]}'>IMDb Page</a></h2></li>"
+      client.puts "</ul></html>"
+    end
+
+    puts "At #{Time.now.strftime("%I:%M:%S%P")} - Sent title search to client"
+
   elsif path == "/styles.css"
     css = File.read('./stylesheets/styles.css')
     client.puts(css)
