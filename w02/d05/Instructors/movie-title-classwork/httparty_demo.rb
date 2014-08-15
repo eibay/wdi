f0714efab2da1e0cd76d50548aa3b79b369417e3
jkonowitch <jeff.konowitch@generalassemb.ly>
2014-08-15 15:27:58 -0400

    updates

--- w02/d05/Instructors/movie-title-classwork/httparty_demo.rb
+++ w02/d05/Instructors/movie-title-classwork/httparty_demo.rb
@@ -1,6 +1,15 @@
+word = "purrfect"
+
+# Instead of:
+# require 'socket'
+
+# omdbapi = TCPSocket.new 'www.omdbapi.com', 80
+# omdbapi.puts "GET /?s=#{word}"
+# response = omdbapi.gets
+# omdbapi.close
+
 require 'json'
 require 'httparty'
 
-word = "purrfect"
 response = HTTParty.get("http://www.omdbapi.com/?s=#{word}")
 puts JSON.parse(response)
