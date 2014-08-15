866d504f9bbb720a70bf20fdf9f3660267157e01
clay-reed-a <clay.reed.a@gmail.com>
2014-08-15 15:19:34 -0400

    floors

--- w02/d05/Classwork/movie-title-classwork/server.rb
+++ w02/d05/Classwork/movie-title-classwork/server.rb
@@ -47,41 +47,43 @@ loop do
   params = parse_url(path)
   puts params 
 
-  if path == "/"
+  if params[:path] == "/"
     html = File.read('./views/index.html')
     client.puts(html)
-  elsif path == "/styles.css"
+  elsif params[:path] == "/styles.css"
     css = File.read('./stylesheets/styles.css')
     client.puts(css) 
-  elsif params.include? :query_params
-    if params[:query_params].include? :specific_word 
-      word = params[:query_params][:specific_word]
+  elsif params[:path] == "/words"
+    if params.include? :query_params
+      if params[:query_params].include? :specific_word 
+        word = params[:query_params][:specific_word]
 
-      omdbapi = TCPSocket.new 'www.omdbapi.com', 80
-      omdbapi.puts "GET /?s=#{word}"
-      response = omdbapi.gets
-      omdbapi.close
-      parsed = JSON.parse(response)
+        omdbapi = TCPSocket.new 'www.omdbapi.com', 80
+        omdbapi.puts "GET /?s=#{word}"
+        response = omdbapi.gets
+        omdbapi.close
+        parsed = JSON.parse(response)
 
-      puts parsed 
+        puts parsed 
 
-      if parsed.include? "Error"
-        client.puts "<h1>There are no movies w/ \"#{word}\" in them</h1>"
-      else 
-        html = File.read('./views/movies.html')
-        html = html.gsub('{{search_word}}', word)
-        movies = []
+        if parsed.include? "Error"
+          client.puts "<h1>There are no movies w/ \"#{word}\" in them</h1>"
+        else 
+          html = File.read('./views/movies.html')
+          html = html.gsub('{{search_word}}', word)
+          movies = []
 
-        parsed["Search"].each do |movie|
-          individual_movie = File.read('./views/individual_movie.html')
-          individual_movie = individual_movie.gsub('{{title}}', movie["Title"])
-          individual_movie = individual_movie.gsub('{{year}}', movie["Year"])
-          individual_movie = individual_movie.gsub('{{imdb_id}}', movie["imdbID"])
-          movies.push(individual_movie)
-        end
+          parsed["Search"].each do |movie|
+            individual_movie = File.read('./views/individual_movie.html')
+            individual_movie = individual_movie.gsub('{{title}}', movie["Title"])
+            individual_movie = individual_movie.gsub('{{year}}', movie["Year"])
+            individual_movie = individual_movie.gsub('{{imdb_id}}', movie["imdbID"])
+            movies.push(individual_movie)
+          end
 
-        html = html.gsub('{{movies}}', movies.join(''))
-        client.puts(html)
+          html = html.gsub('{{movies}}', movies.join(''))
+          client.puts(html)
+        end 
       end 
     end 
   else
