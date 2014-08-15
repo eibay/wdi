1ab72044167337c5a11e06a57517607e2a810e7c
theerickramer <theerickramer@gmail.com>
2014-08-15 14:31:18 -0400

    Classwork w02 d05

--- w02/d05/Classwork/movie-title-solution/server.rb
+++ w02/d05/Classwork/movie-title-solution/server.rb
@@ -18,31 +18,22 @@ loop do
     css = File.read('./stylesheets/styles.css')
     client.puts(css)
   elsif path.split('/')[1] == "words"
-    query = path.split('/')[2]
-    word = query.split('=')[1]
+    word = path.split('/')[2]
 
     omdbapi = TCPSocket.new 'www.omdbapi.com', 80
-    omdbapi.puts "GET /#{query}"
+    omdbapi.puts "GET /?s=#{word}"
     response = omdbapi.gets
-    omdbapi.close
     parsed = JSON.parse(response)
 
-    html = File.read('./views/movies.html')
-    html = html.gsub('{{search_word}}', word)
-
-    movies = []
-    
+    list = []
     parsed["Search"].each do |movie|
-      individual_movie = File.read('./views/individual_movie.html')
-      individual_movie = individual_movie.gsub('{{title}}', movie["Title"])
-      individual_movie = individual_movie.gsub('{{year}}', movie["Year"])
-      individual_movie = individual_movie.gsub('{{imdb_id}}', movie["imdbID"])
-      movies.push(individual_movie)
+      list.push("<li>#{movie['Title']} ( #{movie['Year']} ) <a href='http://www.imdb.com/title/#{movie['imdbID']}'>See IMDB</a></li>")
     end
 
-    html = html.gsub('{{movies}}', movies.join(''))
-
+    html = File.read('./views/movies.html')
+    html = html.gsub('{{list}}', list.join('')).gsub("{{search_word}}", word)
     client.puts(html)
+
   else
     html = File.read('./views/404.html')
     client.puts(html)
