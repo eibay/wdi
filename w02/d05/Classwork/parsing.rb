d412dc6f537d5a339606573bc12c84ded15a69f1
jkonowitch <jeff.konowitch@generalassemb.ly>
2014-08-15 14:20:54 -0400

    classwork

--- w02/d05/Classwork/parsing.rb
+++ w02/d05/Classwork/parsing.rb
@@ -1,11 +1,31 @@
+require 'pry'
+
 url = "/the_forest?animal=monkeys&food=bananas"
 
-# Take ^, and turn into V
+def parse_url(url)
+  path = url.split("?")[0]
+  query_string = url.split("?")[1]
 
-# {
-#   path: "the_forest",
-#   query_params: {
-#     animal: "monkeys",
-#     food: "bananas"
-#   }
-# }
+  params = {}
+  params[:path] = path
+
+  pairs = query_string.split("&")
+
+  key_values = []
+
+  pairs.each do |pair|
+    key_values.push(pair.split("="))
+  end
+
+  query_params = {}
+
+  key_values.each do |key_value|
+    query_params[key_value[0].to_sym] = key_value[1]
+  end
+
+  params[:query_params] = query_params
+
+  return params
+end
+
+puts parse_url(url)
