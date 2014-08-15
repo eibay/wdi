596ce67d88997d36bc11652f286eadfc45fbe66b
crawfordforbes <crawford.forbes@gmail.com>
2014-08-14 21:03:30 -0400

    movies, now with drop shadow

---
+++ w02/d04/Classwork/movie_server.rb
@@ -0,0 +1,41 @@
+require 'socket'
+require 'pry'
+require 'json'
+
+server = TCPServer.new 2000
+
+while true
+
+	client = server.accept
+	client_ip = client.remote_address.ip_address
+
+	puts "#{Time.now} - Client has connected from #{client_ip}"
+
+	request = client.gets.chomp
+	path = request.split(' ')[1]
+	puts "#{Time.now} - Client #{client_ip} is attempting to reach #{path}"
+
+#	if path == '/inception'
+
+movie = path.split('/')[1]
+puts movie
+		omdb_api = TCPSocket.new 'www.omdbapi.com', 80
+		omdb_api.puts "GET /?t=#{movie}"
+
+		puts "#{Time.now} - Connecting to OMBD"
+
+		json_response = omdb_api.gets.chomp
+		parsed_response = JSON.parse(json_response)
+
+		omdb_api.close
+		puts "#{Time.now} - Disconnected from OMBD"
+
+		client.puts parsed_response
+		puts "#{Time.now} - Sent movie info to client #{client_ip}"
+
+
+
+	client.close
+	puts "#{Time.now} - Client #{client_ip} has disconnected"
+
+end
