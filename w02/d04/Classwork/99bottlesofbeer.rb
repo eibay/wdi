0761bf897af0686ea6bf3a412e54701790cb8475
im11041991 <i.martinez8@umiami.edu>
2014-08-15 00:49:12 -0400

    fixed

---
+++ w02/d04/Classwork/99bottlesofbeer.rb
@@ -0,0 +1,21 @@
+# nums= [*0..99]
+
+
+# index = 99
+# while index >= 0
+# 	puts "#{index} bottles of beer on the wall, #{index} bottles of beer, take one down pass it around"
+# 	index -= 1
+
+# end
+
+# index = 99
+
+# while index >= 0
+# 	puts "#{nums[index]} bottles of beer on the wall, #{nums[index]} bottles of beer, take one down pass it around"
+# 	index -= 1
+# end
+
+names = ["Jeff", "Sean", "Neel"]
+names.each do |a|
+	puts a 
+end
