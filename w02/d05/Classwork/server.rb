6d9a28de3f49afba20c436d7def294432e175bea
clay-reed-a <clay.reed.a@gmail.com>
2014-08-15 11:39:21 -0400

    parses it, but, surely, there must be a better way

--- w02/d05/Classwork/server.rb
+++ w02/d05/Classwork/server.rb
@@ -9,7 +9,10 @@ loop do
   client = server.accept
 
   request = client.gets.chomp
+
+  puts request 
   path = request.split(" ")[1]
+  puts path 
 
   if path == "/"
     html = File.read('./views/index.html')
@@ -17,31 +20,35 @@ loop do
   elsif path == "/styles.css"
     css = File.read('./stylesheets/styles.css')
     client.puts(css)
-  elsif path.split('/')[1] == "words"
-    word = path.split('/')[2]
+  elsif path.split('/')[1][0,7] == "words?s"
+    word = path.split('/')[1][8,path.length-1] 
 
     omdbapi = TCPSocket.new 'www.omdbapi.com', 80
     omdbapi.puts "GET /?s=#{word}"
     response = omdbapi.gets
     omdbapi.close
     parsed = JSON.parse(response)
+    if parsed.include? "Error"
+      client.puts "<h1>There are no movies w/ \"#{word}\" in them</h1>"
+    else 
 
-    html = File.read('./views/movies.html')
-    html = html.gsub('{{search_word}}', word)
+      html = File.read('./views/movies.html')
+      html = html.gsub('{{search_word}}', word)
 
-    movies = []
+      movies = []
     
-    parsed["Search"].each do |movie|
-      individual_movie = File.read('./views/individual_movie.html')
-      individual_movie = individual_movie.gsub('{{title}}', movie["Title"])
-      individual_movie = individual_movie.gsub('{{year}}', movie["Year"])
-      individual_movie = individual_movie.gsub('{{imdb_id}}', movie["imdbID"])
-      movies.push(individual_movie)
-    end
+      parsed["Search"].each do |movie|
+        individual_movie = File.read('./views/individual_movie.html')
+        individual_movie = individual_movie.gsub('{{title}}', movie["Title"])
+        individual_movie = individual_movie.gsub('{{year}}', movie["Year"])
+        individual_movie = individual_movie.gsub('{{imdb_id}}', movie["imdbID"])
+        movies.push(individual_movie)
+      end
 
-    html = html.gsub('{{movies}}', movies.join(''))
+      html = html.gsub('{{movies}}', movies.join(''))
 
-    client.puts(html)
+      client.puts(html)
+    end 
   else
     html = File.read('./views/404.html')
     client.puts(html)
