d5d82dbf01a3c9173401c54299807affb17b29b5
clay-reed-a <clay.reed.a@gmail.com>
2014-08-18 17:17:11 -0400

    style view, refractor script to not use external files

--- w03/d01/Classwork/script.rb
+++ w03/d01/Classwork/script.rb
@@ -4,6 +4,69 @@ require 'httparty'
 require 'socket'
 require 'erb'
 
+html = <<THE_HTML_VIEW
+<!DOCTYPE html>
+<html lang=en>
+<head>
+	<meta chaset=utf-8>
+	<title> You searched for a <%= $search %>... </title>
+	<style>
+		body {
+			background-color: white; 
+		}
+
+		h1 {
+			color: white;
+			text-align: center;
+			font-size: 3em;
+			padding: 0;
+			margin: 0;
+		}
+
+		h1 span {
+			color: red;
+		}
+
+		section {
+			border-radius: 25px; 
+			padding-top: .75em;
+			padding-bottom: .75em; 
+			width: 60%;
+			background-color: black;
+			margin: auto;
+		}
+
+		img {
+			display: block;
+			width: 90%;
+			margin: auto;
+			padding-bottom: 2.5em;
+		}
+
+		p {
+			text-align: center; 
+			color: white;
+			font-size: 3em;
+			width: 90%;
+			margin: auto;
+			margin-top: -.45em;
+		}
+
+	</style>
+</head>
+<body>
+	<section> 
+		<h1> You searched for a <span><%= $search %></span> </h1>
+		<% $recent_media.each do |photo| %>
+			<img src=<%= photo %> >
+		<% end %>
+		<p> Those were them. </p>
+	</section>
+</body>
+</html>
+THE_HTML_VIEW
+
+
 $access_token = "4ad7cc36c172434588afd340aa74cd01"
 port = 2000 
 
@@ -21,9 +84,7 @@ def get_standard_images results
 end 
 
 def send_view which 
-	path = "./views/#{which}/index.html.erb"
-	f = File.read path 
-	e = ERB.new f 
+	e = ERB.new which  
 	e.result binding 
 end 
 
@@ -31,18 +92,15 @@ $search = ARGV.last
 if ARGV.last.nil? 
 	abort "USAGE: pictures TAG_SEARCH_TERM" 
 else 
-
 	$recent_media = get_recent_media $search 
 	$recent_media = get_standard_images $recent_media
 
 	s = TCPServer.new port 
 	puts "Get yr browser out & go to localhost:#{port}"
-	loop do 
-		c = s.accept 
-		r = c.gets 
+	c = s.accept 
+	r = c.gets 
 
-		v = send_view "photos" 
-		c.puts v 
-		c.close 
-	end
+	v = send_view html 
+	c.puts v 
+	c.close 
 end  
