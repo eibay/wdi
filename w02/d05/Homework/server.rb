4318e1e9a7aaaef6241a60055569b7cb9eefa809
Nastassia <nastassiacarmona@gmail.com>
2014-08-18 09:54:23 -0400

    adding on to musicbrainz hw

--- w02/d05/Homework/server.rb
+++ w02/d05/Homework/server.rb
@@ -3,6 +3,7 @@ require 'pry'
 require 'json'
 require 'httparty'
 load 'parse_url.rb'
+# load 'local_mb_results.rb'
 
 server = TCPServer.new 2000
 
@@ -24,7 +25,7 @@ loop do
     html = File.read('./views/artists.html')
     query = params[:query_params][:artist]
 
-    mb_response = HTTParty.get("http://musicbrainz.org/ws/2/artist/?query=artist:'#{query}'&fmt=json")
+    mb_response = HTTParty.get("http://musicbrainz.org/ws/2/artist/?query=artist:'#{query}'&fmt=json", headers: {"User-Agent" => "HTTParty"})
     # binding.pry
 
     # mb_response["artist"].each do |x| client.puts x["name"] end
@@ -37,16 +38,21 @@ loop do
       artist_list = artist_list.gsub('{{id}}', x["id"])
       artist_list = artist_list.gsub('{{artist}}', x["name"])
       artists << artist_list
+      binding.pry
+
     end
         html = html.gsub('{{artist_result}}', artists.join(' '))
         client.puts (html)
-      elsif params[:path] == "/artist"
-        # binding.pry
 
-        html = File.read('./views/artist_detail.html')
+    elsif path.split('/')[1] == "/artist"
+        artist_info = File.read('./views/artist_detail.html')
+        id = params[:query_params]
         # binding.pry
-        detail = []
-        mb_response["artist"][]
+        mb_artist = HTTParty.get("http://musicbrainz.org/ws/2/artist/'#{id}'?inc=aliases&fmt=json", headers: {"User-Agent" => "HTTParty"})
+        # artist_info = artist_info.gsub('{{artist_result}}', mb_artist["artist"][])
+        client.puts (artist_info)
+
+
   end
 
 
