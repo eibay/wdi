10f0eaf38e3a5c29bee8af9517082392bfd4eec9
theerickramer <theerickramer@gmail.com>
2014-08-17 21:12:00 -0400

    Homework w02 d05

--- w02/d05/Classwork/movie-title-classwork/server.rb
+++ w02/d05/Classwork/movie-title-classwork/server.rb
@@ -4,31 +4,34 @@ require 'socket'
 
 server = TCPServer.new 2000
 
+# equivalent to while true
 def parse_url(url)
- 
   path = url.split("?")[0]
   query_string = url.split("?")[1]
- 
- 
+
+  params = {}
+  params[:path] = path
+
+  if query_string == nil
+    return params
+  end
+
   pairs = query_string.split("&")
- 
+
   key_values = []
- 
+
   pairs.each do |pair|
     key_values.push(pair.split("="))
   end
- 
+
   query_params = {}
- 
+
   key_values.each do |key_value|
     query_params[key_value[0].to_sym] = key_value[1]
   end
- 
-  params = {
-    path: path,
-    query_params: query_params
-  }
- 
+
+  params[:query_params] = query_params
+
   return params
 end
 
@@ -37,21 +40,20 @@ loop do
   client = server.accept
 
   request = client.gets
-  path = request.split(" ")[1]
-  params = parse_url(path)
+  url = request.split(" ")[1]
+  params = parse_url(url)
 
-  if path == "/"
+  if params[:path] == "/"
     html = File.read('./views/index.html')
     client.puts(html)
-  elsif path == "/styles.css"
+  elsif params[:path] == "/styles.css"
     css = File.read('./stylesheets/styles.css')
     client.puts(css)
-  elsif path.split('/')[1] == "words"
-    query = path.split('/')[2]
-    word = query.split('=')[1]
+  elsif params[:path] == "/words"
+    word = params[:query_params][:specific_word]
 
     omdbapi = TCPSocket.new 'www.omdbapi.com', 80
-    omdbapi.puts "GET /#{query}"
+    omdbapi.puts "GET /?s=#{word}"
     response = omdbapi.gets
     omdbapi.close
     parsed = JSON.parse(response)
--- w02/d05/Classwork/parsing.rb
+++ w02/d05/Classwork/parsing.rb
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-url = "/the_forest?animal=monkeys&food=bananas"
- 
-def parse_url(url)
- 
-  path = url.split("?")[0]
-  query_string = url.split("?")[1]
- 
- 
-  pairs = query_string.split("&")
- 
-  key_values = []
- 
-  pairs.each do |pair|
-    key_values.push(pair.split("="))
-  end
- 
-  query_params = {}
- 
-  key_values.each do |key_value|
-    query_params[key_value[0].to_sym] = key_value[1]
-  end
- 
-  params = {
-    path: path,
-    query_params: query_params
-  }
- 
-  return params
-end
- 
-puts parse_url(url)
- 
-# Take ^, and turn into V
- 
-# {
-#   path: "the_forest",
-#   query_params: {
-#     animal: "monkeys",
-#     food: "bananas"
-#   }
-# }
-=======
 require 'pry'
 
 url = "/the_forest?animal=monkeys&food=bananas"
@@ -72,4 +29,3 @@ def parse_url(url)
 end
 
 puts parse_url(url)
->>>>>>> 763a9c73690f4da1a4cd229046b20b6edb381d69
