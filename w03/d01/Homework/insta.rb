6e0255a042b7926c4111b16c33356967f0eac9dc
adrianmlin <adrian.mx.lin@gmail.com>
2014-08-18 23:53:02 -0400

    trying to link each image

--- w03/d01/Homework/insta.rb
+++ w03/d01/Homework/insta.rb
@@ -42,9 +42,9 @@ while true
 	client = server.accept
 p "client connected"
 	request = client.gets
-p "client is requesting #{request}"
+p "client is requesting request: #{request}"
 	url = request.split(" ")[1]
-p "client is attempting to reach #{url}"
+p "client is attempting to reach url: #{url}"
 	params = parse_url(url)
 
 
@@ -53,14 +53,15 @@ p "client is attempting to reach #{url}"
 
 	elsif params[:path] == "/photo"
 		searched_tag = params[:query_params][:tag]
-p "client searched for #{searched_tag}"
+p "client searched for searched_tag: #{searched_tag}"
 		ig_stuff = HTTParty.get("https://api.instagram.com/v1/tags/#{searched_tag}/media/recent?access_token=23131423.f59def8.a6672ba008ba4698bf9255c69b886261")
 		# ig_stuff = HTTParty.get("https://api.instagram.com/v1/tags/monkey/media/recent?access_token=23131423.f59def8.a6672ba008ba4698bf9255c69b886261")
 		# ig_stuff["data"][1]["images"]["low_resolution"]["url"]  ----- for the image link
-		
+p "client searched for ig_stuff['data']: #{ig_stuff["data"]}"
 		images_html = ""
 		ig_stuff["data"].each do |x|
-			image = "<li><a href='#{x["images"]["standard_resolution"]}'><img src='#{x["images"]["low_resolution"]["url"]}'/></a></li> "
+			image = "<li><a href='/picture_big/#{x["images"]["standard_resolution"]}'><img src='#{x["images"]["low_resolution"]["url"]}'/></a></li> "
+			# image = "<li><img src='#{x["images"]["low_resolution"]["url"]}'/></li> "
 			images_html += image
 		end
 
@@ -70,8 +71,12 @@ p "client searched for #{searched_tag}"
 
 		client.puts html
 
+		##### WHY CANT I LINK TO A ZOOMED IN PHOTO??" ####
+# 	elsif url.split("/")[1] == "picture_big"
+# 		image_link = url.split("/").split("http")[2]
+# 		image_link += ".jpg"
+# p "client searching for image_link: #{image_link}"
 
-		
 
 
 	elsif url == "/style"
