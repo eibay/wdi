939853a4d52d5d6c0c60fa99fa5d24b9f7fedf87
clay-reed-a <clay.reed.a@gmail.com>
2014-08-19 18:12:00 -0400

    remove global vars, use instance vars, make a view for the hashes of hearts

--- w03/d02/Homework/server.rb
+++ w03/d02/Homework/server.rb
@@ -6,9 +6,8 @@ require 'httparty'
 require 'yaml'
 
 s = TCPServer.new 2000
-$access_token = "4ad7cc36c172434588afd340aa74cd01"  
-$app_name = "Hash of Hearts"  
-$cloud = {}
+@app_name = "Hash of Hearts"  
+@cloud = {}
 
 def send_erb_view which  
 	f = "./views/#{which}/index.html.erb" 
@@ -18,11 +17,12 @@ def send_erb_view which
 end 
 
 
+@access_token = "4ad7cc36c172434588afd340aa74cd01"  
 def tagged so
 	# remove possible chars we don't want # 
 	so.delete! '+' 
 	so.delete! "%20"
-	url = "https://api.instagram.com/v1/tags/#{so}/media/recent?client_id=#{$access_token}"
+	url = "https://api.instagram.com/v1/tags/#{so}/media/recent?client_id=#{@access_token}"
 	HTTParty.get url 
 end 
  
@@ -55,24 +55,33 @@ loop do
 	r.parse c
 
 	if r.path == '/' && r.request_method == "GET" 
-		$view_name = "land"
-		v = send_erb_view $view_name
+		@view_name = "land"
+		v = send_erb_view @view_name
 		c.puts v 
 	elsif r.path == "/see" && r.request_method == "GET" 
-		$view_name = "see"
+		@view_name = "see"
 
-		$t = r.query["tag"]
-		$imgs = tagged $t
-		$imgs = transform $imgs 
-		$imgs.slice! 0, 9 
+		@t = r.query["tag"]
+		@imgs = tagged @t
+		@imgs = transform @imgs
+		@imgs.slice! 0, 9
 
-		v = send_erb_view $view_name
-
+		v = send_erb_view @view_name
 		c.puts v 
+
 	elsif r.path == "/cloud" && r.request_method == "POST"
-		$cloud[$tag] = $imgs
-		puts $cloud 
-		c.puts $cloud 
+		@view_name = "cloud"
+
+		@t = r.query["t"] 
+		@imgs = tagged @t 
+		@imgs = transform @imgs 
+		@imgs.slice! 0, 9 
+
+		@cloud[@t] = @imgs  
+
+		v = send_erb_view @view_name
+		c.puts v 
+
 	end 
 
 	c.close 
