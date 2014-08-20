4bec8752f891f59b7be51f0f512608ecd25d150c
yoshiemuranaka <yoshiemuranaka@gmail.com>
2014-08-20 09:02:52 -0400

    homework

--- w03/d02/Instructors/server-demo/server.rb
+++ w03/d02/Instructors/server-demo/server.rb
@@ -11,11 +11,13 @@ loop do
 
 	request = WEBrick::HTTPRequest.new(WEBrick::Config::HTTP)
 	request.parse(client)
+	binding.pry
 
 	if request.path == "/" && request.request_method == "GET"
 		html = File.read("./views/index.html"	)
 		client.puts html
 	elsif request.path == "/" && request.request_method == "POST"
+		binding.pry
 		puppy_name = request.body.split("=")[1]
 		puppies.push(puppy_name)
 		client.puts puppies.join(", ")
