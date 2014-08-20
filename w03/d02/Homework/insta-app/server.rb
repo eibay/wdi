62c16425b3b5bc0c81af5cd9c9673f50b6465572
JBiggs91 <joe.biggica@gmail.com>
2014-08-20 10:44:52 -0400

    update

--- w03/d02/Homework/insta-app/server.rb
+++ w03/d02/Homework/insta-app/server.rb
@@ -1,10 +1,12 @@
 require 'webrick'
 require 'httparty'
 require 'pry'
+require 'uri'
 
 server = TCPServer.new 2000
 
 saved_searches = []
+saved_photos = []
 
 loop do
 
@@ -18,7 +20,7 @@ loop do
 	if request.path == "/"
 		html = File.read("./views/main.html")
 		client.puts(html)
-	elsif request.path == "/photos" && request.request_method == "GET"
+	elsif request.path == "/photos"
 # binding.pry
 		tag = request.query["tag_search"]
 		response = HTTParty.get("https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=4ad7cc36c172434588afd340aa74cd01")
@@ -26,24 +28,37 @@ loop do
 		image_str = ""
 		response["data"].each do |i|
 			image_html = File.read("./views/image_list.html")
-			image_html = image_html.gsub("{{images}}", i["images"]["standard_resolution"]["url"])
+			image_html = image_html.gsub("{{images}}", i["images"]["standard_resolution"]["url"].to_s)
 			image_str += image_html
 		end
-
+# binding.pry
 		image_page_html = File.read("./views/image_page.html")
 		image_page_html = image_page_html.gsub("{{image_str}}", image_str)
 		image_page_html = image_page_html.gsub("{{search_tag}}", tag)
 		client.puts image_page_html
 	
-	elsif request.path == "/photos" && request.request_method == "POST" 
-		tag = request.body.split("=")[0]
+	elsif request.path == "/saved_tags"
+		tag = request.body.split("=")[1]
 		saved_searches.push(tag)
 
 		saved_html = File.read("./views/saved.html")
-		saved_html = saved_html.gsub("{{saved_tags}}", saved_searches.to_s)
+		saved_html = saved_html.gsub("{{saved_data}}", saved_searches.to_s)
 		client.puts saved_html
 
 # binding.pry
+
+	elsif request.path =="/saved_photos"
+# binding.pry
+	
+	photo = URI.decode(request.body.split("=")[1])
+	saved_photos.push(photo)
+
+	saved_html = File.read("./views/saved_photos.html")
+	saved_html = saved_html.gsub("{{saved_data}}", saved_photos.to_s)
+	client.puts saved_html
+
+
+
 
 
 
