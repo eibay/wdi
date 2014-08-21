fd6c4b2fa84fae6d8e72186094d9dae7c6dee342
tdshap <Sue@TessShapiro.local>
2014-08-21 09:39:33 -0400

    Instagram api with sinatra

--- w03/d03/Classwork/hello-sinatra-erb/server.rb
+++ w03/d03/Classwork/hello-sinatra-erb/server.rb
@@ -4,7 +4,7 @@ get("/") do
   erb(:index)
 end
 
-get("/name") do
+post("/name") do
   name = request.params["first_name"]
   erb(:name, { locals: { name: name } })
 end
@@ -14,3 +14,4 @@ numbers = [3, 23, 7, 57]
 get("/fun") do
   erb(:fun, { locals: { favorite_numbers: numbers, time: Time.now } })
 end
+
