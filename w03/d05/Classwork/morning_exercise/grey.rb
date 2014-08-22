5c2e62386b0f44a81c7bfea9a73e65085aec6a2a
jortenberg <jill.ortenberg@gmail.com>
2014-08-22 13:55:17 -0400

    classwork

--- w03/d05/Classwork/morning_exercise/grey.rb
+++ w03/d05/Classwork/morning_exercise/grey.rb
@@ -7,18 +7,37 @@ hash = grey_string.split("\n")
 
 
 
-hash_real = {}
+# hash_real = {}
 array = []
+another_array = []
 
 hash.each do |item|
 	array.push(item.split(","))
 end
 
-binding.pry
+h = {}
 
-hash_real = { "actor" => array[0],
-  "role" => array[1],
-  "episodes" => array[2],
-  "year" => array[3]
-}
+array.each do |item|
+   
+
+end
+
+
+
+h["name"] = "sean"
+h["age"] = 23
+
+
+h
+=> {"name"=>"sean", "age"=>23}
+# "ways to build up a hash"
+
+1st way:
+   myhash = {"name"=>"sean", "age"=>23}
+
+2nd way:
+	myhash = {}
+	myhash["name"] = "sean"
+	myhash["age"] = 23
+
 
