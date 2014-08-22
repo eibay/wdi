c8651b82bd4dbe9a4a5509c05e044114ae87bda9
Nastassia <nastassiacarmona@gmail.com>
2014-08-22 13:55:13 -0400

    Commiting the rest of morning_exercise, finish last two specs

--- w03/d05/Classwork/morning_exercise/greys.rb
+++ w03/d05/Classwork/morning_exercise/greys.rb
@@ -6,13 +6,18 @@ characters = []
 char_file.each do |x|
   char_hash = {}
   character = x.split(",")
+  years = character[3]
   character[3] = character[3].delete("\n")
   char_hash["character"] = character[1]
   char_hash["actor"] = character[0]
-  char_hash["episodes"] = character[2]
-  char_hash["years"] = character[3]
-  char_hash["start_year"] = char_hash["years"].split("-")[0]
-  char_hash["end_year"] = char_hash["years"].split("-")[1]
+  char_hash["episodes"] = character[2].to_i
+  if years.include?("-")
+    char_hash["start_year"] = years.split("-")[0].to_i
+    char_hash["end_year"] = years.split("-")[1].to_i
+  else
+    char_hash["start_year"] = years.to_i
+    char_hash["end_year"] = years.to_i
+  end
   characters << char_hash
 end
 
@@ -32,6 +37,24 @@ characters.each do |x|
     minor << x
   end
 end
+
+doctors = []
+
+characters.each do |x|
+  if x["character"].include?("Dr.")
+    doctors << x
+  end
+end
+
+the_year = []
+
+characters.each do |x|
+  if x["start_year"] <= 2007 && x["end_year"] >= 2007
+    the_year << x["character"]
+  end
+end
+
+
 puts
 puts "Stars"
 puts "-------"
@@ -41,4 +64,7 @@ puts
 puts "Number of Minor Peeps"
 puts "----------------------"
 puts minor.length
-# puts characters
+puts
+puts "The Doctors"
+puts "------------"
+puts doctors
