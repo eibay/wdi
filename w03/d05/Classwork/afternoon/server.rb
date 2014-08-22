1aca7d573f228443e64410f95c3fd6adbc68a708
trishfromtisch <parlaws@gmail.com>
2014-08-22 15:17:25 -0400

    afternoon class

--- w03/d05/Classwork/afternoon/server.rb
+++ w03/d05/Classwork/afternoon/server.rb
@@ -1,14 +1,26 @@
 require 'json'
 require 'sinatra'
+require 'sinatra/reloader'
+require "pry"
 
 get("/") do
+
+erb(:index)
 end
 
 
 post("/students") do
-  
+  first_name = params["first"]
+  last_name = params["last"]
+  email = params["email"]
+
+  person = {"first" => first_name, "last" => last_name, "email"=> email}
+  students = JSON.parse(File.read("./students.txt"))
+  students << person
+  File.write('./students.txt', students.to_json)
 end
 
 get("/students") do
+	erb(:students)
 end
 
