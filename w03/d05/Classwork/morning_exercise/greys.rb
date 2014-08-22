235a4631d8db06cbdc5406b435e0eb9d32cbc429
DanielFarber <farber.daniel.m@gmail.com>
2014-08-22 12:05:17 -0400

    afternoon's assignment

--- w03/d05/Classwork/morning_exercise/greys.rb
+++ w03/d05/Classwork/morning_exercise/greys.rb
@@ -11,27 +11,28 @@ str.split("\n").each do |data|
 	cast << {
 		actor: actor[0],
 		character: actor[1],
-		episodes: actor[2]
+		episodes: actor[2].split(" ")[0].to_i
 	}
 	if actor[3].include?("-")
-		cast[index][:start_year] = actor[3].split("-")[0]
-		cast[index][:end_year] = actor[3].split("-")[1]
+		cast[index][:start_year] = actor[3].split("-")[0].to_i
+		cast[index][:end_year] = actor[3].split("-")[1].to_i
 	else
-		cast[index][:start_year] = actor[3]
+		cast[index][:start_year] = actor[3].to_i
+		cast[index][:end_year] = actor[3].to_i
 	end
 	index += 1
 end
 
 greater_than = []
 cast.each do |actor|
-	if actor[:episodes].scan(/\d+/)[0].to_i > 150
+	if actor[:episodes] > 150
 		greater_than << actor[:actor]
 	end
 end
 
 less_than = []
 cast.each do |actor|
-	if actor[:episodes].scan(/\d+/)[0].to_i < 10
+	if actor[:episodes] < 10
 		less_than << actor[:actor]
 	end
 end
@@ -44,8 +45,8 @@ end
 aught_seven = []
 
 cast.each do |actor|
-	aught_seven << actor[:character] if actor[:start_year] == "2007"
-	if actor[:start_year].to_i < 2007 && actor[:end_year].to_i >= 2007
+	aught_seven << actor[:character] if actor[:start_year] == 2007
+	if actor[:start_year] < 2007 && actor[:end_year] >= 2007
 		aught_seven << actor[:character]
 	end
 end
