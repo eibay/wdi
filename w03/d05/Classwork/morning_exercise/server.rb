ea67ad26797b2c684d509e694510f01a21c74245
crawfordforbes <crawford.forbes@gmail.com>
2014-08-22 13:55:13 -0400

    greys anatomy

--- w03/d05/Classwork/morning_exercise/server.rb
+++ w03/d05/Classwork/morning_exercise/server.rb
@@ -1,26 +1,77 @@
 require 'pry'
-epiArr = []
+all_actors = []
+stars = []
+minor = []
+docs = []
+y2k = []
 csv = File.read('./greys.txt')
-charArr = []
-binding.pry
-csv.split("\n").each { 
-	|x| charArr<<{character: x.split(",")[0], actor: x.split(",")[1], episodes: x.split(",")[2].split(" ")[0].to_i,  
-		# (if x.split(",")[3].length == 4 
-		# 	start_year: x.split(",")[3],
-		# 	end_year: x.split(",")[3] 
-		# else start_year: x.split(",")[3].split("-")[0]
-		# 	end_year: x.split(",")[3].split("-")[1] 
-		# end)
-	}
-}
+
+file_array = csv.split("\n")
+
+file_array.each do |actor_string|
+	actor_array = actor_string.split(",")
+	hash = {}
+	hash[:actor] = actor_array[0]
+	hash[:character] = actor_array[1]
+
+	episode_split = actor_array[2].split(" ")
+	hash[:episodes] = episode_split[0].to_i
+
+	if actor_array[3].include?("-")
+		dates = actor_array[3].split("-")
+		hash[:start_year] = dates[0].to_i
+		hash[:end_year] = dates[1].to_i
+	else
+		hash[:start_year] = actor_array[3].to_i
+		hash[:end_year] = actor_array[3].to_i
+	end
+	all_actors.push(hash)
+end
+
 puts "stars, minor, docs, 2007"
 command = gets.chomp.downcase
 if command == "stars"
-	binding.pry
-	charArr[0].each do |x| 
-		if x[:episodes].split(" ")[0].to_i > 150
-			epiArr<<x[:actor]
+	all_actors.each do |actor|
+		if actor[:episodes] > 150
+			stars.push(x[:actor])
 		end
 	end
+	puts stars
 end
-	puts epiArr
+
+if command == "minor"
+	all_actors.each do |x|
+		if x[:episodes] < 10
+			minor.push(x[:actor])
+		end
+	end
+	puts minor.length
+end
+
+if command == "docs"
+	all_actors.each do |x|
+		if x[:character].include?("Dr.")
+			docs.push(x[:character])
+		end
+	end
+	puts docs
+end
+
+if command == "2007"
+	all_actors.each do |x|
+		if ((x[:start_year] < 2007) && (x[:end_year] >= 2007)) || (x[:start_year] == 2007)  
+			y2k.push(x[:character])
+		end
+	end
+	puts y2k.length
+end
+
+
+
+
+
+
+
+
+
+
