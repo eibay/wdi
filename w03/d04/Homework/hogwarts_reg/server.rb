2e755cbdd48962c4ee68d3eeab7e6cc96f9b6c60
JBiggs91 <joe.biggica@gmail.com>
2014-08-22 01:07:14 -0400

    hogwarts reg

---
+++ w03/d04/Homework/hogwarts_reg/server.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'pry'
+require 'uri'
+
+students = []
+
+get('/students/new') do 
+	erb(:signup_page)
+end
+
+post('/') do 
+	student_info = {}
+
+	name = URI.decode(params["name"].upcase)
+	age = params["age"]
+	spell = params["spell"].upcase
+
+	student_info[:name] = name
+	student_info[:age] = age
+	student_info[:spell] = spell
+
+	students << student_info
+
+	erb(:main, {locals: { students: students }})
+end
+
+get('/') do 
+	erb(:main, {locals: { students: students }})
+end
+
+get('/students/show/:hodor') do 
+
+	name = URI.decode(request.path.split("/")[3])
+
+	erb(:show_page, {locals: { students: students, name: name }})
+end
+
+get('/search') do 
+	result = params["search"].upcase
+	erb(:search_results, {locals: { students: students, result: result}})
+end
