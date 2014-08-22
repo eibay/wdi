24cd086548ff29204687022245ed3a7635e709b0
JBiggs91 <joe.biggica@gmail.com>
2014-08-22 12:02:41 -0400

    update

--- w03/d05/Classwork/morning_exercise/character.rb
+++ w03/d05/Classwork/morning_exercise/character.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
-character_array = File.read("./greys.txt")
+character_str = File.read("./greys.txt")
 
 array = []
- character_array.split("\n").each do |x|
+ character_str.split("\n").each do |x|
  	hash = {}
  	hash[:actor] = x.split(',')[0]
  	hash[:character] = x.split(',')[1]
- 	hash[:episodes] = x.split(',')[2]
+ 	hash[:episodes] = x.split(',')[2].split(' ')[0].to_i
+
  	year = x.split(',')[3]
  	puts hash[:actor]
  	if year.length == 4
@@ -17,14 +18,52 @@ array = []
  		hash[:start_year] = year.split('-')[0].to_i
  		hash[:end_year] = year.split('-')[1].to_i
  	end
- 	array << hash
  	
+ 	array << hash
  end
 
+# Stars
+# 
+onefifty_array = []
+array.each do |y|
+	if y[:episodes] > 150
+		onefifty_array << y
+	end
+end
 
+# Minor peeps
+# 
+peeps_array = []
+array.each do |z|
+	if z[:episodes] < 10
+		peeps_array << z
+	end
+end
+less_than_10 = peeps_array.length
 
+# Dr. Please!
+# 
+dr_array = []
+array.each do |hodor|
+	if hodor[:character].split(" ")[0] == "Dr."
+		dr_array << hodor
+	end
+end
 
+# 2007
+# 
+oseven_array = []
+oseven_characters = []
 
+array.each do |a|
+	if a[:start_year] <= 2007 && a[:end_year] >= 2007
+		oseven_array << a	
+	end
+end
+
+oseven_array.each do |b|
+		oseven_characters << b[:character]
+end
 
 
 
