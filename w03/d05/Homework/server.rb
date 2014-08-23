d9f147ef6a27f0bae87712f0e050cc439c8aa315
clay-reed-a <clay.reed.a@gmail.com>
2014-08-23 15:35:39 -0400

    fix bug where patients.db would be a ruby hash instead of json

--- w03/d05/Homework/server.rb
+++ w03/d05/Homework/server.rb
@@ -14,19 +14,20 @@ get "/appointments" do
 end 
 
 post "/appointments" do 
-	patient = {}
-
-	patient["first_name"] = params["first_name"]
-	patient["last_name"] = params["last_name"]
-	patient["birth_year"] = params["birth_year"]
-	patient["birth_year"] = params["condition"]
-	patient["date_admitted"] = Date.today.to_s 
+	
+	patient = {
+		"first_name" => params["first_name"], 
+		"last_name" => params["last_name"], 
+		"birth_year" => params["birth_year"], 
+		"condition" => params["condition"],
+		"date_admitted" => Date.today.to_s  
+	}
 
 	f = File.read "./patients.db"
 	patients = JSON.parse f 
 	patients << patient 
 
-	File.write "./patients.db", patients 
+	File.write "./patients.db", patients.to_json 
 
 	redirect '/'
 end 
