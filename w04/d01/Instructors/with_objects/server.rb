29bee0a13840d405bc476ff74793d8989122ed23
jkonowitch <jeff.konowitch@generalassemb.ly>
2014-08-25 13:48:47 -0400

    with objects

---
+++ w04/d01/Instructors/with_objects/server.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+require_relative './lib/student'
+
+get("/") do
+	students = Student.all()
+  
+	erb(:index, { locals: { students: students} })
+end
+
+
+post("/students") do
+  first_name = params["first"]
+  last_name = params["last"]
+  email = params["email"]
+
+  person = {"first" => first_name, "last" => last_name, "email" => email}
+
+  Student.create(person)
+
+  erb(:index, {locals: { students: Student.all() } })
+end
+
+get("/students/new") do
+	erb(:new)
+end
+
+get("/students/:first_name") do 
+  student = Student.find_by("first", params[:first_name])
+
+  erb(:student, { locals: { student: result} })
+end
