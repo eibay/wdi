50fbea0b7d5851f2c3a2bf6f24241a6fb4b2f987
neelp22 <neelp22@gmail.com>
2014-08-26 13:45:01 -0400

    adding hw review

--- w04/d02/Instructors/morning_review/lib/student.rb
+++ w04/d02/Instructors/morning_review/lib/student.rb
@@ -1,7 +1,10 @@
 require 'json'
+require 'pry'
+require 'securerandom'
 
 class Student
   def self.create(student)
+    student["id"] = SecureRandom.hex
     students = self.all()
     students.push(student)
     students_json = JSON.generate(students)
@@ -11,6 +14,12 @@ class Student
   def self.find_by(key, value)
     self.all().find do |student|
       student[key] == value
+    end
+  end
+
+  def self.select_by(key, value)
+    self.all().select do |student|
+      student[key].downcase == value.downcase
     end
   end
 
