857f3d4301dbae2976661b6242239ca6d28c2700
clay-reed-a <clay.reed.a@gmail.com>
2014-08-26 16:36:42 -0400

    add some of the views in

--- w04/d02/Homework/server.rb
+++ w04/d02/Homework/server.rb
@@ -9,11 +9,12 @@ get '/' do
 	# navigation 
 	# description of services 
 
+	erb :index
 end 
 
 get "/shuttles/new" do 
 	# navigation 
-	# form to create a new shuttle 
+	# form to create a new shuttle
 
 end 
 
@@ -27,22 +28,26 @@ get "/astronauts" do
 	# navigation 
 	# list all astronauts 
 
+	erb :astronauts
 end 
 
 get "/shuttles" do 
 	# navigation 
 	# list all shuttles 
 
+	erb :shuttles
 end 
 
 get "/astronauts/:astronaut_id" do 
 	# navigation 
 	# an astronaut profile 
 
+	erb :astronaut 
 end 
 
 get "/shuttles/:shuttle_id" do
 	# navigation 
 	# a shuttle profile 
 
+	erb :shuttle
 end 
