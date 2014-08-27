92947edb33838a1d4c85a9dd7dfd319b1f3e32ab
im11041991 <i.martinez8@umiami.edu>
2014-08-27 09:55:20 -0400

    did my hw

---
+++ w04/d02/Homework/lib/astronauts.rb
@@ -0,0 +1,31 @@
+require 'securerandom'
+require 'json'
+
+class Astronaut
+	def self.create (astronaut)
+		astronauts = self.all()
+
+		astronaut["id"] = SecureRandom.hex
+		astronauts.push(astronaut)
+
+		astronauts_json = JSON.generate(astronauts)
+
+		File.write('./astronauts.txt', astronauts_json)
+	end
+
+	def self.find_by(key, value)
+		self.all().find do |astronaut|
+			astronaut[key] == value
+		end
+	end
+
+	def self.select_by(key, value)
+		self.all().select do |astronaut|
+			astronaut[key] == value
+		end
+	end
+
+	def self.all()
+		return JSON.parse(File.read('./astronauts.txt'))
+	end
+end
