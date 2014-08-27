eea4bc00fc15523edd8aed071906255ede146ce2
wuuk <alexfong@hotmail.com>
2014-08-27 13:51:46 -0400

    afteroon

---
+++ w04/d03/Classwork/Instance_method/afternoon_comedians.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require_relative './comedian'
+
+comedian1 = Comedian.new("Benny Hill", "Q: What gets wetter the more it dries? A: A towel") )
+comedian2 = Comedian.new("George Carlin", "Q: What do lawyers wear to court? A: Lawsuits!")
+comedian3 = Comedian.new("Amy Schumer", "Crap")
+
+
+
+
+
+
---
+++ w04/d03/Classwork/Instance_method/comedian.rb
@@ -0,0 +1,51 @@
+#create a class call 'Comedian'
+#Give it a joke method that willputs your favorite joke
+#Give comedian the attributes - name, age fav_joke
+
+#think about the behavoir and how we want it to work
+
+
+class Comedian
+
+	def initialize(name, age, fav_joke)
+		#set local varibles to instance variables
+		@name = name 
+
+		@age = age
+
+		@fav_joke = fav_joke
+
+		 # puts "What did thebuffalo say on the first day school?"
+		 # puts "Bison"
+		 # puts joke
+	end
+
+	def tell_joke
+
+		puts "Hello! @fav_joke
+
+	end
+
+
+end
+
+
+
+# class Comedian
+
+# 	def self.joke
+
+# 		puts "DO NOT READ THE NEXT SENTENCE.
+# 					You little rebel, I like you."
+
+# 	end
+
+
+	
+# 	def joke
+
+# 		puts "Teacher: What do you call a person who keeps on talking when people are no longer interested?
+# 					Student: A teacher!"
+
+# 	end
+# end
