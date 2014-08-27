3683bf97c702c0de3c99e2b10fdaae9c1b390f90
neener <janinemrosen@gmail.com>
2014-08-27 13:46:30 -0400

    huh

---
+++ w04/d03/Classwork/instance_methods_from_instructors/comedians.rb
@@ -0,0 +1,16 @@
+Class Comedian
+
+comedians=[]
+
+def initialize(comedian, joke)
+		@comedian = comedian
+		@joke = joke
+	end
+
+	def tell_joke
+		puts @joke
+	end
+
+end
+
+#nope don't get it thanks
