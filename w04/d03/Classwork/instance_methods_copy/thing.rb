dbd19b2970cbe6ca35e1e532586ede3f2e94b94b
jortenberg <jill.ortenberg@gmail.com>
2014-08-27 13:53:20 -0400

    blah

---
+++ w04/d03/Classwork/instance_methods_copy/thing.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require_relative './comedian'
+
+
+
+comedians = []
+
+sam = Comedian.new("Sam", 34, "blah ming")
+comedians.push(sam)
+george = Comedian.new("George", 30, "ching chong ha")
+comedians.push(george)
+wilma = Comedian.new("Wilma", 27, "bling bling")
+comedians.push(wilma)
+
+binding.pry
--- w04/d03/Classwork/thing.rb
+++
@@ -1,5 +0,0 @@
-require 'pry'
-require_relative './comedian'
-require_relative './dog'
-
-binding.pry
