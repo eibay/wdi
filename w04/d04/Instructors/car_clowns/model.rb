0e400cb31ccdd8a40ad6bb9fed066d0542149484
jkonowitch <jeff.konowitch@generalassemb.ly>
2014-08-28 00:18:37 -0400

    enhance

--- w04/d04/Instructors/car_clowns/model.rb
+++ w04/d04/Instructors/car_clowns/model.rb
@@ -24,10 +24,31 @@ class Model
   end
 
   def self.all()
-    return JSON.parse(File.read(self.file_name()))
+    array_of_hashes = JSON.parse(File.read(self.file_name()))
+    instances = []
+
+    array_of_hashes.each do |entity|
+      instances << self.new(entity)
+    end
+
+    return instances
   end
 
   def self.file_name()
     self.name().downcase + "s.txt"
+  end
+
+  # Instance methods
+
+  def initialize(attributes)
+    @attributes = attributes
+  end
+
+  def to_json
+    @attributes.to_json
+  end
+
+  def [](key)
+    return @attributes[key]
   end
 end
