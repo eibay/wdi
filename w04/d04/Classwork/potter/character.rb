3563721ca511475892be2f6fec951eb8fb86c5b2
pateltejal9500 <pateltejal9500@gmail.com>
2014-08-28 19:59:14 -0400

    homework

---
+++ w04/d04/Classwork/potter/character.rb
@@ -0,0 +1,9 @@
+require_relative './model'
+
+class Character < Model
+
+  def house
+    House.find_by("id", @attributes["house_id"])
+  end
+
+end
---
+++ w04/d04/Classwork/potter/house.rb
@@ -0,0 +1,9 @@
+require_relative './model'
+
+class House < Model
+
+  def character
+    Character.select_by("house_id", @attributes["id"])
+  end
+
+end
