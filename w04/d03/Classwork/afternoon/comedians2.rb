ea3c93c3d49f03415f783e857299ae066bbb2e04
theerickramer <theerickramer@gmail.com>
2014-08-27 22:54:45 -0400

    Homework w04 d03

--- w04/d03/Classwork/afternoon/comedians2.rb
+++ w04/d03/Classwork/afternoon/comedians2.rb
@@ -18,7 +18,6 @@ comedians = [mitch = Comedian.new({name: "Mitch", age: 37, joke: "I used to do d
 			richard = Comedian.new({name: "Richard", age: 50, joke: "I had to stop drinking, cause I got tired of waking up in my car driving 90."}),
 			tommy = Comedian.new({name: "Tommy", age: 40, joke: "I’m on a whiskey diet…I’ve lost three days already."})]
 
-
 # comedians.each {|comedian| puts "#{comedian.getInfo[:name]}, age #{comedian.getInfo[:age]}, says '#{comedian.getInfo[:joke]}'"}
 
 comedians.each {|comedian| comedian.say }
