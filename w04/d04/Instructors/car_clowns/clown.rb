ad72cfd40353c4d366ff039e711a3cee17cca520
im11041991 <i.martinez8@umiami.edu>
2014-08-28 12:33:23 -0400

    clowns

--- w04/d04/Instructors/car_clowns/clown.rb
+++ w04/d04/Instructors/car_clowns/clown.rb
@@ -1,4 +1,9 @@
 require_relative './model'
 
 class Clown < Model
+	def car
+		return Car.find_by("id", @attributes["car_id"])
+	end
 end
+
+#find_by used to find only one whereas select_by is used to find all 
