a15a4ace73caa764e89f546399ccc9e58cd3b1b3
Nastassia <nastassiacarmona@gmail.com>
2014-08-28 09:52:17 -0400

    Modified hw

--- w04/d03/Homework/user.rb
+++ w04/d03/Homework/user.rb
@@ -3,25 +3,30 @@ require 'pry'
 
 class User
   def initialize(hash)
-    @first_name = hash[:first]
-    @last_name = hash[:last]
-    @gender = hash[:gender]
-    @city = hash[:city]
-    @state = hash[:state]
+    @attributes = hash
+    # @first_name = hash[:first]
+    # @last_name = hash[:last]
+    # @gender = hash[:gender]
+    # @city = hash[:city]
+    # @state = hash[:state]
   end
 
   def greeting()
-    return "Hello! My name is: #{@first_name + " " + @last_name}! I'm from: #{@city}, #{@state}"
+    return "Hello! My name is: #{@attributes[:first] + " " + @attributes[:last]}! I'm from: #{@attributes[:city]}, #{@attributes[:state]}"
+  end
+
+  def [](key)
+    return @attributes[key]
   end
 
   def self.random()
     random_user = HTTParty.get("http://api.randomuser.me")
-    random_user_info = ["results"][0]["user"]
+    random_user_info = random_user["results"][0]["user"]
 
     random_hash = {first: random_user_info["name"]["first"], last: random_user_info["name"]["last"], gender: random_user_info["gender"], city: random_user_info["location"]["city"], state: random_user_info["location"]["state"]}
 
-    puts random_user_info
-    # return (User.new(random_hash)
-    return random_user["results"][0]["user"]
+    # puts random_user_info
+    return (User.new(random_hash)
+    # return random_user["results"][0]["user"]
   end
 end
