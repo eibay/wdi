f71b87507ec3348a730b24295ddc3291df086d19
lisajwells <lisawells999@gmail.com>
2014-08-29 17:17:28 -0400

    one more form to build

---
+++ w04/d05/Homework/WDumblr/lib/post.rb
@@ -0,0 +1,10 @@
+require_relative './model'
+
+class Post < Model
+
+	 def author
+	 	Author.find_by("id", @attributes["author_id"])
+	 end
+
+
+end
---
+++ w04/d05/Homework/WDumblr/server.rb
@@ -0,0 +1,54 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+require 'httparty'
+require 'json'
+require_relative './lib/author'
+require_relative './lib/post'
+
+get("/") do
+	erb(:index)
+end
+
+get("/posts") do
+	erb(:posts, { locals: { posts: Post.all() } })
+end
+
+get("/posts/new") do
+	erb(:new_post, { locals: { authors: Author.all() } })
+end
+
+post("/posts") do
+	binding.pry
+	post_hash = {
+		title: params["title"],
+		keyword: params["keyword"],
+		created_at: params["created_at"],
+		content: params["content"],
+		author_id: params["author_id"]
+	}
+
+	Post.create(post_hash)
+
+	erb(:posts, { locals: { posts: Post.all() } })
+end
+
+
+get("/authors") do
+	erb(:authors, { locals: { authors: Author.all() } })
+end
+
+get("/authors/new") do
+	erb(:new_author, { locals: { posts: Post.all() } })
+end
+
+get("/posts/:id") do
+	post = Post.find_by("id", params[:id])
+	post_author = post.author
+	erb(:post, { locals: { post: post, post_author: post_author } })
+end
+
+get("/authors/:id") do
+	author = Author.find_by("id", params[:id])
+	erb(:author, { locals: { author: author } })
+end
