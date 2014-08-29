185acf09d69fd99dff4d8781f2bd0e3415e5537c
DanielFarber <farber.daniel.m@gmail.com>
2014-08-29 08:56:42 -0400

    Did the hw

---
+++ w04/d04/Classwork/objects/mail_app.rb
@@ -0,0 +1,3 @@
+require_relative "./mailbox.rb"
+require "pry"
+
---
+++ w04/d04/Classwork/westeros/house.rb
@@ -0,0 +1,10 @@
+require "json"
+require_relative "./model"
+
+class House < Model
+
+	def characters
+		Character.select_by("house_id", @attributes["id"])
+	end
+
+end
