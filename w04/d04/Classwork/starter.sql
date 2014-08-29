e8ea3c87aad1918a375a38432c2d92ef1848b5ef
jortenberg <jill.ortenberg@gmail.com>
2014-08-28 20:18:40 -0400

    homework wk04 day04

--- w04/d04/Classwork/starter.sql
+++ w04/d04/Classwork/starter.sql
@@ -22,3 +22,45 @@ SELECT * FROM meerkats;
 -- delete
 DROP TABLE cars
 
+
+-- HOMEWORK
+
+CREATE DATABASE tablefun;
+
+CREATE TABLE schools (id serial primary key, name varchar(50), city varchar(50), state varchar(30), zip varchar(5));
+
+
+INSERT INTO schools (name, city, state, zip) VALUES ('South Side HS', 'Rockville Centre', 'New York', '11570');
+INSERT INTO schools (name, city, state, zip) VALUES ('Oceanside HS', 'Oceanside', 'New York', '11480');
+INSERT INTO schools (name, city, state, zip) VALUES ('University of Arizona', 'Tucson', 'Arizona', '14383');
+
+SELECT * FROM schools;
+
+UPDATE schools
+SET Column1 = REPLACE(Manhattan High)
+WHERE Column1 LIKE '%a%'
+
+CREATE TABLE teachers (id serial primary key, name varchar(50), email varchar(40), subject varchar(50), age integer);
+
+INSERT INTO teachers (name, email, subject, age) VALUES ('Ms. Jane Smith', 'jsmith@oceansidehs.com', 'Math', 30);
+INSERT INTO teachers (name, email, subject, age) VALUES ('Mr. Seymour Brown', 'sbrown@southsidehs.com', 'Math', 35);
+
+INSERT INTO teachers (name, email, subject, age) VALUES ('Mr. John Miluf', 'jmiluf@uofarizona.com', 'Painting', 38);
+INSERT INTO teachers (name, email, subject, age) VALUES ('Mr. Sam Williamson', 'swilliamson@oceansidehs.com', 'Physics', 23);
+INSERT INTO teachers (name, email, subject, age) VALUES ('Ms. Jill Nelson', 'jnelson@oceansidehs.com', 'Gym', 40);
+INSERT INTO teachers (name, email, subject, age) VALUES ('Ms. Amy Smith', 'asmith@uorarizona.com', 'Anthropology', 51);
+
+SELECT * FROM teachers;
+SELECT * FROM schools;
+
+UPDATE teachers
+SET Column5 = REPLACE(Column5,40)
+-- WHERE Column1 LIKE '%a%'
+
+UPDATE schools SET name='Manhattan High';
+
+UPDATE teachers SET age=40 WHERE subject='Math';
+
+DELETE FROM teachers WHERE subject='math';
+
+
