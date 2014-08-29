57e4208b5dde99e05b8a2bc36271b6bd9058d756
clay-reed-a <clay.reed.a@gmail.com>
2014-08-29 10:26:05 -0400

    an authors model

---
+++ w04/d05/Homework/lib/author.rb
@@ -0,0 +1,76 @@
+require 'json'
+require 'securerandom'
+
+class Author 
+
+	# define a db var # 
+
+	@@db = "./author.db"  
+
+
+	# the init # 
+
+	attr_accessor :name, :email 
+	attr_reader :id 
+
+	def initialize name, email, id=SecureRandom.hex 
+		@name = name 
+		@email = email 
+		@id = id 
+	end
+
+
+	# helper methods # 
+
+	def db_hashes 
+		f = File.read @@db 
+		JSON.parse f
+	end 
+
+
+	# class methods # 
+
+	def self.all
+		db_hashes.map &:to_author 
+	end
+
+	def self.find_all_by key, val 
+		Author.all.select do |author|
+			author.send(key) == val 
+		end 
+	end 
+
+	def self.find_by key, val 
+		Author.all.find do |author|
+			author.send(key) == val 
+		end 
+	end
+
+	def self.find_by_id id  
+		Author.all.find do |author|
+			author.send(:id) == id 
+		end 
+	end 
+
+
+	# instance methods # 
+
+	def to_hash 
+		{"name" => self.name, "email" => self.email, "id" => self.id}
+	end 
+
+	def create
+		db_hashes << self.to_hash
+		File.write @@db, db_hashes.to_json 
+	end 
+
+end 
+
+
+# for reading from db # 
+
+class Hash 
+	def to_author 
+		Author.new self["name"], self["email"], self["id"]
+	end 
+end 
