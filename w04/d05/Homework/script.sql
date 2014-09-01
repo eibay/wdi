338325051dbc10186a29104a4b610697fd59e466
clay-reed-a <clay.reed.a@gmail.com>
2014-09-01 13:13:47 -0400

    draft sql script

--- w04/d05/Homework/script.sql
+++ w04/d05/Homework/script.sql
@@ -1,2 +1,20 @@
-CREATE TABLE authors (id , name varchar(50), email varchar(30)
+
+
+CREATE TABLE authors (id serial primary key, 
+	name varchar(50), 
+	email varchar(30)
+); 
+
+CREATE TABLE posts (id serial primary key,
+	subject varchar(50), 
+	content varchar(1000), -- limit 1000 chars for post 
+	created_at varchar(), 
+	author_id integer 
+); 
+
+CREATE TABLE images (id serial primary key,
+	src varchar(300),
+	alt varchar(600), 
+	post_id integer,
+	author_id integer
 ); 
