949050b73fc601f9e2b9c8819acd18360382e2f1
clay-reed-a <clay.reed.a@gmail.com>
2014-09-02 12:34:12 -0400

    create a ufo table w/ schema.sql

---
+++ w05/d02/Classwork/activerecord_exercises/ufo_sightings/schema.sql
@@ -0,0 +1,22 @@
+
+-- run this w/ 
+-- $ psql -d ufo_sightings -a -f schema.sql
+
+CREATE TABLE ufos (
+	id serial primary key, 
+	sighted_at varchar(8),
+	reported_at varchar(8), 
+	location varchar(130),
+	shape varchar(10), 
+	duration varchar(30),
+	description text
+);
+
+-- where 
+
+-- sighted_at & reported_at are both x 
+-- ufo[x].length => 8 
+
+-- & shape & duration are 
+-- varchar_size for "shape"
+
