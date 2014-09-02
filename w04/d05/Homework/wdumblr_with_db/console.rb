d13b9ae2129f32a0312d24a1c7d49906f20e6c66
neener <janinemrosen@gmail.com>
2014-09-02 00:14:03 -0400

    reflection, database attempt, one part exercise

---
+++ w04/d05/Homework/wdumblr_with_db/console.rb
@@ -0,0 +1,9 @@
+require_relative './lib/author'
+require_relative './lib/post'
+require_relative './lib/model'
+require_relative './lib/image'
+require_relative './lib/connection'
+
+require 'pry'
+
+binding.pry
