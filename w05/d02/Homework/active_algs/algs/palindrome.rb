25f6af2b8904616a6bfc15e59d30dcc5067c7d9e
DanielFarber <farber.daniel.m@gmail.com>
2014-09-03 08:52:11 -0400

    Updated my palindrom algorithm to include phrases

--- w05/d02/Homework/active_algs/algs/palindrome.rb
+++ w05/d02/Homework/active_algs/algs/palindrome.rb
@@ -4,4 +4,8 @@ def palindrome(string)
 	string == string.reverse
 end
 
+def palindromic_phrase(string)
+	string.downcase.gsub(/\W/, "") == string.downcase.gsub(/\W/, "").reverse
+end
+
 binding.pry
