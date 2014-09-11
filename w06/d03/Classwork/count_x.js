53cff5c74883b2d416bd266ce8ac4fa1124f234d
tdshap <Sue@TessShapiro.local>
2014-09-11 09:09:27 -0400

    javascrip problems

--- w06/d03/Classwork/count_x.js
+++ w06/d03/Classwork/count_x.js
@@ -6,11 +6,25 @@
 // end 
 
 
+// return ("thing".search("xx"));
+
 
 function count_xx(thing){
-	return ("thing".search("xx"));
+	var i = 0;
+	var counter = 0;
+	while (i < thing.length) {
+		if (thing.charAt(i) == "x" && thing.charAt(i+1) == "x"){
+			counter += 1;
+		}		
+		i += 1;
+	}
+	return counter
 }
 
-a = "xxhelloxx"
+var num = count_xx("xxhelloxx");
 
-console.log(count_xx(a))
+
+var msg = "that answer is " + num;
+console.log(msg)
+
+
