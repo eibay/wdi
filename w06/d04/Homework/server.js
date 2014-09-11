5f9b00fd0e79adb4c7ba991a8002af8d41f9e0c8
clay-reed-a <clay.reed.a@gmail.com>
2014-09-11 17:03:33 -0400

    setting up my homework playpen

---
+++ w06/d04/Homework/server.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var http = require('http');
+var request = require('request'); // https://www.npmjs.org/package/request
+
+instagram_api_key = "17a4630338cd4da38b94cbbecd787fae";
