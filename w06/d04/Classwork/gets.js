9f1de9b8867a51a67b71b580211189083181391a
clay-reed-a <clay.reed.a@gmail.com>
2014-09-11 12:59:25 -0400

    comments

--- w06/d04/Classwork/gets.js
+++ w06/d04/Classwork/gets.js
@@ -1,23 +1,35 @@
 #!/usr/local/bin/node 
 
 function gets(what, callback) {
+	// initialize stdin reading process 
 	process.stdin.resume(); 
+	// log the contents of what along w/ a newline 
 	console.log(what + '\n'); 
 
+	// listen for a 'data' event in stdin,  
+	// perform the hereunder callback & 
+	// detach the listener from stdin 
 	process.stdin.once('data', function(data) {
+		// this callback 
+		// stores the data as a string inside str 
 		str = data.toString();
+		// & performs the callback  
+		// passed to gets(), passing it str 
 		callback(str); 
 	}); 
 }
 
-gets("What is your age?", function(name) {
-	gets("What is your name?", function(email) {
+gets("What is your age?", function(age) {
+	// wait until you get age then 
+	gets("What is your name?", function(name) {
+		// wait until you get name then 
 		gets("What is kind of computer do you have?", function(computer) {
-			console.log('');
-			console.log("Your age is: ", name);
-			console.log("Your name is: ", email);
-			console.log("You computer is a: ", computer); 
-			process.exit();
+			// wait until you get computer then 
+			console.log(''); // log a newline 
+			console.log("Your age is: ", age); // log a description of name 
+			console.log("Your name is: ", name); // etc 
+			console.log("You computer is a: ", computer); // etc
+			process.exit(); // terminate script 
 		}); 
     });
 }); 
