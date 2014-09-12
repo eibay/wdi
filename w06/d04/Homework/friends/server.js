5b482b7c5786738c1dfa29ed582fff0c5d3ec529
lisajwells <lisawells999@gmail.com>
2014-09-11 21:08:04 -0400

    sad javascript homework

--- w06/d04/Homework/friends/server.js
+++ w06/d04/Homework/friends/server.js
@@ -16,21 +16,26 @@ var allFriends = ["Rachel Green", "Monica Geller", "Phoebe Buffay", "Joey Tribbi
 		var css = fs.readFileSync('./style.css');
 		response.end(css); 
 	} else if (path == '/cast') {
+		var html = fs.readFileSync('./cast.html', 'utf8');
 		var allFriends = ["Rachel Green", "Monica Geller", "Phoebe Buffay", "Joey Tribbiani", "Chandler Bing", "Ross Geller", "Gunther", "Janice"];
 		var friendList = [];
 		for (var i = 0; i < allFriends.length; i++) {
 			friendList.push('<li>' + allFriends[i] + '</li>')
-		}		
-debugger
-		response.end("this is cast"); 
+		}	
+		var friendListJoined = friendList.join("");
+// ben did the next 2 lines
+		var splitHtml = html.split('#');
+		var finalHtml = splitHtml[0] + friendListJoined + splitHtml[1];
+		response.end(finalHtml); 
 	} else if (path == '/cast/random') {
-		// var html = "member"
-		// response.end(member);
-// var randomFriend = allFriends[Math.floor(Math.random() * allFriends.length)]
+		var html = fs.readFileSync('./member.html', 'utf8');
+// I tried to copy what Ben did but it doesn't see the css and i'm out of time
+		var allFriends = ["Rachel Green", "Monica Geller", "Phoebe Buffay", "Joey Tribbiani", "Chandler Bing", "Ross Geller", "Gunther", "Janice"];
 
-	// }
-
-		response.end("this is member"); 
+		var randomFriend = allFriends[Math.floor(Math.random() * allFriends.length)];
+		var splitHtml = html.split('#');
+		var finalHtml = splitHtml[0] + randomFriend + splitHtml[1];
+		response.end(finalHtml); 
 	} else {
 		response.end("<h2>you have no friends</h2>");
 	};
