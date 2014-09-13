b724d6c745b26c4ce51a4ef26ff0000bc401d4df
lisajwells <lisawells999@gmail.com>
2014-09-13 14:53:20 -0400

    got 4567 to listen to 2000

--- w06/d05/Homework/randomuser_lab_9_12_14/band_getter.rb
+++ w06/d05/Homework/randomuser_lab_9_12_14/band_getter.rb
@@ -15,6 +15,19 @@ end
 # Each listed band should have a link to their page
 
 get("/") do
+	bands = HTTParty.get("http://127.0.0.1:2000/")
 
+	erb(:index, { locals: { bands: bands } })
 end
 
+# post("/band/create") do
+
+
+# 	erb(:index)
+# end
+
+# House.create({
+#   name: "Lannister", 
+#   sigil_url: "http://img1.wikia.nocookie.net/__cb20140402110342/gameofthrones/images/thumb/0/0b/House-Lannister-heraldry.jpg/250px-House-Lannister-heraldry.webp",
+#   region: "The Westerlands"
+# })
