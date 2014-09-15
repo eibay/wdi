da7ae4da7e8d466decc9b3ea71e332cdabcf38c9
Nastassia <nastassiacarmona@gmail.com>
2014-09-15 09:14:47 -0400

    Committing hw

--- w06/d05/Homework/part1/scratch.js
+++ w06/d05/Homework/part1/scratch.js
@@ -20,3 +20,42 @@ function matchStuff(companies, query){
 };
 
 matchStuff(companies, query);
+
+console.log("####DRACONIC NAMES####")
+
+var adj = [{"beautiful": "vorel"}, {"big": "turalisj"}, {"black":"vutha"}, {"blue":"ulhar"}];
+var noun = [{"battle":"vargach"}, {"bronze":"aujir"}, {"blood":"iejir"}, {"bravery":"sveargith"}];
+
+console.log(adj);
+console.log(noun);
+
+>>>>>>>>>>>>>>>
+
+var display_result = [];
+var name_adj = makeName(adj);
+var name_noun = makeName(noun);
+display_result.push(name_adj);
+display_result.push(name_noun);
+var name_result = JSON.stringify(display_result);
+var blah = JSON.stringify(Object.keys(display_result[0]));
+
+//   var companies_in_json = JSON.stringify(companies);
+//
+//
+//   if (path == "/"){
+//     debugger;
+//     response.end(companies_in_json);
+//   } else if (path == "/"+ query){
+//     debugger; 
+//     matchStuff(companies, query);
+//     result = JSON.stringify(result);
+//     response.end(result);
+//   } else {
+//     debugger;
+//     response.end("err.");
+//   }
+//
+// });
+//
+//
+// server.listen(2000);
