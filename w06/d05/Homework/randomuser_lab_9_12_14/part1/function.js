ef7542e53b1387e0f60285e0251f18e5c1b1b7c8
neener <janinemrosen@gmail.com>
2014-09-15 10:23:48 -0400

    classwork function

---
+++ w06/d05/Homework/randomuser_lab_9_12_14/part1/function.js
@@ -0,0 +1,14 @@
+var companies = [
+  {name: "apple", location: "cupertino"},
+  {name: "google", location: "mountain view"}
+];
+
+var findCompanyByName = function(firstAfterSlash){
+
+var companies.forEach(function(company){
+    if (company["name"] == path.split("/")[1]){
+      company = JSON.stringify(company)
+      response.end(company);
+  	}
+});
+  	console.log(company);
