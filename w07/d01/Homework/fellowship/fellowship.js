01fd6e1e58c8aa948a106f71f8219b4a4c1d4308
clay-reed-a <clay.reed.a@gmail.com>
2014-09-15 17:18:52 -0400

    makeMiddleEarth happen

--- w07/d01/Homework/fellowship/fellowship.js
+++ w07/d01/Homework/fellowship/fellowship.js
@@ -18,6 +18,22 @@ var buddies = [
 var lands = ["The Shire", "Rivendell", "Mordor"];
 
 function makeMiddleEarth(lands) {
+	var middleEarth = document.getElementById("middle-earth"); 
+	for (var i = 0; i < lands.length; i++) {
+		var land = lands[i]; 
+		var divId = land.toLowerCase().replace(' ', '-');  
+
+		var div = document.createElement("div");
+		div.id = divId; 
+		middleEarth.appendChild(div); 
+
+		var h1 = document.createElement("h1"); 
+		h1.innerText = land; 
+		var landDiv = document.getElementById(divId); 
+		landDiv.appendChild(h1); 
+
+		
+	}
 }
 
 function makeHobbits(hobbits) {
