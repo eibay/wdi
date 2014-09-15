2af7ec882682af6d44e30afb9862fb97821242af
Nastassia <nastassiacarmona@gmail.com>
2014-09-15 11:37:16 -0400

    Random person

--- w07/d01/Classwork/clswrk.js
+++ w07/d01/Classwork/clswrk.js
@@ -12,3 +12,21 @@ function findCompanyByName(CompanyName){
 }
 
 console.log(findCompanyByName("google"));
+
+var people = [];
+function createPerson(){
+	var name = ["Edgar", "Balthazar", "Bartholomew", "Lenore", "Clarice", "Gabrielle", "Maximillian", "Ebenezer", "Sage", "Giovanni", "Vladimir", ];
+	var last = ["Rodriguez", "Whitfield", "Mansfield", "DiGuiseppe", "Dashevsky"];
+	var location = ["Russia", "New York", "Spain", "Portugal"]
+
+	var name_result = name[parseInt((Math.random() * name.length))];
+	var last_result = last[parseInt((Math.random() * last.length))];
+	var location_result = location[parseInt((Math.random() * location.length))];
+
+	var person = {firstName: name_result, lastName: last_result, location: location_result}
+
+	people.push(person);
+	return person
+};
+
+console.log(createPerson());
