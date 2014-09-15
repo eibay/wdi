cc6b4108ecfb2f3ed50d207e02a88f507c79e3da
DanielFarber <farber.daniel.m@gmail.com>
2014-09-15 11:37:36 -0400

    It worked in my server

--- w06/d05/Homework/randomuser_lab_9_12_14/part_two/scratch.js
+++ w06/d05/Homework/randomuser_lab_9_12_14/part_two/scratch.js
@@ -6,8 +6,16 @@ console.log("Please enter your name.");
 
 process.stdin.on("readable", function() {
   var name = process.stdin.read();
+  if (name != null) {
+  	name = name.replace("\n", "");
+  	console.log("Hello, " + name + ".");
+  	process.end();
   }
-});
+ });
+
+process.stdin.on("end", function() {
+	console.log("Enter your city and state of residence, please.");
+})
 
 
-console.log(name);
+
---
+++ w07/d01/Classwork/createPeople.js
@@ -0,0 +1,48 @@
+var fs = require("fs")
+
+path = "/Users/farberge/dev/wdi/Rosencrantz/w06/d05/Homework/randomuser_lab_9_12_14/part_two/"
+
+
+
+var people = [];
+
+var first_names = JSON.parse(fs.readFileSync(path + "first_names.txt", "UTF8"));
+var last_names = JSON.parse(fs.readFileSync(path + "last_names.txt", "UTF8"));
+var locations = JSON.parse(fs.readFileSync(path + "locations.txt", "UTF8"));
+
+debugger;
+
+function createPerson() {
+	people.push(createUser());
+
+}
+
+function extractRandomly(array) {
+	rand = Math.floor(Math.random() * array.length);
+	return array[rand];
+
+}
+
+function createUser() {
+	var user = {
+		"first": extractRandomly(first_names),
+		"last": extractRandomly(last_names),
+		"location": extractRandomly(locations),
+		"id": people.length,
+	}
+	return user;
+}
+
+
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+console.log(createPerson());
+
+
+
