3f5f7fe065aa630bf73af9681f9f301ff27c7761
adrianmlin <adrian.mx.lin@gmail.com>
2014-09-15 21:15:49 -0400

    fellowship

--- w06/d05/Homework/randomuser_lab_9_12_14/ruby.rb
+++ w06/d05/Homework/randomuser_lab_9_12_14/ruby.rb
@@ -5,13 +5,13 @@ require 'json'
 require 'pry'
 
 storage_array = []
-
+users_all = JSON.parse(HTTParty.get("http://127.0.0.1:2000/users"))
 
 get '/' do 
-  users = JSON.parse(HTTParty.get("http://127.0.0.1:2000/users"))
+  
   
   5.times do
-    storage_array << users[rand(users.length)]
+    storage_array << users_all[rand(users_all.length)]
   end
 
   erb(:index, {locals: {storage_array: storage_array}})
@@ -21,10 +21,10 @@ end
 
 
 post '/user/create' do
-  new_user = JSON.parse(HTTParty.get('http://127.0.0.1:2000/user/create'))
-  storage_array << new_user
-  redirect request.referrer
+  new_user = JSON.parse(HTTParty.post('http://127.0.0.1:2000/user/create'))
+  users_all << new_user
 
+  redirect request.referrer
 end
 
 
@@ -32,7 +32,7 @@ end
 
 get '/user/:user_id' do
     
-  user_info = storage_array.detect { |hash| hash['id'] == params[:user_id] }
+  user_info = users_all.detect { |hash| hash['id'] == params[:user_id] }
 
 
   erb(:user_show, {locals: {user_info: user_info}})
@@ -43,7 +43,7 @@ post '/comment' do
 
 
 
-  this_user = storage_array.detect { |hash| hash['id'] == params['this_user_id']}
+  this_user = users_all.detect { |hash| hash['id'] == params['this_user_id']}
   this_user['comments'] = [] if this_user['comments'] == nil #create new array as key if it doesnt exist. so i dont overwrite
   new_temp_array = this_user['comments'] #takes the values out and makes new_temp_array. this way we can save old values, since otherwise we'd overwrite.
   new_temp_array << params["comment"] #shovel in the new comment
--- w07/d01/Homework/fellowship/fellowship.js
+++ w07/d01/Homework/fellowship/fellowship.js
@@ -15,22 +15,114 @@ var buddies = [
   "Boromir"
 ];
 
+var middleEarth = document.getElementById("middle-earth");
+
 var lands = ["The Shire", "Rivendell", "Mordor"];
 
 function makeMiddleEarth(lands) {
-}
+	lands.forEach(function(land) {
+		
+		var landName = document.createElement("h1")
+		landName.innerText = land
+
+		var land = document.createElement("div");
+		land.appendChild(landName)
+
+		middleEarth.appendChild(land)
+	})
+};
+makeMiddleEarth(lands);
+
+
+
 
 function makeHobbits(hobbits) {
+	var theShire = middleEarth.children[0]
+	var hobbit_ul = document.createElement("ul");
+
+	hobbits.forEach(function(name) {
+		var hobbit_li = document.createElement("li")
+		hobbit_li.innerText = name
+
+		hobbit_ul.appendChild(hobbit_li);
+
+	})
+	theShire.appendChild(hobbit_ul)
 }
+makeHobbits(hobbits)
+
 
 function keepItSecretKeepItSafe() {
+	var frodo = middleEarth.children[0].children[1].children[0]
+	var theRing = document.createElement('div')
+	theRing.id = 'the-ring'
+	theRing.innerText = "The One Ring"
+
+	frodo.appendChild(theRing)
+
+	// create a div with an id of 'the-ring'
+    // add the ring as a child of Frodo
 }
+keepItSecretKeepItSafe()
 
 function makeBuddies(buddies) {
+	var aside = document.createElement('aside')
+	var ul = document.createElement('ul')
+
+	buddies.forEach(function(buddy) {
+		var li = document.createElement('li')
+		li.innerText = buddy
+		ul.appendChild(li)
+	})
+	aside.appendChild(ul)
+
+	middleEarth.insertBefore(aside, middleEarth.children[1])
+	// create an aside tag
+  	// display an unordered list of buddies in the aside
+  	// insert your aside before rivendell
 }
+makeBuddies(buddies)
 
 function beautifulStranger() {
+	var strider = middleEarth.children[1].children[1].children[3]
+	strider.innerText = "Aragorn"
+	// change the buddy 'Strider' textnode to "Aragorn"
 }
+beautifulStranger()
 
 function forgeTheFellowShip() {
+	var travellers = middleEarth.children[0].children[1]
+	travellers.parentNode.removeChild(travellers)
+	middleEarth.children[1].appendChild(travellers)
+
+	var fellowship = document.createElement('div')
+	fellowship.id = 'the-fellowship'
+	var fellowship_ul = document.createElement('ul')
+
+	for(i=0; i<4; i++) {
+		var fellowship_li_h = document.createElement('li')
+		fellowship_li_h.innerText = hobbits[i]
+		fellowship_ul.appendChild(fellowship_li_h)
+		alert(hobbits[i] + " has joined the Fellowship!")
+
+		var fellowship_li_b = document.createElement('li')
+		fellowship_li_b.innerText = buddies[i]
+		fellowship_ul.appendChild(fellowship_li_b)
+		alert(buddies[i] + " has joined the Fellowship!")
+	}
+	var fellowship_li_b = document.createElement('li')
+	fellowship_li_b.innerText = buddies[4]
+	fellowship_ul.appendChild(fellowship_li_b)
+	alert(buddies[4] + " has joined the Fellowship!")
+
+	fellowship.appendChild(fellowship_ul)
+
+	middleEarth.children[2].appendChild(fellowship)
+
+	// move the hobbits and the buddies to Rivendell
+  	// create a new div called 'the-fellowship'
+  	// add each hobbit and buddy one at a time to 'the-fellowship'
+  	// after each character is added make an alert that they have joined your party
 }
+
+forgeTheFellowShip()
