030abab1116108f838a5c059d69c5dbc6b9687b9
Nastassia <nastassiacarmona@gmail.com>
2014-09-17 11:52:26 -0400

    classwork

---
+++ w07/d03/Classwork/atm.js
@@ -0,0 +1,18 @@
+// var fs = require('fs');
+//
+// var inputs = process.argv;
+// var monetary_amt = inputs[3];
+//
+// if (inputs[2] == "deposit"){
+//   var file = JSON.parse(fs.readFileSync('./monies.txt').toString());
+//   console.log(file);
+//   console.log(typeof(file));
+//   var result = file.push(monetary_amt);
+//   result = result.to_json;
+//   console.log(file);
+//   file.writeFileSync('./monies.txt', result);
+//   // // fs.appendFileSync('./monies.txt', inputs[3]);
+// } else if (inputs[2] == "withdraw"){
+//   var monies  = fs.readFileSync('./monies.txt').toString();
+//
+// }
