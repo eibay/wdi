40c7a4c1280eef0b7f943dd0206f610779d81693
clay-reed-a <clay.reed.a@gmail.com>
2014-09-18 14:31:00 -0400

    I am he as you are me & we are all together

--- w07/d04/Classwork/ajax_lab/explore/server.rb
+++ w07/d04/Classwork/ajax_lab/explore/server.rb
@@ -1,12 +1,27 @@
+
+# use the sinatra web framework 
 require 'sinatra'
-require 'json'
+# use the json & httparty libraies  
+require 'json' 
 require 'httparty'
 
+# when the server recieves a get request for a path  
+# assume the string following the first forward slash is a film title 
 get '/:movie_title' do
+  # set HTTP response body's Content-Type in the HTTP response header  
+  # http://rubydoc.info/github/sinatra/sinatra/Sinatra/Helpers:content_type
+  # http://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Response_fields 
+  # to JSON 
   content_type :json
 
+  # replace chars unusable in a uri w/ uri escape sequences 
   url = URI.encode("http://www.omdbapi.com/?t=#{params[:movie_title]}")
+
+  # send out a get request to omdb api 
   movie = HTTParty.get(url)
 
+  # send the json str in the response body 
+  # of the response from the api 
+  # to the client 
   movie.response.body
 end
