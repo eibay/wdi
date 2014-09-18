b2030650ea397e8f4f6db2db4f103deddadacde4
brendee <brenda.dargan@gmail.com>
2014-09-18 16:11:19 -0400

    W07/D04 classwork

--- w07/d04/Classwork/ajax_lab/explore/server.js
+++ w07/d04/Classwork/ajax_lab/explore/server.js
@@ -3,21 +3,24 @@ var http = require('http');
 var httpClient = require('request');
 var url = require('url');
 
+// Creates the server
 var server = http.createServer();
 
+// Activates server? Is on a method called on the server variable?
 server.on('request', function(request, response) {
+// Parses url from API. True makes the request asynchronous.  
   var parsedRequest = url.parse(request.url, true);
   console.log("Request received to: " + parsedRequest.pathname)
-
+// Conditional handles the routes. Sends user to index or specific movie page depending on the route.
   if (parsedRequest.pathname == '/') {
     response.end(fs.readFileSync('./index.html'));
   } else if (parsedRequest.pathname == '/movies') {
     var movieTitle = parsedRequest.query.title;
-
+// Treats ruby server as an API?
     httpClient.get('http://localhost:4567/' + encodeURIComponent(movieTitle), function(err, res2, body) {
       response.end(body);
     });
   }
 });
-
+// Tells the server to listen
 server.listen(2000);
--- w07/d04/Classwork/ajax_lab/explore/server.rb
+++ w07/d04/Classwork/ajax_lab/explore/server.rb
@@ -2,11 +2,14 @@ require 'sinatra'
 require 'json'
 require 'httparty'
 
+# Creates a route
 get '/:movie_title' do
+# Outputs JSON from Sinatra
   content_type :json
-
+# Formats and sets the API response to a variable
   url = URI.encode("http://www.omdbapi.com/?t=#{params[:movie_title]}")
+# Initiates the get request  
   movie = HTTParty.get(url)
-
+# Returns the information
   movie.response.body
 end
