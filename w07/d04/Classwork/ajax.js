5f6abac69fe3e8b3f9f1efab9f2cd36cc3c109c6
clay-reed-a <clay.reed.a@gmail.com>
2014-09-18 11:21:32 -0400

    use addEventListener instead of window.onload to allow multiple event listener assignments to window

--- w07/d04/Classwork/ajax.js
+++ w07/d04/Classwork/ajax.js
@@ -1,10 +1,11 @@
-window.onload = function() {
+window.addEventListener('load', function() {
+	console.log("window loaded!");
 	var xhr = new XMLHttpRequest(); 
 
 	xhr.open('GET', "http://www.omdbapi.com/?t=True%20Grit", true);
 	xhr.send(null);
 
-	xhr.addEventListener("load", function() {
+	xhr.addEventListener('load', function() {
 		console.log(xhr.response); 
 	}); 
-}
+}) 
