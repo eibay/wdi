ce26c6c2482f066b7f375e9458ffe60cab4f7a75
tdshap <Sue@TessShapiro.local>
2014-09-18 09:37:39 -0400

    js banking app, a little tic tac toe

---
+++ w07/d03/Homework/tictactoe.js
@@ -0,0 +1,20 @@
+var td = document.querySelectorAll("td")
+
+var 
+
+
+for (var i; i<td.length; i++){
+	addEventListner("click", function(event){
+		event.target.innerText = "x"
+
+
+
+	})
+
+}
+
+
+
+function won(board){
+
+}
