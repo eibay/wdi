dfdbcf44d06e2fa553a4fb39196a0ac222c4faf6
clay-reed-a <clay.reed.a@gmail.com>
2014-09-18 16:45:38 -0400

    finish writing javascript, add route to interact w/ ajax call, add additional ids to index view

--- w07/d04/Homework/server.rb
+++ w07/d04/Homework/server.rb
@@ -7,3 +7,13 @@ get '/' do
 
 	haml :index
 end
+
+get '/pictures' do 
+	content_type :json 
+	# this route will interact w/ the ajax call 
+	search = params["search"]
+	city = params["city"]
+
+	# it will return an array of strs,  
+	# representing src attributes 
+end 
