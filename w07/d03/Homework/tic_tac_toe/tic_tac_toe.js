e75c7933b6936773c267060bf98bcfa62cd53115
shotarokamegai <shotaro326@gmail.com>
2014-09-18 09:22:49 -0400

    did homework

---
+++ w07/d03/Homework/tic_tac_toe/tic_tac_toe.js
@@ -0,0 +1,19 @@
+var board = [["", "", ""], ["", "", ""], ["", "", ""]];
+
+function won(board) {
+	
+	for (var i = 0; i < winningCombos.length; i++) {
+		var winningCombo = winningCombos[i];
+		if (board[winningCombo[0]] == "x" && board[winningCombo[1]] == "x"  && board[winningCombo[2]] == "x") {
+		x wins
+		} else ( you write this one) {
+		o wins
+}
+
+
+
+
+
+
+
+}
