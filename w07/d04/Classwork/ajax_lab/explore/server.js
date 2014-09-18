8fed28fb1ee92dbad2f417d51c01843776e7d36a
jkonowitch <jkonowitch@gmail.com>
2014-09-18 13:33:41 -0400

    lab

---
+++ w07/d04/Classwork/ajax_lab/explore/server.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var http = require('http');
+var httpClient = require('request');
+var url = require('url');
+
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+  var parsedRequest = url.parse(request.url, true);
+  console.log("Request received to: " + parsedRequest.pathname)
+
+  if (parsedRequest.pathname == '/') {
+    response.end(fs.readFileSync('./index.html'));
+  } else if (parsedRequest.pathname == '/movies') {
+    var movieTitle = parsedRequest.query.title;
+
+    httpClient.get('http://localhost:4567/' + encodeURIComponent(movieTitle), function(err, res2, body) {
+      response.end(body);
+    });
+  }
+});
+
+server.listen(2000);
