f3d7b9a4b88a125bc39efc2ecdcfb17ca99876bb
JBiggs91 <joe.biggica@gmail.com>
2014-09-19 09:55:46 -0400

    google api working on client side

--- w07/d04/Homework/server.rb
+++ w07/d04/Homework/server.rb
@@ -12,22 +12,23 @@ end
 
 get '/city-pics' do 
   content_type :json
+
   lat = params[:lat]
   lng = params[:lng]
   url = URI.encode("https://api.instagram.com/v1/media/search?lat=#{lat}&lng=#{lng}&client_id=4ad7cc36c172434588afd340aa74cd01")
   pictures = HTTParty.get(url)
-binding.pry
+
   pictures.response.body
 
 end
 
-# get '/tag-pics' do 
-#   content_type :json
-#   tag = params[:tag]
-#   url = URI.encode("https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=4ad7cc36c172434588afd340aa74cd01")
-#   pictures = HTTParty.get(url)
-# binding.pry
-#   pictures.response.body
+get '/tag-pics' do 
+  content_type :json
+  tag = params[:tag]
+  url = URI.encode("https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=4ad7cc36c172434588afd340aa74cd01")
+  pictures = HTTParty.get(url)
 
-# end
+  pictures.response.body
+
+end
 
