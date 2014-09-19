151da26b52c1023e710ba0dcbb6ebee11d99795f
lisajwells <lisawells999@gmail.com>
2014-09-19 10:14:22 -0400

    to allow me to pull

--- w07/d04/Homework/instagram/server.rb
+++ w07/d04/Homework/instagram/server.rb
@@ -7,7 +7,9 @@ require 'pry'
 # ruby gets the photos from instagram
 
 get '/' do 
-tag_name = 'sunglasses'
+
+# for the time being, until i figure out how to make a button work
+tag_name = 'grass'
 response = HTTParty.get("https://api.instagram.com/v1/tags/#{tag_name}/media/recent?client_id=4ad7cc36c172434588afd340aa74cd01")
 	erb(:index, {locals: {response: response}})
 end
