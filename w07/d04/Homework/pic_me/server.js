1c30f8414d17bf997c8c5fb980d4afc005967169
theerickramer <theerickramer@gmail.com>
2014-09-18 23:07:20 -0400

    location search instagram works

--- w07/d04/Homework/pic_me/server.js
+++ w07/d04/Homework/pic_me/server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var request = require('request');
 var fs = require('fs');
-
-// var url = require('url');
+var httpAdapter = 'http';
+var geocoderProvider = 'google';
+var geocoder = require('node-geocoder').getGeocoder(geocoderProvider, httpAdapter);
 
 var server = http.createServer(function(req,res) {
 	var path = req.url
@@ -14,18 +15,14 @@ var server = http.createServer(function(req,res) {
 	} else if(path.split('?')[0] == '/photos') {
 		var loc = path.split('=')[1];
 		console.log('made it!')
-		// var loc = request.params['loc'].split(' ').join('+')
-		var googloc = request('http://maps.googleapis.com/maps/api/geocode/json?address=' + loc, function(err, res2, body) {
-		});
-		
-		// var lat = googloc['results'][0]['geometry']['location']['lat']; 
-		// var lng = googloc['results'][0]['geometry']['location']['lng']; 
 
-		request('https://api.instagram.com/v1/media/search?lat=40.739446&lng=-73.8333651&client_id=6e4453d0a1e84159a1b28c13d09916cb', function(err, res2, body) {
+		geocoder.geocode(loc, function(err, geores) {
+			var lat = geores[0]['latitude'];
+			var lng = geores[0]['longitude']
+			request('https://api.instagram.com/v1/media/search?lat=' + lat + '&lng=' + lng + '&client_id=6e4453d0a1e84159a1b28c13d09916cb', function(err, instares, body) {
 			res.end(body);
+			});
 		});
-		// var photos = request('https://api.instagram.com/v1/media/search?lat=' + lat + '&lng=' + lng + '&client_id=6e4453d0a1e84159a1b28c13d09916cb', function(err, res2, body) {
-		// });
 	}
 });
 
