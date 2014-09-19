227cb47ecee8fd108cba4fbf1b60a1b334b0fe55
clay-reed-a <clay.reed.a@gmail.com>
2014-09-19 11:02:46 -0400

    query omdb api & extract poster url

--- w07/d05/Classwork/server.rb
+++ w07/d05/Classwork/server.rb
@@ -7,3 +7,12 @@ get '/' do
 
 	haml :index 
 end 
+
+get "/search" do 
+	title = params["title"]
+	api_url = "http://www.omdbapi.com/?t=#{title}"
+	movie_json = HTTParty.get api_url 
+	result = JSON.parse movie_json 
+	p result["Poster"]
+	result["Poster"]
+end 
