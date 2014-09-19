cb07a7504b6c8eadbf7a11f3f828ed40876f8fa4
clay-reed-a <clay.reed.a@gmail.com>
2014-09-19 11:05:28 -0400

    finish

--- w07/d05/Classwork/server.rb
+++ w07/d05/Classwork/server.rb
@@ -9,10 +9,9 @@ get '/' do
 end 
 
 get "/search" do 
-	title = params["title"]
+	title = URI.encode params["title"]
 	api_url = "http://www.omdbapi.com/?t=#{title}"
 	movie_json = HTTParty.get api_url 
 	result = JSON.parse movie_json 
-	p result["Poster"]
 	result["Poster"]
 end 
