0cb67d9115c0091ef54963d9ed224013768c382f
lisajwells <lisawells999@gmail.com>
2014-09-19 22:38:18 -0400

    filled word div with underscores, but commas too

--- w07/d05/Homework/hangman/hangman.js
+++ w07/d05/Homework/hangman/hangman.js
@@ -20,3 +20,15 @@ for(var i=0; i<word.length; i++) {
  	}
  }}
 
+function dashAdd(word) {
+	wordDiv = document.querySelector('div.game-word');
+	dashArray = []
+		// count number of letters in word
+		// fill wordDiv with that many underscore chars
+		for (i=0; i < word.length; i++) {
+		dashArray.push("_");
+		wordDiv.innerText = dashArray;
+		
+	}
+
+}
