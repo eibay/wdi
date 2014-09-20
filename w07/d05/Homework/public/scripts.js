061ef04b3603d8cbac911ad0012b6da7a3878f80
pateltejal9500 <pateltejal9500@gmail.com>
2014-09-20 00:29:50 -0400

    homework

--- w07/d05/Homework/public/scripts.js
+++ w07/d05/Homework/public/scripts.js
@@ -1,54 +1,163 @@
-  function getRandomWord(word) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxC...');
-
-    xhr.addEventListener('load', function(){
-      console.log(xhr.response);
-      console.log(typeof xhr.response);
-      parsedWord = JSON.parse(xhr.response);
-      console.log(parsedWord);
-      return secretWord(parsedWord["word"]);
-        });
+window.onload = function(){  
+var word = []
+var wrongOptions = []
+var correctOptions = []
+var wordViews = {}
 
 
-  function theWord(correctArray, word){
-    word.split("")
+var spanGuessed = document.getElementsByClassName("guessed-letters")
+var h5_guessed = document.createElement("h5")
+spanGuessed[0].appendChild(h5_guessed)
 
-  }
- 
+var leftSpan = document.getElementsByClassName("guesses-left")
+var h5_left = document.createElement("h1")
+leftSpan[0].appendChild(h5_left)
+h5_left.innerText = 8
 
+var wordPlacement = document.getElementsByClassName("game-word")
+wordPlacement[0].innerText = ""
 
-function incorrect(word, letter, incorrectArray1){
-  if( word.indexOf(letter) == -1){
-     incorrectArray.push(letter)
-    console.log(incorrectArray)
+randomWord()
 
-  }
-  return incorrectArray1
+function gettingValues(hash){
+    var keys = Object.keys(hash);
+    var values = keys.map(function(v) { return hash[v]; });
+    var wordPlacement = document.getElementsByClassName("game-word")
+    console.log(values.join(""))
+    wordPlacement[0].innerText = values.join("")
 }
 
-function correct (word, letter, indexValues1){
- 
-  if (word.indexOf(letter) != -1){
-     
-    for(i = 0; i < word.length; i++){
-      if (word[i] == letter){
-        indexValues.push(i)
-        console.log(indexValues)
-      } 
+function wordScreen(){
+  wordViews ={}
+  for(i=0; i < word[0].length; i++){
+    wordViews[i]="#"
     }
-    return indexValues1
+    gettingValues(wordViews)
 }
+
+
+  function randomWord(letter){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
+    xhr.send()
+    xhr.addEventListener('load', function(){
+      parsedWord = JSON.parse(xhr.response);
+      word = [parsedWord.word.toLowerCase()]
+      wordScreen()
+      console.log(word)
+       });
+    
+
+}
+
+buttonsEvent()
+
+  function buttonsEvent(){
+    buttons = document.querySelectorAll("button")
+      for (i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', function(event){
+        if(event.target.id == "guess-button" && wrongOptions.length < 8 && correctOptions.length != word[0].length){
+          letter = document.querySelector("input").value
+          console.log(letter.length)
+          if (letter.length != 1){
+            alert("TRY AGAIN WRONG AMOUNT OF LETTERS")
+          }
+          check(letter)
+          guessedLetter()
+          console.log(wrongOptions)
+          console.log(correctOptions)
+          document.querySelector("input").value = ""
+          letterBoard()
+        }else if (correctOptions.length == word[0].length){
+          alert("YOU ALREADY WON!, PLAY AGAIN")
+        
+        } else if (wrongOptions.length == 8){
+          alert("OUT OF TRIES!, PLAY AGAIN!")
+        } else {
+        randomWord()
+        wrongOptions = []
+        correctOptions = []
+        h5_left.innerText = 8
+        giveUp()
+        h5_guessed.innerText = ""
+
+        }
+
+        })
+      }
+  
+} 
+
+function checkWin(){
+  if (correctOptions.length == word[0].length){
+    alert("YOU WON!")
+  }
+}
+function giveUp(){
+  console.log(correctOptions.length)
+  console.log(word[0].length)
+  if(event.target.className == "button give-up"){
+    alert("YOU LOST!!THE WORD WAS "+word[0].toUpperCase())
+  } else {
+    alert(word[0].toUpperCase()+" WAS THE WORD")
+
+  }
+
+}
+
+function letterBoard(){
+  wordPlacement= document.getElementsByClassName("game-word")
+ 
+
+  for (i = 0; i < correctOptions.length; i++){
+    letterIndex = word[0].indexOf(correctOptions[i])
+    wordViews[letterIndex] = correctOptions[i]
+    wordViews.sort
+    gettingValues(wordViews)
+
+
+    
+    console.log(wordViews)
+    
+  }
+}
+function check(letter){
+  if (word[0].indexOf(letter) != -1 && letter.length == 1){
+    checkArrayCorrect(letter)
+  }else if (letter.length == 1) {
+    alert("TRY AGAIN")
+    checkArrayWrong(letter)
+  }
+}
+
+function checkArrayCorrect(letter){
+  if(correctOptions.indexOf(letter) == -1){
+    correctOptions.push(letter)
+   checkWin()
+  } else {
+    alert("Already Tried That Letter")
+  }
+}
+
+function checkArrayWrong(letter){
+  if(wrongOptions.indexOf(letter) == -1){
+    wrongOptions.push(letter)
+    left()
+     } else {
+    alert("Already Tried That Letter")
+  }
+}
+function guessedLetter(){
+h5_guessed.innerText = correctOptions.join(" ") + " " +wrongOptions.join(" ")
+}
+
+function left(){
+  h5_left.innerText = (8 - (wrongOptions.length))
+}
+
 }
 
 
-var incorrectArray = []
-var indexValues = []
-var button = document.getElementById("guess-button")
-var word = secretWord()
-button.addEventListener('click', function(){
-  var letter = document.querySelector("input").value
-  var incorrectArray = incorrectArray.concat(incorrect(word, letter, incorrectArray))
-  var indexValues = indexValues.concat(correct(word, letter, indexValues))
 
-})
+
+
