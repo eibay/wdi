f065690ebeb3ebb2a95e91158f32586aa5162298
lisajwells <lisawells999@gmail.com>
2014-09-20 09:15:12 -0400

    adding function of find indices of matches

--- w07/d05/Homework/hangman/hangman.js
+++ w07/d05/Homework/hangman/hangman.js
@@ -1,8 +1,22 @@
 var matches = []
 var wrongs = []
 var word = "poodle"
-var blank = []
 
+// fills word div with blanks
+function dashAdd(word) {
+	wordDiv = document.querySelector('div.game-word');
+	dashArray = []
+		// count number of letters in word
+		// fill wordDiv with that many underscore chars
+		for (i=0; i < word.length; i++) {
+		dashArray.push("_");
+		dashArrayNoCommas = dashArray.join('');
+		wordDiv.innerText = dashArrayNoCommas;		
+	}
+};
+
+// finds all matches and puts them in an array
+// calls function to put wrong guess in an array
 function matchGuess(letter, word){
 for(var i=0; i<word.length; i++) {
     if (letter == word[i]) {
@@ -18,18 +32,17 @@ for(var i=0; i<word.length; i++) {
  		if (wrongs.indexOf(letter) == -1) {
  		wrongs.push(letter);
  	}
- }}
+ }};
 
-function dashAdd(word) {
-	wordDiv = document.querySelector('div.game-word');
-	dashArray = []
-		// count number of letters in word
-		// fill wordDiv with that many underscore chars
-		for (i=0; i < word.length; i++) {
-		dashArray.push("_");
-		dashArrayNoCommas = dashArray.join('');
-		wordDiv.innerText = dashArrayNoCommas;
-		
-	}
+// finds index of letter in word
+function findIndicesLetter(word, letter) {
+	var indices = [];
+	var idx = word.indexOf(letter);
+	while (idx != -1) {
+	  indices.push(idx);
+	  idx = word.indexOf(letter, idx + 1);
+	} console.log(indices)
+};
 
-}
+
+
