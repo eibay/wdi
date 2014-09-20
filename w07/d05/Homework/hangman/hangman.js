9c7c0701572b7d3d499cdeabb3c8f6552af9885d
lisajwells <lisawells999@gmail.com>
2014-09-20 18:37:51 -0400

    replacing dashes right in right guess function

--- w07/d05/Homework/hangman/hangman.js
+++ w07/d05/Homework/hangman/hangman.js
@@ -21,7 +21,10 @@ function dashAdd(word) {
 function matchGuess(letter, word){
 for(var i=0; i<word.length; i++) {
     if (letter == word[i]) {
-      matches.push(letter);
+		dashArray.splice([i], 1, letter);
+		console.log(dashArray);
+		dashArrayNoCommas = dashArray.join('');
+		wordDiv.innerText = dashArrayNoCommas;
   		}  
 	else 
 	{wrongGuess(letter);} 
@@ -35,28 +38,5 @@ for(var i=0; i<word.length; i++) {
  		wrongs.sort();
  	}
  }};
-
-// finds index of letter in word
-	var indices = [];
-	var idx = word.indexOf(letter);
-function findIndicesLetter(word, letter) {
-	while (idx != -1) {
-	  indices.push(idx);
-	  idx = word.indexOf(letter, idx + 1);
-	}  console.log(indices);
-};
-
-// replaces dashes with correct letters
-// using dashArray before it's joined to string
-function replaceDash(dashArray) {
-	findIndicesLetter(word, letter);
-	for (var k = 0; k < indices.length; k++); {
-		dashArray.splice(idx, 1, letter);
-		dashArrayNoCommas = dashArray.join('');
-		wordDiv.innerText = dashArrayNoCommas;
-	}
-};
-
-
 
 
