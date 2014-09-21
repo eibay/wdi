0e44018d6aadec6d12133a4d6fda52339f1a7174
Nastassia <nastassiacarmona@gmail.com>
2014-09-21 18:36:29 -0400

    Wrote js to hook up input guess to check if it matches game word.

---
+++ w07/d05/Homework/ui/js/game.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+  var matches = [];
+  var misses = [];
+  var guess_button = document.querySelector("button");
+  var guessed_letter = document.querySelector("input");
+  var word_div = document.querySelector("div.game-word");
+  var word = word_div.innerText.toLowerCase();
+
+
+  function noDupesInMisses(letter){
+    for(var m=0;m<=misses.length;m++) {
+      if (misses.indexOf(letter) == -1){
+        misses.push(letter);
+      }
+  }
+  }
+
+  function noDupesInMatches(letter){
+   for(var m=0;m<=matches.length;m++) {
+     if (matches.indexOf(letter) == -1){
+       matches.push(letter);
+     }
+  }
+  }
+
+  function matchGuess(letter, word){
+    for(var i=0; i<word.length; i++) {
+    if (letter == word[i]) {
+      noDupesInMatches(letter);
+    }
+  }
+    if (matches.indexOf(letter) == -1){
+      noDupesInMisses(letter);
+    }
+  };
+
+  function was_guessed(letter){
+    if (matches.indexOf(letter) != -1 || misses.indexOf(letter) != -1){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  guess_button.addEventListener("click", function(){
+    var input = guessed_letter.value.toLowerCase();
+    matchGuess(input, word);
+    guessed_letter.value = "";
+    console.log("matches: " + matches);
+    console.log("misses: " + misses);
+  })
+};
