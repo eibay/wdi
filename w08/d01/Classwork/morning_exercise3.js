2d124b328d6669df30d862e276c4bddf4f9cb317
JBiggs91 <joe.biggica@gmail.com>
2014-09-22 18:27:03 -0400

    css hw

--- w08/d01/Classwork/morning_exercise3.js
+++ w08/d01/Classwork/morning_exercise3.js
@@ -1,9 +1,8 @@
 function stringInfo(string) {
 	var hash = {}
 	hash["length"] = string.length
-	hash["original"] = string
-	hash["splitBySlash"] = string.split('/')
+	hash.original = string
+	hash.splitBySlash = string.split('/')
 	return hash
 }
-
 console.log(stringInfo('hello /my name is /joe'))
