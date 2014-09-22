8aa3a70fb0b684ad944f3a115f25042b86455009
neener <janinemrosen@gmail.com>
2014-09-22 11:05:53 -0400

    classwork exercises

---
+++ w08/d01/Classwork/4.js
@@ -0,0 +1,13 @@
+cats = []
+
+request(url, function (error, response, body) {
+     var 
+
+
+
+     response = JSON.parse("www.dummyapi.com/cats");
+     response = JSON.parse.split("/");
+     response = cats.push
+     return = cats.push
+     
+});
