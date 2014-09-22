74fe77c409ed62ca5f10f51e15e7d1e11870b5c3
Nastassia <nastassiacarmona@gmail.com>
2014-09-22 06:30:27 -0400

    Got the counter working, and some starting canvas drawing

--- w07/d05/Homework/ui/js/game.js
+++ w07/d05/Homework/ui/js/game.js
@@ -7,12 +7,61 @@ window.onload = function(){
   var word = word_div.innerText.toLowerCase();
   var guesses_left = document.querySelector("span.guesses-left");
   var letters_guessed = document.querySelector("span.guessed-letters");
+  var counter = 0;
+  var miss_counter = 0;
+  var canvas = document.querySelector('canvas#hangman');
+  var game = document.querySelector('div#game-over');
 
+  function count(){
+    counter += 1;
+    guesses_left.innerText = 8 - counter;
+    if (counter == 8){
+      return game.innerText = "GAME OVER!"
+    }
+  }
+  function missCount(){
+    var missCount = misses.length;
+    if (missCount == 1){
+      drawHead();
+    } else if (missCount == 2){
+      drawTorso();
+    }
+  }
+
+  function drawHead(){
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+      var centerX = canvas.width / 2;
+      var centerY = (canvas.height / 2) - 40;
+      var radius = 15;
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      ctx.fillStyle="#FFF";
+      ctx.fill();
+    }
+  }
+
+    function drawTorso(){
+      if (canvas.getContext){
+        var ctx = canvas.getContext('2d');
+        var centerX = canvas.width / 2;
+        var centerY = (canvas.height / 2) - 40;
+        ctx.beginPath();
+        // ctx.moveTo(0,0);
+        // ctx.lineTo()
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo(centerX, (canvas.height / 2) + 30);
+        ctx.strokeStyle="#FFF";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+      }
+    }
 
   function noDupesInMisses(letter){
     for(var m=0;m<=misses.length;m++) {
       if (misses.indexOf(letter) == -1){
         misses.push(letter);
+        missCount();
       }
   }
   }
@@ -34,6 +83,7 @@ window.onload = function(){
     if (matches.indexOf(letter) == -1){
       noDupesInMisses(letter);
     }
+    count();
   };
 
   function was_guessed(letter){
@@ -47,7 +97,7 @@ window.onload = function(){
   guess_button.addEventListener("click", function(){
     var input = guessed_letter.value.toLowerCase();
     matchGuess(input, word);
-    guesses_left.innerText = "blah"
+    guesses_left.innerText = (8 - counter).toString();
     letters_guessed.innerText = matches + misses;
     guessed_letter.value = "";
     console.log("matches: " + matches);
