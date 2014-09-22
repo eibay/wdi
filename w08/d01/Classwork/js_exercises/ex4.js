64fbe55231b6a5b4d7469127584fa30f1d7e9538
Nastassia <nastassiacarmona@gmail.com>
2014-09-22 11:02:45 -0400

    Correcting ex4

--- w08/d01/Classwork/js_exercises/ex4.js
+++ w08/d01/Classwork/js_exercises/ex4.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
 request.get("http://www.dummyapi.com/cats", function(err, response, body){
-  var api_res = JSON.parse(response);
+  var api_res = JSON.parse(response.body);
   console.log(api_res.length);
 })
