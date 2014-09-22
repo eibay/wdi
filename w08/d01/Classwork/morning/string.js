79ee64aba22b5febb9535829fac01a286349ebb4
HeidiW <foywilliams@gmail.com>
2014-09-22 11:06:19 -0400

    morning work

---
+++ w08/d01/Classwork/morning/string.js
@@ -0,0 +1,7 @@
+/*write a function howbig that takes a string as a parameter. The function returns:
+"small" if the string is less than 3 characters long
+"medium" if it is between 3 and 5 characters long
+"big" if it is greater than 5 characters long*/
+
+
+
