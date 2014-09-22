754cf3653f89adb7ce223a79f544b038406f2209
yoshiemuranaka <yoshiemuranaka@gmail.com>
2014-09-22 11:05:45 -0400

    morning exercises

---
+++ w08/d01/Classwork/jsExercise3.js
@@ -0,0 +1,10 @@
+function stringInfo(string){
+
+	var stringInfoHash = {
+		length: string.length,
+		original: string,
+		splitBySlash: "/" + string
+	};
+
+	return stringInfoHash
+};
---
+++ w08/d01/Classwork/jsExercises.js
@@ -0,0 +1,4 @@
+function makeArray(word1, word2){
+	var wordArray = [word1, word2];
+	return wordArray
+};
