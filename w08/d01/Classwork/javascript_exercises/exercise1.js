b5f797083f231d0b5f7a278eb49efc8ff18a35d1
trishfromtisch <parlaws@gmail.com>
2014-09-22 11:06:08 -0400

    Javascript Exercises

---
+++ w08/d01/Classwork/javascript_exercises/exercise1.js
@@ -0,0 +1,6 @@
+function makeArray(word1, word2) {
+	var array = [];
+	array.push(word1);
+	array.push(word2);
+	return array
+};
---
+++ w08/d01/Classwork/javascript_exercises/exercise2.js
@@ -0,0 +1,9 @@
+function howBig(str) {
+	if (str.length < 3) {
+		return "small"
+	} else if (str.length >= 3 && str.length =< 5) {
+		return "medium"
+	} else {
+		return "big"
+	}
+}
