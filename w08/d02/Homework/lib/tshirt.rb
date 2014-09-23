f8cfb13d5c5fc5097102e070e2567bf06c2f070e
clay-reed-a <clay.reed.a@gmail.com>
2014-09-23 14:25:31 -0400

    set up server's relationship to models

---
+++ w08/d02/Homework/lib/tshirt.rb
@@ -0,0 +1,3 @@
+require 'active_record'
+
+TShirt = Class.new ActiveRecord::Base
--- w08/d02/Homework/server.rb
+++ w08/d02/Homework/server.rb
@@ -1,1 +1,9 @@
 require 'sinatra'
+require_relative "./lib/connection"
+require_relative "./lib/tshirt"
+require_relative "./lib/order"
+
+
+get '/' do 
+  haml :index
+end 
