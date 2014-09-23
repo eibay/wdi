8cae52b01bf42fef8921aa38b04744928e5090cf
clay-reed-a <clay.reed.a@gmail.com>
2014-09-23 16:06:24 -0400

    begin writing routes for orders, write view for order confirmation

--- w08/d02/Homework/server.rb
+++ w08/d02/Homework/server.rb
@@ -7,3 +7,30 @@ require_relative "./lib/order"
 get '/' do 
   haml :index, {locals: {t_shirts: TShirt.all}} 
 end 
+
+get "/orders" do 
+  t_shirt = TShirt.find_by_id params["tshirt_id"] 
+  total_price = tshirt.price.to_f *
+  params["tshirt_quanity"].to_i 
+
+  order = Order.new(
+    tshirt_quanity: params["tshirt_quanity"],
+         tshirt_id: params["tshirt_id"]
+             total: total_price
+  )
+
+  haml :order, {locals: {order: order, t_shirt: t_shirt}}
+end 
+
+post "/orders" do
+  order = Order.new( 
+            emails: params["email"], 
+         tshirt_id: params["tshirt_id"], 
+    tshirt_quanity: params["tshirt_quanity"], 
+             total: params["total"]
+  )
+
+  order.save 
+
+  haml :reciept, {locals: order}
+end 
