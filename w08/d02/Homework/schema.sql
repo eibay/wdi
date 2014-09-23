7066b27f5067468345497fde008d4d32a99947ec
clay-reed-a <clay.reed.a@gmail.com>
2014-09-23 14:18:03 -0400

    correct integer to decimal

--- w08/d02/Homework/schema.sql
+++ w08/d02/Homework/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tshirts (
   id SERIAL PRIMARY KEY,
   available_quanity INTEGER, 
-  price INTEGER(7,2), -- max price 99999.99, we're a family company 
+  price DECIMAL(7,2), -- max price 99999.99, we're a family company 
   img_src VARCHAR(225)
 );
 
@@ -10,5 +10,5 @@ CREATE TABLE orders (
   email VARCHAR(225),
   tshirt_id INTEGER,
   tshirt_quanity INTEGER, 
-  total INTEGER(9,2) -- no orders over 9999999.99, see above 
+  total DECIMAL(9,2) -- no orders over 9999999.99, see above 
 );
