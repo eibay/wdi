25a8b821a11044fd7427dd50693d0b23938a183e
shotarokamegai <shotaro326@gmail.com>
2014-09-24 09:43:59 -0400

    didhomework

---
+++ w08/d02/Homework/tshirtly/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tshirts (
+	id serial primary key,
+	name varchar(50),
+	price varchar(50),
+	quantity integer,
+	image varchar(200)
+);
+
+CREATE TABLE users (
+	id serial primary key,
+	tshirt_id integer,
+	quantity integer,
+	email varchar(30)
+);
