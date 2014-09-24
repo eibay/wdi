5566f6bf20c405dd81d3df636e13ea5b34fd6652
clay-reed-a <clay.reed.a@gmail.com>
2014-09-24 10:41:22 -0400

    draft returnJoke

---
+++ w08/d03/Homework/ex01.js
@@ -0,0 +1,3 @@
+function returnJoke() {
+  return "There's a video trending on the Huffington Post as I write this--no joke--with the headline: \"This Deer Thought No One Was Watching It Fart, Now the Whole World Knows.\" I promise you, Reddit was watching it fart first.";
+}
