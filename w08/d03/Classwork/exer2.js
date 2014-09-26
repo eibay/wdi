12089924f350ede82d0c2cd671b6275dd00a3208
lisajwells <lisawells999@gmail.com>
2014-09-26 13:24:14 -0400

    exercise2 8 returns names in JSON from function

--- w08/d03/Classwork/exer2.js
+++ w08/d03/Classwork/exer2.js
@@ -4,8 +4,8 @@
 //++4 Change the hello world route to '/hello'
 //++5 Add a route '/yo' that says "yo world" (with no additional files)
 //++6 Add CSS to style the hello route, but no CSS in a file
+//++7 Every route returns an array of names, in json
 
-//7 Every route returns an array of names, in json
 //8 Every route returns json of names that was returned from a function
 //9 Every route calls a function that returns an array of 3 people, where each person is represented by a hash of attributes (e.g. name, age, etc)
 //10 Two routes - one for names, one for people
@@ -20,12 +20,21 @@ var names = ["Eddie", "Becky", "Pasha", "David"]
 var server = http.createServer(function(req, response) {
 	console.log(req.url);
 	if (req.url == '/hello') {
-		response.end(JSON.stringify(names));
+		response.end(returnNames(names));
 	}
 	else if (req.url == '/yo') {
-		response.end(JSON.stringify(names));
+		response.end(returnNames(names));
 	}
 	else response.end('error');
 
 });
 server.listen(2000);
+
+function returnNames(array) {
+	namesResult = JSON.stringify(array);
+	return namesResult;
+}
+
+
+
+
