06cdf1c30cf93562b8adf0bdf1fe1a456edc5116
lisajwells <lisawells999@gmail.com>
2014-09-26 15:58:27 -0400

    exercise2 10 ... 2 routes, one for names, one for people

--- w08/d03/Classwork/exer2.js
+++ w08/d03/Classwork/exer2.js
@@ -6,8 +6,8 @@
 //++6 Add CSS to style the hello route, but no CSS in a file
 //++7 Every route returns an array of names, in json
 //++8 Every route returns json of names that was returned from a function
+//++9 Every route calls a function that returns an array of 3 people, where each person is represented by a hash of attributes (e.g. name, age, etc)
 
-//9 Every route calls a function that returns an array of 3 people, where each person is represented by a hash of attributes (e.g. name, age, etc)
 //10 Two routes - one for names, one for people
 //11 Use the node-simple-router library to simplify the above
 var http = require('http');
@@ -19,16 +19,27 @@ var people = [{name: "Eddie", age: 36, favorite: "beer"}, {name: "Becky", age: 3
 
 var server = http.createServer(function(req, response) {
 	console.log(req.url);
-	if (req.url == '/hello') {
-		response.end(returnPeople(people));
+	if (req.url == '/names') {
+		response.end(returnNames(people));
 	}
-	else if (req.url == '/yo') {
+	else if (req.url == '/people') {
 		response.end(returnPeople(people));
 	}
 	else response.end('error');
 
 });
 server.listen(2000);
+
+function returnNames(array) {
+	var names = [];
+	for (var i=0; i < array.length; i++) {
+		names.push(array[i]["name"]);
+
+	}
+	var namesResult = JSON.stringify(names);
+	return namesResult;
+}
+
 
 function returnPeople(array) {
 	var new_people = [];
@@ -37,11 +48,6 @@ function returnPeople(array) {
 	}
 		
 		return JSON.stringify(new_people);
-}
-
-function returnNames(array) {
-	var namesResult = JSON.stringify(array);
-	return namesResult;
 }
 
 
