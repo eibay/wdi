d91785b39741f8bc5fd620a89c5bb969634a54ad
lisajwells <lisawells999@gmail.com>
2014-09-26 08:28:06 -0400

    exercise2 4

--- w08/d03/Classwork/exer2.js
+++ w08/d03/Classwork/exer2.js
@@ -1,22 +1,25 @@
-// Create a server that just returns "Hello World" (for every route)
-// Route that returns Hello World wrapped in a marquee tag
-// Route that returns Hello World in a marquee tag, but read from a file
-// Change the hello world route to '/hello'
-// Add a route '/yo' that says "yo world" (with no additional files)
-// Add CSS to style the hello route, but no CSS in a file
-// Every route returns an array of names, in json
-// Every route returns json of names that was returned from a function
-// Every route calls a function that returns an array of 3 people, where each person is represented by a hash of attributes (e.g. name, age, etc)
-// Two routes - one for names, one for people
-// Use the node-simple-router library to simplify the above
+//++1 Create a server that just returns "Hello World" (for every route)
+//++2 Route that returns Hello World wrapped in a marquee tag
+//++3 Route that returns Hello World in a marquee tag, but read from a file
+//++4 Change the hello world route to '/hello'
+//5 Add a route '/yo' that says "yo world" (with no additional files)
+//6 Add CSS to style the hello route, but no CSS in a file
+//7 Every route returns an array of names, in json
+//8 Every route returns json of names that was returned from a function
+//9 Every route calls a function that returns an array of 3 people, where each person is represented by a hash of attributes (e.g. name, age, etc)
+//10 Two routes - one for names, one for people
+//11 Use the node-simple-router library to simplify the above
 var http = require('http');
 var url = require('url');
 var request = require('request');
 var fs = require('fs');
 
 var server = http.createServer(function(req, response) {
+	console.log(req.url)
+	if (req.url == '/hello') {
 
-response.end("<html><marquee>Hello World</marquee></html>");
+	response.end(fs.readFileSync("exer2.html"));
+	}
 
 });
 server.listen(2000);
