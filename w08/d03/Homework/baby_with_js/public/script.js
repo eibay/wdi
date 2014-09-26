315368c73ef23f14500645ed839a34c2d375fc62
clay-reed-a <clay.reed.a@gmail.com>
2014-09-26 15:45:40 -0400

    jQuerification complete

--- w08/d03/Homework/baby_with_js/public/script.js
+++ w08/d03/Homework/baby_with_js/public/script.js
@@ -3,17 +3,14 @@ $(document).ready(function() {
   var request = new XMLHttpRequest();
   // didn't about how to communicate to the server 
   // what year we were getting so get it from present document 
-  var secondHeader = document.querySelector("header h2"); 
-  var headerText = secondHeader.innerText; 
+  var secondHeader = $("header h2"); 
+  var headerText = secondHeader.text(); 
   var year = headerText.split(' ')[3].replace('!', ''); 
   // have a route set up on the server giving JSON array of babies
   // gets the year param from the url 
-  var requestURL = "http://localhost:4567/json/" + year;  
-  request.open('GET', requestURL, true);
-  request.send(null);
-  request.addEventListener('load', function() {
-    var babies = JSON.parse(request.response);
-    for (var b = 0; b < babies.length; b++) {
+  var jsonRequestUrl = "http://localhost:4567/json/" + year;
+  $.getJSON(jsonRequestUrl, null, function(babies) {
+     for (var b = 0; b < babies.length; b++) {
       var baby = babies[b];
       var babyRow = createBabyRow(baby); 
       babyTable.append(babyRow); 
@@ -26,12 +23,12 @@ function createBabyRow(baby) {
   // how to find attr & assign them to 
   // corresponding th dynamically? 
   var attrs = ["name", "county", "gender", "count"]; 
-  var babyRow = document.createElement("tr"); 
+  var babyRow = $("<tr></tr>"); 
   for (var a = 0; a < attrs.length; a++) {
     var attr = attrs[a];
-    var data = document.createElement("td"); 
-    data.innerText = baby[attr]; 
-    babyRow.appendChild(data); 
+    var data = $("<td></td>"); 
+    data.text(baby[attr]); 
+    babyRow.append(data); 
   }
   return babyRow; 
 }
