4922afca5bcd01191d1faf1412ddeb9abe9ede27
lisajwells <lisawells999@gmail.com>
2014-09-27 13:58:22 -0400

    exer3 1 (html to browser and to console.log)

--- w08/d03/Classwork/exer3.js
+++ w08/d03/Classwork/exer3.js
@@ -5,11 +5,14 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 	console.log("connection made")
 
+// wait *** how does html console.log? All I can do is both?
+
 	var path = request['url'];
 	console.log("client is attempting to connect from");
 
 	if (path == '/') {
-		console.log("Lisa's name");
+		response.end("<html><p>Lisa's name</p></html>")
+		console.log("<html><p>Lisa's name</p></html>");
 	};
 
 
