8fcdfc096483c4bd3b67b6761fa2e24b4a3aaebb
HeidiW <foywilliams@gmail.com>
2014-09-28 14:25:02 -0400

    Part one is working again

--- w08/d03/Homework/server.rb
+++ w08/d03/Homework/server.rb
@@ -1,20 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
-require 'httparty'
-require 'active_record'
 require 'json'
 
-get("/") do
+get('/') do
 	names = JSON.parse(File.read('./public/ny-baby-names.json'))
 	year = params[:year]
-	binding.pry
 	
-	year = names["data"].map do |y|
+	years = names["data"].map do |y|
 		y[8]
 	end
 
-	unique_years = year.uniq.sort
+	unique_years = years.uniq.sort
 
 	erb(:index, { locals: { unique_years: unique_years, names: names, year: year } })
 end
