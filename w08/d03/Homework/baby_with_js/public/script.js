528375e47a4892f5bb270518c546b43a98cc112e
clay-reed-a <clay.reed.a@gmail.com>
2014-09-28 11:54:58 -0400

    generalized adjustBabyTable routine, follow jQuery naming conventions, use jQuery to add an eventListener to both dropDowns

--- w08/d03/Homework/baby_with_js/public/script.js
+++ w08/d03/Homework/baby_with_js/public/script.js
@@ -1,64 +1,60 @@
 $(document).ready(function() {
-  var babyTable = $("table");
-  // get year from the current location's path using jQuery 
+  adjustBabyTable(); 
+
+  var $dropDowns = $("select.gender, select.county"); 
+
+  $dropDowns.change(function() {
+    adjustBabyTable();  
+  });
+});
+
+function adjustBabyTable() {
+
+  // AJAX variables 
   var year = $(location).attr("pathname").replace("/years/", '');
   var jsonRequestUrl = "http://localhost:4567/json/";
   var requestParams = {"year": year}; 
+
+  // DOM variables 
+  var $babyTable = $("table");
+  var countyDropdown = document.querySelector("select.county");
+  var genderDropdown = document.querySelector("select.gender");
+ 
+
+  destroyAllBabyRows(); 
   $.getJSON(jsonRequestUrl, requestParams, function(babies) {
     $.each(babies, function(b, baby) {
-      var babyRow = createBabyRow(baby); 
-      babyTable.append(babyRow); 
-    });
-  });
+      // what kind of table are we building? 
+   
+      var gender = genderDropdown.value; 
+      var genderIsBlank = gender == '-';
+      var babyHasThisGender = baby["gender"] == gender; 
 
-  var genderDropdown = document.querySelector("select.gender");
-  genderDropdown.addEventListener('change', function(e) {
-    var gender = genderDropdown.value;
-    destroyAllBabyRows() 
-    $.getJSON(jsonRequestUrl, requestParams, function(babies) {
-      $.each(babies, function(b, baby) {
-        var genderIsBlank = gender == '-'; 
-        var babyHasThisGender = baby["gender"] == gender; 
-        var countyIsBlank = countyDropdown.value == '-';  
-        var babyHasThisCounty = baby["county"] == countyDropdown.value; 
-        if (genderIsBlank || babyHasThisGender)
-          if (countyIsBlank || babyHasThisCounty)
-            babyTable.append(createBabyRow(baby)); 
+      var county = countyDropdown.value; 
+      var countyIsBlank = county == '-'; 
+      var babyHasThisCounty = baby["county"] == county;  
 
-      }); 
-    });    
-  }); 
-
-  var countyDropdown = document.querySelector("select.county"); 
-  countyDropdown.addEventListener('change', function(e) {
-    var county = countyDropdown.value; 
-    destroyAllBabyRows() 
-    $.getJSON(jsonRequestUrl, requestParams, function(babies) {
-      $.each(babies, function(b, baby) {
-        var countyIsBlank = county == '-';
-        var babyHasThisCounty = baby["county"] == county; 
-        var genderIsBlank = genderDropdown.value == '-'; 
-        var babyHasThisGender = baby["gender"] == genderDropdown.value; 
+      // select from the babies only the babies we need 
+      if (genderIsBlank || babyHasThisGender) 
         if (countyIsBlank || babyHasThisCounty) 
-          if (genderIsBlank || babyHasThisGender)  
-            babyTable.append(createBabyRow(baby)); 
-      });
+          $babyTable.append(createBabyRow(baby));  
     }); 
   }); 
-});
+}
+
 
 function createBabyRow(baby) {
   // hard-coded array in order of th 
   // how to find attr & assign them to 
   // corresponding th dynamically? 
   var attrs = ["name", "county", "gender", "count"]; 
-  var babyRow = $("<tr></tr>");
+  var $babyRow = $("<tr></tr>");
   $.each(attrs, function(a, attr) {
-    var data = $("<td></td>"); 
-    data.text(baby[attr]); 
-    babyRow.append(data); 
+    var $data = $("<td></td>"); 
+    $data.text(baby[attr]); 
+    $babyRow.append($data); 
   }); 
-  return babyRow; 
+  return $babyRow; 
 }
 
 function destroyAllBabyRows() {
