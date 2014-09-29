b37480ea1621a1f6e505740464cf7819f6ddc025
theerickramer <theerickramer@gmail.com>
2014-09-29 15:59:43 -0400

    js fun

--- w09/d01/Classwork/router_view_starter/javascripts/app.js
+++ w09/d01/Classwork/router_view_starter/javascripts/app.js
@@ -1,11 +1,50 @@
+var IndexView = Backbone.View.extend({
+		initialize: function(){
+			console.log('A new Index view has been created');
+			this.render();
+
+		},
+
+		render: function(){
+			this.$el.html('<h2>Welcome to our page!</h2>').hide().fadeIn(600);
+		}
+	});
+
+		var CreateView = Backbone.View.extend({
+		initialize: function(){
+			console.log('A new Create view has been created')
+			this.render();
+		},
+
+		render: function(){
+			var header = $('header');
+			header.html('<h2>Create stuff!</h2>').hide().fadeIn(600);
+		}
+	})
+
 $(function(){
 
   var AppRouter = Backbone.Router.extend({
     routes: {
+    	"": "index",
+    	"create": "create"
     }
   });
 
-  var appRouter = new AppRouter;
+  var router = new AppRouter;
+
+  router.on('route:index', function(){
+  	var header = $('header');
+  	var indexView = new IndexView({el: header});
+  	// var header = $('header');
+  	// header.html('<h2>Welcome to our page!</h2>');
+  });
+
+  router.on('route:create', function(){
+  	var createView = new CreateView();
+  	// var header = $('header');
+  	// header.html('<h2>Create stuff!</h2>');
+  });
 
   Backbone.history.start();
 
