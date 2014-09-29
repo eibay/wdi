1d968e893e1fa728980f9f8ffa12c840f997887f
DanielFarber <farber.daniel.m@gmail.com>
2014-09-29 11:37:57 -0400

    Finished the weekend's bonus in w08/d03/Homework/jsbrowser3

--- w08/d03/Homework/jsbrowser3/public/script.js
+++ w08/d03/Homework/jsbrowser3/public/script.js
@@ -1,19 +1,22 @@
 
 
 window.onload = function() {
-	generateButtons()
+var master = jQuery.get("/names", success, "json")	
 }
 
-var master = jQuery.get("/names", success, "json")
 
 
 
 function success(event) {
+	generateButtons()
 	var yearNames = extractByYear(event.data)
+	populateLists(yearNames)
+	createTable()
+	displayTable(yearNames)
 }
 
 function generateButtons(array) {
-	$("body").append("<div class='container'><div class='row'><div class='col-md-1 col-md-offset-4'><button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>County <span class='caret'></span></button><ul class='dropdown-menu county_list' role='menu'></ul></div><div class='col-md-1 col-md-offset-1'><button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>Gender <span class='caret'></span></button><ul class='dropdown-menu gender_list' role='menu'></ul></div></div></div>")
+	$("body").append("<div class='container'><div class='row'><div class='col-md-1 col-md-offset-4 btn-group'><button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>County <span class='caret'></span></button><ul class='dropdown-menu county_list' role='menu'></ul></div><div class='col-md-1 col-md-offset-1'><button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>Gender <span class='caret'></span></button><ul class='dropdown-menu gender_list' role='menu'></ul></div></div></div>")
 }
 
 function extractByYear(array) {
@@ -24,7 +27,19 @@ function extractByYear(array) {
 	return output
 }
 
-function populate_county_lists(array) {
+function createTable() {
+	$(".container").append("<div class='row'><header class='col-md-8 col-md-offset-2'><table class='table table-bordered table-striped'><thead><tr><th data-field='name'>Name</th><th data-field='county'>County</th><th data-field='gender'>Gender</th><th data-field='count'>Count</th></tr></thead></table></header></div>")	
+}
+
+function displayTable(array) {
+	array.forEach(function(item) {
+		$(".table").append("<tr id='last'>")
+		$("#last").append("<td>" + item[9] + "</td>").append("<td>" + item[10] + "</td>").append("<td>" + item[11] + "</td>").append("<td>" + item[12] + "</td>")
+		$("#last").removeAttr("id", null)
+	})
+}
+
+function populateLists(array) {
 	var strings = []
 	array.forEach(function(item) {
 		if (_.contains(strings, item[10])) {}
@@ -34,6 +49,28 @@ function populate_county_lists(array) {
 		return [item.charCodeAt(0), item.charCodeAt(1), item.charCodeAt(2), item.charCodeAt(3)]
 	})
 	strings.forEach(function(county) {
-	$(".county_list").append("<li><a href='#'>" + county + "</a></li>")
+	$(".county_list").append("<li><a class='county_item' href='#"+ county + "'>" + county + "</a></li>")
 	})
+	$(".county_item").click(array, displayCountyTable)
+	$(".gender_list").append("<li><a class='gender_item' href='#male'>Male</a></li>").append("<li><a class='gender_item' href='#female'>Female</a></li>")
+	$(".gender_item").click(array, displayGenderTable)
 }
+
+function displayCountyTable(feed) {
+	$("tbody").detach()
+	var list = _.filter(feed.data, function(item){
+		return item[10] == feed.target.innerText
+	})
+	displayTable(list)
+}
+
+function displayGenderTable(feed) {
+	$("tbody").detach()
+	var list = _.filter(feed.data, function(item){
+		return item[11] == feed.target.innerText[0]
+	})
+	displayTable(list)
+}
+
+
+
--- w09/d01/Classwork/router_view_starter/javascripts/app.js
+++ w09/d01/Classwork/router_view_starter/javascripts/app.js
@@ -2,10 +2,20 @@ $(function(){
 
   var AppRouter = Backbone.Router.extend({
     routes: {
+    	"": "index",
+    	"#create": "targ",
     }
   });
 
-  var appRouter = new AppRouter;
+  var router = new AppRouter;
+
+  router.on("route:index", function(){
+  	console.log("Welcome to our page!")
+  })
+
+  router.on("route:targ"), function(){
+  	console.log("Created!")
+  }
 
   Backbone.history.start();
 
