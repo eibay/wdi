a6917c037747921e19b18e361973a79d4e7d3458
jortenberg <jill.ortenberg@gmail.com>
2014-09-29 15:26:59 -0400

    blah

---
+++ w09/d01/Classwork/router_view_starter/javascripts/app.js
@@ -0,0 +1,50 @@
+// below is a hash?
+  var IndexView = Backbone.View.extend({
+    initialize: function(){
+      console.log("A new index view has been created!");
+    },
+    render: function(){
+      var header = $("header");
+      header.html("<h2>Welcome to our page!</h2>");
+    }
+  });
+
+  var CreateView = Backbone.View.extend({
+    initialize: function(){
+      console.log("Welcome to the Create Page!");
+    },
+    render: function(){
+      var header = $("header");
+      header.html("<h2>Welcome to our create page!</h2>");
+    }
+  });
+
+// This is a router, not a server
+// jquery way of doing onload
+$(function(){
+
+// make an object variable router(Like active record), that inherits all backbones functions
+// Backbone does not use slash like sinatra, root is empty string
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+    	"": "index",
+    	"create": "create"
+    }
+  });
+// making an instance variable of the object AppRouter
+  var router = new AppRouter;
+
+// listening for the root route to be hit, then executing what is inside the function (console.log welcome)
+	router.on('route:index', function() {
+		var indexView = new IndexView();
+    indexView.render();
+	});
+
+	router.on('route:create', function() {
+    var createView = new CreateView();
+    createView.render();
+	});
+
+  Backbone.history.start();
+
+});
