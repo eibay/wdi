95e2dd4e55ee0cf8b0bb3fd472248835d1be328c
im11041991 <i.martinez8@umiami.edu>
2014-09-29 15:25:13 -0400

    pulling hw instructions

--- w09/d01/Classwork/router_view_starter/javascripts/app.js
+++ w09/d01/Classwork/router_view_starter/javascripts/app.js
@@ -1,11 +1,46 @@
-$(function(){
+	var IndexView = Backbone.View.extend({
+		initialize: function(){
+				console.log("A new Index view has been created ");
+				this.render()
+		},
+		render: function(){
+			var header = $('header')
+			this.$el.html("<h2>Welcome to our page! </h2>");
+		}
+	});
 
+	var CreateView = Backbone.View.extend({
+		initialize: function(){
+			console.log("A new Create view has been created");
+			this.render()
+		},
+		render: function(){
+			var header = $('header')
+			header.html("<h2>CREATE!</h2>");
+		}
+	})
+
+
+
+$(function(){
   var AppRouter = Backbone.Router.extend({
     routes: {
+    	"": "index",
+    	"create": "create",
     }
   });
 
-  var appRouter = new AppRouter;
+  var router = new AppRouter;
+
+  router.on('route:index', function(){
+  	var indexView = new IndexView({el: header});
+  	console.log("Welcome to our page!");
+  })
+
+  router.on('route:create', function(){
+  	var createVIew = new CreateView
+  	console.log("Create some stuff!");
+  })
 
   Backbone.history.start();
 
