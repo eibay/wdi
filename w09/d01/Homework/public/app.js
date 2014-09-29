0c8fd1d29292cfb703b24852a16d8449a5a0cad2
clay-reed-a <clay.reed.a@gmail.com>
2014-09-29 18:18:14 -0400

    handle multiple Ajax requests asynchronously

--- w09/d01/Homework/public/app.js
+++ w09/d01/Homework/public/app.js
@@ -10,6 +10,7 @@ $(function(){
 
   var router = new AppRouter(); 
 
+  // route to say hello 
   router.on("route:helloer", function(name) {
     var name = capitalize(name);  
     var main = document.querySelector("main"); 
@@ -24,10 +25,49 @@ $(function(){
     } 
   });
 
+  router.on("route:movies", function() {
+    var movies = [
+      "His Girl Friday", 
+      "What Happened Was...", 
+      "Pontypool"
+    ];
+
+    var requestArray = []; 
+    for (var i = 0; i < movies.length; i++) {
+
+      var apiUrl = "http://www.omdbapi.com/";
+      var queryParams = "?t=" + encodeURIComponent(movies[i]); 
+      var apiQueryUrl = apiUrl + queryParams; 
+      
+
+      var request = new XMLHttpRequest(); 
+      requestArray.push(request);
+
+      requestArray[i].open("GET", apiQueryUrl, true); 
+      requestArray[i].send(null); 
+      requestArray[i].addEventListener('load', function(e) {
+        var movie = JSON.parse(e.currentTarget.response); 
+        addMovie(movie.Title, movie.Year, movie.Poster);
+      });
+    }
+  }); 
+
   Backbone.history.start();
 
 });
 
 function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1); 
+}
+
+function addMovie(title, year, poster) {
+  var main = document.querySelector("main");
+  var div = document.createElement("div"); 
+  var posterImg = document.createElement("img");
+  posterImg.src = poster; 
+  div.appendChild(posterImg); 
+  var titleHeader = document.createElement("h3");
+  titleHeader.innerText = title + " (" + year + ')'; 
+  div.appendChild(titleHeader); 
+  main.appendChild(div); 
 }
