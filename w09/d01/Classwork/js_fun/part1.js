6ed18e565bea83ebcbf5233bd62c5f8fcc549b5e
brendee <brenda.dargan@gmail.com>
2014-09-29 15:59:39 -0400

    Added afternoon exercises

---
+++ w09/d01/Classwork/js_fun/part1.js
@@ -0,0 +1,16 @@
+// Write a function that takes an array of integers and an integer as arguments. The function should return true if the given integer appears in the array, and false if it does not. do not use [].indexOf
+
+// Ie.
+
+// inArray([2,4,5], 4);
+// // true
+
+// inArray([8,3,4], 7);
+// // false
+
+function inArray(array, num){
+	 array.contains(num) 
+	return true
+};
+
+inArray([2,4,5], 4);
