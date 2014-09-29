ca15b85a1c2b7989e62c8b1dd3340cd005d15fe5
HeidiW <foywilliams@gmail.com>
2014-09-29 15:59:32 -0400

    What I got done for the test

--- w08/d03/Homework/baby_with_js/babynames.js
+++ w08/d03/Homework/baby_with_js/babynames.js
@@ -1,1 +1,12 @@
-var 
+var xhr = new XMLHttpRequest;
+
+xhr.open('GET', './public/ny-baby-names.json', true)
+
+xhr.send();
+
+/*xhr.addEventListener('load', function(e) {
+	var parsedResponse = JSON.parse(xhr.response);
+	var url = document.querySelector("")
+}
+	)*/
+
---
+++ w09/d01/Classwork/js_fun/js_fun.js
@@ -0,0 +1,8 @@
+/*Write a function that takes an array of integers and an integer as arguments. The function should 
+return true if the given integer appears in the array, and false if it does not. */
+
+function inArray([1..9], integer){
+	if integer = [i][1..9],
+		return true
+} else if 
+        return false
