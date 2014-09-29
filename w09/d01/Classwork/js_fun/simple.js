0fe13fe5efcd000b7081be6db6fddb731326829e
clay-reed-a <clay.reed.a@gmail.com>
2014-09-29 16:10:09 -0400

    get enough of the challenge working I can move on

--- w09/d01/Classwork/js_fun/simple.js
+++ w09/d01/Classwork/js_fun/simple.js
@@ -18,21 +18,26 @@ window.addEventListener('load', function() {
   });
   var zipcodeInput = document.querySelector('input');
   zipcodeInput.addEventListener('keyup', function() {
-    var apiResponseJSON = JSON.parse(request.response);
-    var hotspotData = apiResponseJSON["data"];
-    var numListed = 0;
-    while (numListed < 10) {
-      for (var j = 0; j < hotspotData.length; j++) {
-        var hotspot = hotspotData[j];
-        var hotspotZip = hotspot[15];
-        var hotspotName = hotspot[9];
-        var hotspotAddr = hotspot[10]; 
-        if (hotspotZip == zipcodeInput.value) {
-          listHotspot(hotspotName, hotspotAddr); 
-          numListed++; 
-        }
+    var request = new XMLHttpRequest();
+    request.open('GET', "https://data.cityofnewyork.us/api/views/4u6b-frhh/rows.json", true);
+    request.send(null);
+    request.addEventListener('load', function() {
+      if (zipcodeInput.value.length == 5) {
+        var apiResponseJSON = JSON.parse(request.response);
+        var hotspotData = apiResponseJSON["data"];
+        var numListed = 0;
+        for (var j = 0, numListed = 0; j < hotspotData.length, numListed < 10; j++) {
+          var hotspot = hotspotData[j];
+          var hotspotZip = hotspot[15];
+          var hotspotName = hotspot[9];
+          var hotspotAddr = hotspot[10]; 
+          if (hotspotZip == zipcodeInput.value) {
+            listHotspot(hotspotName, hotspotAddr); 
+            numListed = numListed + 1; 
+          }
+        } 
       }
-    } 
+    });
   }); 
 });
 
