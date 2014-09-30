be4adf9595280c2ef62e441b2e717a51b7772859
clay-reed-a <clay.reed.a@gmail.com>
2014-09-30 11:01:17 -0400

    function to add item to DOM

--- w09/d02/Classwork/public/script.js
+++ w09/d02/Classwork/public/script.js
@@ -1,14 +1,19 @@
 $(function(){
-
-  var $theGroceryList = $("ul"); 
   var $theItemInput = $("input#grocery_item");
   var $button = $("section.add button"); 
   $button.on('click', function() {
     var item = $theItemInput.val(); 
     var params = {"item": item}; 
-    console.log(item); 
     $.post("/items", params, function(response) {
       console.log(response); 
     }); 
+    addItemToList(item); 
   });   
 });
+
+function addItemToList(itemStr) {
+  var $theGroceryList = $("ul"); 
+  var $listItem = $("<li></li>"); 
+  $listItem.text(itemStr);
+  $theGroceryList.append($listItem); 
+}
