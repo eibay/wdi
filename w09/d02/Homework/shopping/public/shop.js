3528c1dbcf78a26751ade8322d214e62504d4163
lisajwells <lisawells999@gmail.com>
2014-09-30 23:31:59 -0400

    hw, can strikethrough purchased on list

--- w09/d02/Homework/shopping/public/shop.js
+++ w09/d02/Homework/shopping/public/shop.js
@@ -1,31 +1,42 @@
 var shopList = ["milk", "eggs", "bacon"];
-
+var $list;
 function makeList(shopList){
-	var $list = $("ul");
+	$list = $("ul");
 	for (i = 0; i < shopList.length; i++) {
-		$list.append("<li>" + shopList[i] + "</li>");
+		$list.append("<li><input type='checkbox' id='strikeItem'>" + shopList[i] + "</li>");
 	} 
 };
+
+function markPurchased() {
+	var listItems = $('ul').children();
+	for(var i = 0; i < listItems.length; i++){
+		if (listItems[i].children[0].checked == true){
+		$("li").eq(i).addClass('strike');
+		};
+	}
+}
+
+function addItem() {
+	var input = $('#itemAdd').val();
+	var ul = $('ul');
+	ul.append("<li><input type='checkbox' id='strikeItem'>" + input + "</li>");
+}
+
+
 
 $(function(){
 
 makeList(shopList);
 
-
-var button =$('button');
+var button =$('button#add');
 button.on("click", function() {
 	addItem();
-
 })
 
-
-function addItem() {
-	var input = $('#itemAdd').val();
-	var ul = $('ul');
-	ul.append("<li>" + input + "</li>");
-}
-
-
+var purchasedButton = $('button#purchased');
+	purchasedButton.on("click", function() {
+	markPurchased();
+});
 
 
 
