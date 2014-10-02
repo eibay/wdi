8c24fe0ee32d81391eb7b4719dbd5c1af281e057
tdshap <Sue@TessShapiro.local>
2014-10-02 09:07:02 -0400

    to do app with backbone views, models, and collections

--- w09/d02/Classwork/grocery list/server.rb
+++ w09/d02/Classwork/grocery list/server.rb
@@ -9,20 +9,52 @@ after do
   ActiveRecord::Base.connection.close
 end
 
-get("/") do
+get("/") do	
 	erb(:index, {locals: { list: ListItem.all }})
 end
 
-post("/add_item") do 
-	content_type :json
-	list_item = params["list"]
-	a = ListItem.create({item: list_item})
+# post("/add_item") do 
+# 	binding.pry
+# 	content_type :json
+# 	list_item = params["list"]
+# 	quantity = params["quantity"]
+# 	a = ListItem.create({item: list_item, quantity: quantity})
+# 	a.to_json
+# end 
+
+# delete("/delete") do 
+
+# 	item_to_delete = params["item"]
+# 	a = ListItem.find_by(id: item_to_delete)
+# 	a.destroy
+# end 
+
+# get ("/all_items") do 
+# 	all_todos = ListItem.all.to_json   
+# 	all_todos
+# end 
+
+# routes using backbone model 
+post("/items") do 
+
+	# content_type :json
+	attributes = JSON.parse(request.body.read)
+	list_item = attributes["item"]
+	quantity = attributes["quantity"]
+	a = ListItem.create({item: list_item, quantity: quantity})
 	a.to_json
 end 
 
-delete("/delete") do 
-
-	item_to_delete = params["item"]
+delete("/items/:id") do 
+	item_to_delete = params["id"]
 	a = ListItem.find_by(id: item_to_delete)
 	a.destroy
 end 
+
+get ("/items") do 
+	all_todos = ListItem.all.to_json   
+	all_todos
+end 
+
+
+
