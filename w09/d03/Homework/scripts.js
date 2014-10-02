35f802716dffc149fe1ca867df4e1ded6e55176a
neener <janinemrosen@gmail.com>
2014-10-02 09:28:46 -0400

    ajax call img trouble

--- w09/d03/Homework/scripts.js
+++ w09/d03/Homework/scripts.js
@@ -1,26 +1,38 @@
-window.addEventListener('load', function() {
-  var button = document.querySelector('button');
-  button.addEventListener('click', function(){
-    var xhr = new XMLHttpRequest;
-
-    xhr.open('GET', 'http://api.randomuser.me/', true);
-    xhr.send();
-    xhr.addEventListener('load', function(){
-      var response = JSON.parse(xhr.response)
-      var picture = response.results[0].user.picture;
-      for (var i = 0; i < picture.length; i++){
-              showPicture(picture[i]);
-      }
-    });
-  });
 
 function showPicture(picture){
    debugger
   var unorderedList = document.querySelector("ul");
   var listItem = document.createElement("li");
-  listItem.innerText = picture;
+  var img = document.createElement("img");
+  img.attr('src', picture);
+  listItem.appendChild(img);
+  // listItem.innerText = picture;
   unorderedList.appendChild(listItem);
 };
+
+window.addEventListener('load', function() {
+
+  var button = document.querySelector('button');
+  button.addEventListener('click', function(){
+    console.log("button clicked");
+    var xhr = new XMLHttpRequest;
+
+    xhr.open('GET', 'http://api.randomuser.me/', true);
+ 
+    xhr.addEventListener('load', function(){
+      var response = JSON.parse(xhr.response)
+            debugger
+      var picture = response.results[0].user.picture.large;
+
+      // for (var i = 0; i < picture.length; i++){
+      showPicture(picture);
+      // }
+    });
+
+    xhr.send();
+  });
+
+
 
 });
 
