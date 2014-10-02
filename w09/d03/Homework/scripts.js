5a57f9c5f4e5e2a0dd9c7f9e8f83601657e895c8
neener <janinemrosen@gmail.com>
2014-10-02 11:15:55 -0400

    workinn it randomapi

--- w09/d03/Homework/scripts.js
+++ w09/d03/Homework/scripts.js
@@ -1,14 +1,25 @@
 
-function showPicture(picture){
-   debugger
+function showPicture(pic, first){
+   // debugger
   var unorderedList = document.querySelector("ul");
   var listItem = document.createElement("li");
+  // var img = document.querySelector("img");
+
   var img = document.createElement("img");
-  img.attr('src', picture);
+  var pTag = document.createElement("p");
+  pTag.innerText=first
+  // img.src = picture;
+  img.setAttribute('src', pic)
+ 
+  listItem.appendChild(pTag);
   listItem.appendChild(img);
+
+
+ 
   // listItem.innerText = picture;
   unorderedList.appendChild(listItem);
 };
+
 
 window.addEventListener('load', function() {
 
@@ -21,11 +32,12 @@ window.addEventListener('load', function() {
  
     xhr.addEventListener('load', function(){
       var response = JSON.parse(xhr.response)
-            debugger
+            // debugger
       var picture = response.results[0].user.picture.large;
-
+      var name = response.results[0].user.name.first;
       // for (var i = 0; i < picture.length; i++){
-      showPicture(picture);
+
+      showPicture(picture, name);
       // }
     });
 
