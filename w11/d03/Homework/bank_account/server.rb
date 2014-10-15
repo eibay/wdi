d53bff2381a5e88270717a2b02a8f3a58206cff6
lisajwells <lisawells999@gmail.com>
2014-10-15 19:18:25 -0400

    adding some server logic

--- w11/d03/Homework/bank_account/server.rb
+++ w11/d03/Homework/bank_account/server.rb
@@ -11,3 +11,44 @@ use Rack::Session::Cookie, {
 	:secret => 'theapricotdanishisthebestthing'
 }
 
+get '/' do
+	erb(:index)
+end
+
+# this is for login
+post '/sessions' do
+	users = User.all
+	accounts = Account.all
+	session[:email] = params[:email]
+	session[:password] = params[:pwd]
+	
+	user = User.find_by(email: session[:email])
+
+	if user[:password] == session[:password]
+
+		account_id
+		redirect "/accounts/#{params[:id]}"
+binding.pry
+	
+
+
+end
+
+# this is for signup
+post '/users' do
+	new_user = {
+		:email => params[:email],
+		:password => params[:pwd]
+	}
+
+	User.create(new_user)
+
+	erb(:index)
+end
+
+# this is to edit balance
+put '/accounts/:id' do
+
+	erb(:account, { locals: { balance: balance } })
+end
+
