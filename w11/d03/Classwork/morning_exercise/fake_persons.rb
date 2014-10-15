18feebb440b74fe3683efd8be6a1c1ad6a28fec4
clay-reed-a <clay.reed.a@gmail.com>
2014-10-15 09:17:29 -0400

    set up main loop

---
+++ w11/d03/Classwork/morning_exercise/fake_persons.rb
@@ -0,0 +1,14 @@
+require 'faker'
+
+hash_literal = {}
+
+class Person
+  def initialize name, phone_number 
+    @name = name 
+    @phone_number = phone_number 
+  end 
+end 
+
+1000.times do 
+  
+end 
