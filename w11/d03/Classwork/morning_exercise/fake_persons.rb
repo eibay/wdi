bdeda1383583bedfba4a53e09c3e1e2fdc59a1cf
clay-reed-a <clay.reed.a@gmail.com>
2014-10-15 09:21:09 -0400

    Write main loop

--- w11/d03/Classwork/morning_exercise/fake_persons.rb
+++ w11/d03/Classwork/morning_exercise/fake_persons.rb
@@ -9,6 +9,11 @@ class Person
   end 
 end 
 
-1000.times do 
-  
+1000.times do |n| 
+  person_name = Faker::Name.name 
+  person_number = Faker::PhoneNumber.phone_number 
+  person_object = Person.new person_name, phone_number 
+  hash_literal[n] = person_object   
 end 
+
+p hash_literal 
