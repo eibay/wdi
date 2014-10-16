4ac7ba8d953afe0c91608c85de3ac160061c38af
clay-reed-a <clay.reed.a@gmail.com>
2014-10-16 11:42:05 -0400

    fix syntax errors

--- w11/d03/Homework/lib/connection.rb
+++ w11/d03/Homework/lib/connection.rb
@@ -1,8 +1,6 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection {
-   adapter: "postgresql",
-      host: "localhost", 
-  username: "clayreed", 
-  database: "make_bank"
-}
+ActiveRecord::Base.establish_connection adapter: "postgresql",
+                                           host: "localhost", 
+                                       username: "clayreed", 
+                                       database: "make_bank"
