f50d9808a94d6af591dfd05dc4ab46737214db40
pateltejal9500 <pateltejal9500@gmail.com>
2014-10-16 15:54:40 -0400

    encryption

--- w11/d03/Homework/schema.sql
+++ w11/d03/Homework/schema.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE accounts;
 \c accounts;
-CREATE TABLE users(name varchar(250), password varchar(250),amount integer, id serial primary key);
+CREATE TABLE users(name varchar(250), password_digest varchar(250),amount integer, id serial primary key);
--- w11/d03/Homework/server.rb
+++ w11/d03/Homework/server.rb
@@ -9,35 +9,51 @@ after do
 end
 
 use Rack::Session::Cookie, {
-    :key => 'rack.session',
-    :path => '/',
-    :secret => 'accountinformation'
-   }
+  :key => 'rack.session',
+  :path => '/',
+  :secret => 'accountinformation'
+}
 
 
 
-  get "/" do
-    erb(:index)
+get "/" do
+  erb(:index)
     #login page
   end
 
   post "/user" do
     login = params[:login]
     password = params[:password]
-    user = User.where({name: login, password: password})
-    if user[0]
-      session[:user] = user[0].name
-      erb(:user, {locals: {user: user[0]}})
+    user = User.find_by({name: login})
+    if user 
+      user.authenticate(password) #give you the user if true or false
+       if user 
+          session[:user] = user.name
+           erb(:user, {locals: {user: user}})
+      else
+        erb(:index)
+      end
     else
-      erb(:index)
+    erb(:index)
   end
 end
 
-get "/users/new" do
-  erb(:signup)
-end
 
-post "/users" do
+
+    # user = User.where({name: login, password: password})
+    # if user[0]
+    #   session[:user] = user[0].name
+    #   erb(:user, {locals: {user: user[0]}})
+    # else
+    #   erb(:index)
+    # end
+  # end
+
+  get "/users/new" do
+    erb(:signup)
+  end
+
+  post "/users" do
     user = {name: params[:login], password: params[:password], amount: params[:amount]}
     User.create(user)
     erb(:index)
@@ -48,25 +64,25 @@ post "/users" do
     user = User.find(params[:id])
     if session[:user] == user.name
     #seeing that users info with a place to change value
-      erb(:user, {locals: {user: user}})
-    else
-      erb(:index)
-   end
- end
+    erb(:user, {locals: {user: user}})
+  else
+    erb(:index)
+  end
+end
 
-  put "/users/:id" do
-    user = User.find(params[:id])
-    if session[:user] == user.name
+put "/users/:id" do
+  user = User.find(params[:id])
+  if session[:user] == user.name
     newUser = {amount: params[:amount]}
     user.update(newUser)
-   erb(:user, {locals: {user: user}})
- else
-  erb(:index)
+    erb(:user, {locals: {user: user}})
+  else
+    erb(:index)
   end
 end
 
 
 get "/logout" do
-      response.delete_cookie 'rack.session'
-      erb(:index)
+  response.delete_cookie 'rack.session'
+  erb(:index)
 end
