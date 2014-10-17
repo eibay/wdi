c1e653e19c6a287ffdcb6bec7b4da006b0d1008f
pateltejal9500 <pateltejal9500@gmail.com>
2014-10-17 08:59:28 -0400

    homework but templates

--- w11/d04/Classwork/first_rails_app/config/routes.rb
+++ w11/d04/Classwork/first_rails_app/config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  
-  resources :puppies
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+
+  get 'puppies/index'
+
+
+  resources :puppies
+
+
+  root 'puppies#index'
+
+  get '/puppies' => 'puppies#all'
+
   post '/puppies' => 'puppies#create'
+
+  get '/puppies/:id' => 'puppies#show'
+
+  put '/puppies/:id' => 'puppies#update'
+
+  delete '/puppies/:id' => 'puppies#delete'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
