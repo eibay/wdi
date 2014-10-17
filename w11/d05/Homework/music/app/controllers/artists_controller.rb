0f34840f47d655b5db660991c4fa3ca65f9e3726
theerickramer <theerickramer@gmail.com>
2014-10-17 16:31:56 -0400

    artists songs

---
+++ w11/d05/Homework/music/app/controllers/artists_controller.rb
@@ -0,0 +1,25 @@
+class ArtistsController < ApplicationController
+
+	def index
+		@artists = Artist.all()
+		render(:index)
+	end
+
+	def create 
+		Artist.create({name: params[:name][:attr], genre: params[:genre][:attr], number_of_albums: params[:number_of_albums][:attr]})
+		redirect_to '/'
+	end
+
+	def show
+		@artist = Artist.find(params[:id])
+		@song = Song.new()
+		@songs = Song.all()
+	end
+
+	def update
+		@artist = Artist.find(params[:id])
+		@artist.update({name: params[:artist][:name], genre: params[:artist][:genre], number_of_albums: params[:artist][:number_of_albums]})
+		redirect_to request.referrer
+	end
+
+end
