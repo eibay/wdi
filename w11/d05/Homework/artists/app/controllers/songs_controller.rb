5a7c8808c815596be025c7cf8c23f265a2007d1d
lisajwells <lisawells999@gmail.com>
2014-10-18 18:57:41 -0400

    adding ability to add song with CORRECT artist_id

--- w11/d05/Homework/artists/app/controllers/songs_controller.rb
+++ w11/d05/Homework/artists/app/controllers/songs_controller.rb
@@ -2,9 +2,9 @@ class SongsController < ApplicationController
 
 def create
 
-	artist = Artist.find_by(params[:id])
-	Song.create(name: params[:name], artist_id: artist.id)
-
+	# artist = Artist.find_by(params[:id])
+	Song.create(name: params[:name], artist_id: params[:artist_id])
+# binding.pry
 	redirect_to '/artists'
 end
 
