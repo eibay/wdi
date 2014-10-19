c8c5562875f35fb3c9cfc6109b16603543db4dd5
clay-reed-a <clay.reed.a@gmail.com>
2014-10-18 22:00:28 -0400

    try to use ActiveRecord properly, write a form to input an album

--- w11/d05/Homework/app_tunes/app/controllers/albums_controller.rb
+++ w11/d05/Homework/app_tunes/app/controllers/albums_controller.rb
@@ -1,6 +1,6 @@
 class AlbumsController < ApplicationController
   def view 
     @album = Album.find_by_id params[:id]
-    @songs = Song.find_by :album_id, params[:id]
+    @songs = Song.where :album_id, params[:id]
   end  
 end 
--- w11/d05/Homework/app_tunes/app/controllers/artists_controller.rb
+++ w11/d05/Homework/app_tunes/app/controllers/artists_controller.rb
@@ -5,12 +5,11 @@ class ArtistsController < ApplicationController
 
   def view 
     @artist = Artist.find_by_id params[:id]
-    @albums = Album.find_by :artist_id, params[:id] 
+    @albums = Album.where :artist_id == params[:id] 
   end 
 
   def create 
     Artist.create :name => params["name"], :genre => params["genre"], :number_of_albums => 0 
     redirect_to :index 
-     # undefined method `index_url' for ArtistsController upon create 
   end 
 end 
