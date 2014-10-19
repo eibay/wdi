7ba4f7d8b99e37242a7e8de97bd01ba540c9646a
clay-reed-a <clay.reed.a@gmail.com>
2014-10-18 20:32:32 -0400

    change indexes to views

--- w11/d05/Homework/app_tunes/config/routes.rb
+++ w11/d05/Homework/app_tunes/config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
 
   get 'artists' => 'artists#index'
   get 'artists/:id' => 'artists#view' 
-  get 'artists/:id/albums' => 'albums#index'
+  get 'artists/:id/albums' => 'albums#view'
+  get 'songs/:id' => 'song#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
