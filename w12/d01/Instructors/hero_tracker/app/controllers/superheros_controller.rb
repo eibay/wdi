46a585f1536b85d059f02d307aaf838981ecefea
Sean West <sean.west@generlassemb.ly>
2014-10-20 15:35:18 -0400

    update

--- w12/d01/Instructors/hero_tracker/app/controllers/superheros_controller.rb
+++ w12/d01/Instructors/hero_tracker/app/controllers/superheros_controller.rb
@@ -16,6 +16,7 @@ class SuperherosController < ApplicationController
 		end
 	end
 
+	#HTTParty.put('http://localhost:3000/superheros/1.json', :body => {name: "sean", real_identity: "john"})
 	def update
 		superhero = Superhero.find(params[:id])
 		superhero.update(name: params["name"], age: params["age"], real_identity: params["real_identity"], powers: params["powers"], city_id: params["city_id"])
