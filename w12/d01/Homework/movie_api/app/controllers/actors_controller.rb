bc58c1686e5998016daa97f4fe50f4788b3fdb81
clay-reed-a <clay.reed.a@gmail.com>
2014-10-20 20:08:22 -0400

    copy Neel's code line-by-line, understand what is meant by bad inputs from looking closer at lines below comments

---
+++ w12/d01/Homework/movie_api/app/controllers/actors_controller.rb
@@ -0,0 +1,53 @@
+class ActorsController < ApplicationController 
+
+    def index 
+      actors = Actor.all 
+
+      # list roles an actor has had, like this 
+      actors.each do |actor|
+        actor["roles"] = Role.where actor_id: actor.id 
+      end 
+    end 
+
+    respond_to do |format|
+      format.json { render :json => actors }
+    end 
+end 
+
+
+def create 
+  actor = Actor.new(
+    fname: params["fname"],
+    lname: params["lname"],
+    picture_url: params["picture_url"]
+  )
+
+  respond_to do |format|
+    format.json do 
+      if actor.valid? 
+        actor.save 
+        render :json => actor 
+      else 
+        render :json => actor.errors.messages.to_json 
+      end 
+    end 
+  end 
+end 
+
+def update 
+  actor = Actor.find_by_id params[:id]
+  actor.fname = params["fname"],
+  actor.lname = params["lname"],
+  actor.picture_url = params["picture_url"]
+
+  respond_to do |format|
+    format.json do 
+      if actor.valid?
+        actor.save
+        render :json => actor 
+      else 
+        render :json => actor.errors.messages.to_json
+      end 
+    end 
+  end 
+end 
--- w12/d01/Homework/movie_api/app/controllers/movies_controller.rb
+++ w12/d01/Homework/movie_api/app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   def index
     movies = Movie.all
 
-    # prolly a better way to do this but for now I'm just gonna #
+    # prolly a better way to do this but for now I'm just gonna... 
     movies.each do |movie|
       movie["roles"] = Role.where movie_id: movie.id 
     end  
@@ -23,7 +23,7 @@ class MoviesController < ApplicationController
       title: params["title"], 
        year: params["year"], 
  poster_url: params["poster_url"]
- )
+    )
 
     respond_to do |format|
       format.json do
@@ -38,9 +38,9 @@ class MoviesController < ApplicationController
   end
 
   #Good input
-  #HTTParty.put('http://localhost:3000/superheros/1.json', :body => {year: "2008"})
+  #HTTParty.put('http://localhost:3000/superheros/1.json', :body => {title: "Love Exposure", year: "2013", poster_url: "http://content7.flixster.com/movie/11/15/93/11159353_800.jpg"})
   #Bad input
-  #HTTParty.put('http://localhost:3000/superheros/1.json', :body => {title: "Love Exposure"})  
+  #HTTParty.put('http://localhost:3000/superheros/1.json', :body => {year: "2013"})  
   def update
     movie = Movie.find_by_id params[:id] 
     movie.title = params["name"]
@@ -58,5 +58,4 @@ class MoviesController < ApplicationController
       end
     end
   end
-
 end
