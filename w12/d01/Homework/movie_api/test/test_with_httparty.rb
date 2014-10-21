397d969fb40715ae2d17c47a2531a5bcf3ab650c
clay-reed-a <clay.reed.a@gmail.com>
2014-10-20 21:05:50 -0400

    forget it, write test w/o really knowing what's going on

--- w12/d01/Homework/movie_api/test/test_with_httparty.rb
+++ w12/d01/Homework/movie_api/test/test_with_httparty.rb
@@ -1,6 +1,20 @@
 require 'httparty'
 
-HTTParty.post 'http://localhost:3000/movies.json', 
+m = HTTParty.post 'http://localhost:3000/movies.json', 
 :body => {title: "Love Exposure", 
   year: "2013", 
   poster_url: "http://content7.flixster.com/movie/11/15/93/11159353_800.jpg"}) 
+
+a = HTTParty.post 'http://localhost:3000/actors.json',
+  :body => {
+    fname: "Makiko",
+    lname: "Watanabe",
+    picture_url: "http://ia.media-imdb.com/images/M/MV5BMTY4MDAwNzYxMF5BMl5BanBnXkFtZTcwNjQxNDc3MQ@@._V1_SX214_CR0,0,214,317_AL_.jpg"
+  }
+
+r = HTTParty.post 'http://localhost:3000/roles.json',
+:body => {
+  character_name: "Kaori",
+  movie_id: m.id, 
+  actor_id: a.id 
+}
